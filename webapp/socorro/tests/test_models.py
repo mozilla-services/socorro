711cc55809246a6d05e3f821d3954f4442ac43ad
sayrer <sayrer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2007-05-07 23:13:09 +0000

    Add tests for Report and add more Frame tests.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@65 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp/socorro/tests/test_models.py
+++ webapp/socorro/tests/test_models.py
@@ -1,4 +1,6 @@
 from socorro.models import *
+import sampledump
+import StringIO
 
 expected = {
     "0|0|libxpcom_core.so|NS_InitXPCOM3_P|nsXPComInit.cpp|549|0x440b8009":
@@ -15,3 +17,43 @@ for (line, signature) in expected.iteritems():
     f = Frame()
     f.readline(line)
     assert f.signature() == signature
+
+#
+# Test processing the output of minidump_stackwalk
+#
+assert sampledump.text != None
+fh = StringIO.StringIO(sampledump.text)
+r = Report()
+crashed_thread = r.read_header(fh)
+assert r.os_name == "Windows NT"
+assert r.os_version == "5.1.2600 Service Pack 2"
+assert r.cpu_name == "x86"
+assert r.cpu_info == "GenuineIntel family 6 model 14 stepping 8"
+assert r.reason == "EXCEPTION_ACCESS_VIOLATION"
+assert r.address == "0x0"
+assert crashed_thread is not None
+assert crashed_thread == 0
+
+#XXX skip over Module for now
+
+frame_num = 0
+loop_count = 0
+for line in fh:
+    loop_count += 1
+    if line.startswith(str(crashed_thread)):
+        frame = Frame()
+        frame.readline(line[0:-1])
+        frame.report_id = r.id
+        r.frames.append(frame)
+        frame_num += 1
+fh.close()
+
+assert loop_count > 12
+assert frame_num == 12
+assert len(r.frames) == 12
+assert r.frames[0].thread_num == "0"
+assert r.frames[0].module_name == "gklayout.dll"
+assert r.frames[0].function == "nsObjectFrame::Instantiate(char const *,nsIURI *)"
+assert r.frames[0].source == r"c:\firefox\mozilla\layout\generic\nsobjectframe.cpp"
+assert r.frames[0].source_line == "1348"
+assert r.frames[0].instruction == "0x24"
