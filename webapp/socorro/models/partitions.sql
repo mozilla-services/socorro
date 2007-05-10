2800a5fb2a8d98e108e02f2ab7c988879b81506a
morgamic <morgamic@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2007-05-10 21:26:45 +0000

    This was the pre-wtfness partitions.sql -- will all be changed, but it's good to note the indexes we'll need for reports_*.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@88 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp/socorro/models/partitions.sql
+++ webapp/socorro/models/partitions.sql
@@ -5,15 +5,43 @@
 -- check constraints and N indexes.  The check constraints seem to be checked
 -- every time a query comes into the parent reports table, so be careful.
 --
+-- Note that the default is a btree type index.
+--
+-- You should also have constraint_exclusion turned on in your pgsql config.
+-- This prevents unnecessary scanning of the date index on child tables.
+--
 
 select make_partition('reports',to_char(m,'YYYY_MM'),'date',
                       m::text, (m + interval '1 month')::text)
   from (select timestamptz '2007-01-01' + i * interval '1 month' as m
           from generate_series(0,23) s(i)) s2;
 
--- and create indexes on them:
+-- Create index on date.
+select exec(subst('create index idx_reports_$$_date on reports_$$ (date)',
+                  ARRAY[to_char(m,'YYYY_MM'), to_char(m,'YYYY_MM')]))
+  from (select timestamptz '2007-01-01' + i * interval '1 month' as m
+          from generate_series(0,23) s(i)) s2;
 
-select exec(subst('create index reports_$$_idx on reports_$$ (date)',
+-- Create index on product, version, build.
+select exec(subst('create index idx_reports_$$_product_version_build on reports_$$ (product, version, build)',
+                  ARRAY[to_char(m,'YYYY_MM'), to_char(m,'YYYY_MM')]))
+  from (select timestamptz '2007-01-01' + i * interval '1 month' as m
+          from generate_series(0,23) s(i)) s2;
+
+-- Create a unique index on uuid.
+select exec(subst('create unique index idx_reports_$$_uuid on reports_$$ (uuid)',
+                  ARRAY[to_char(m,'YYYY_MM'), to_char(m,'YYYY_MM')]))
+  from (select timestamptz '2007-01-01' + i * interval '1 month' as m
+          from generate_series(0,23) s(i)) s2;
+
+-- Create an index on signature.
+select exec(subst('create index idx_reports_$$_signature on reports_$$ (signature)',
+                  ARRAY[to_char(m,'YYYY_MM'), to_char(m,'YYYY_MM')]))
+  from (select timestamptz '2007-01-01' + i * interval '1 month' as m
+          from generate_series(0,23) s(i)) s2;
+
+-- Create an index on url.
+select exec(subst('create index idx_reports_$$_url on reports_$$ (url)',
                   ARRAY[to_char(m,'YYYY_MM'), to_char(m,'YYYY_MM')]))
   from (select timestamptz '2007-01-01' + i * interval '1 month' as m
           from generate_series(0,23) s(i)) s2;
