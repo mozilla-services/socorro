846f6dc13739e6ae2f22850de1a2b43dc769a1a1
sayrer <sayrer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2007-05-17 14:59:35 +0000

    Mirror the socorro module's layout
    git-svn-id: https://socorro.googlecode.com/svn/trunk@108 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- scripts/dist-standalone-processor.py
+++ scripts/dist-standalone-processor.py
@@ -3,10 +3,27 @@ import os
 import shutil
 import glob
 
-procssorDir = "./dist/processor"
+processorDir = "./dist/processor/socorro"
 
-def setupCommon(distPath):
+
+def moveModule(name, globString, distPath):
+  if not os.path.exists(os.path.join(distPath, name)):
+    os.mkdir(os.path.join(distPath, name))
+  matchFiles = glob.glob(globString)
+  for fname in matchFiles:
+    shutil.copy(fname, os.path.join(distPath, name, os.path.basename(fname)))
+
+def setup(distPath):
+  if not os.path.exists(distPath):
+    os.makedirs(distPath)
+  if not os.path.exists(os.path.join(distPath, "lib")):
+    os.makedirs(os.path.join(distPath, "lib"))
   toplevel = ["collect.py", "config.py", "uuid.py", "monitor.py",
               "processor.py"]
   for name in toplevel:
-    shutil.copy("./webapp/socorro/lib/" + name, distPath + "/" + name)
+    shutil.copy("./webapp/socorro/lib/" + name, distPath + "/lib/" + name)
+  moveModule("simplejson", "./webapp/socorro/lib/simplejson/*.py", distPath)
+  moveModule("models", "./webapp/socorro/models/*.py", distPath)
+
+  
+setup(processorDir)
