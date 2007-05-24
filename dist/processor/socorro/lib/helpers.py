aa6d2f524821c01c168b347751ca610e5f0f492d
sayrer <sayrer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2007-05-24 22:27:58 +0000

    update the processor script yet again
    git-svn-id: https://socorro.googlecode.com/svn/trunk@161 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ dist/processor/socorro/lib/helpers.py
@@ -0,0 +1,35 @@
+"""
+Helper functions
+
+All names available in this module will be available under the Pylons h object.
+"""
+from webhelpers import *
+from pylons.helpers import log
+from pylons.i18n import get_lang, set_lang
+
+from genshi.core import Markup
+def wrap_helpers(localdict):
+    def helper_wrapper(func):
+        def wrapped_helper(*args, **kw):
+            return Markup(func(*args, **kw))
+        wrapped_helper.__name__ = func.__name__
+        return wrapped_helper
+    for name, func in localdict.iteritems():
+        if not callable(func) or not func.__module__.startswith('webhelpers.rails'):
+            continue
+        localdict[name] = helper_wrapper(func)
+wrap_helpers(locals())
+
+def EmptyFilter(x):
+  """Return None if the argument is an empty string, otherwise
+     return the argument."""
+  if x == '':
+    return None
+  return x
+
+def get_row_class(i):
+  """
+  Return a row class (1|2) based on passed value.  For use in alternating table
+  row colors.
+  """
+  return 'row'+str(int(i)%2+1)
