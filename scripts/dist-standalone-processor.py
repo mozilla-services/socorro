462f957fb8f87351a9e72dfa6d34b2b29b0a1ec7
sayrer <sayrer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2007-05-24 18:41:55 +0000

    fix processor dist script to copy __init__.py files
    git-svn-id: https://socorro.googlecode.com/svn/trunk@153 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- scripts/dist-standalone-processor.py
+++ scripts/dist-standalone-processor.py
@@ -7,9 +7,12 @@ processorDir = "./dist/processor/socorro"
 
 def setup(distPath):
   makeDistDirs(distPath, ["lib"])
-  copyLibFiles(["monitor.py", "processor.py"], distPath, "/lib/")
+  copyLibFiles(["monitor.py", "processor.py", "__init__.py"],
+               distPath, "/lib/")
   copyModule("simplejson", "./webapp/socorro/lib/simplejson/*.py", distPath)
   copyModule("models", "./webapp/socorro/models/*.py", distPath)
+  shutil.copy("./webapp/socorro/lib/__init__.py",
+              os.path.join(distPath, "__init__.py"))
   shutil.copy("./scripts/start-processor.py",
               os.path.join(distPath, "../start-processor.py"))
   shutil.copy("./docs/README-standalone-processor.txt",
