d90294988a05678383878e089796f13eb36cbea9
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-03-21 20:54:06 +0000

    intial check in of library files
    features:
    
    python 2.4 compatible threadlib
    
    
    
    git-svn-id: https://socorro.googlecode.com/svn/trunk@321 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ socorro/lib/util.py
@@ -0,0 +1,36 @@
+import socorro.lib.config as config
+
+import sys
+import traceback
+import datetime
+
+import threading
+
+aLock = threading.RLock()
+
+def reportExceptionAndContinue(ignoreFunction=None):
+  try:
+    exceptionType, exception, tracebackInfo = sys.exc_info()
+    if ignoreFunction and ignoreFunction(exceptionType, exception, tracebackInfo):
+      return
+    if exceptionType in (KeyboardInterrupt, SystemExit):
+      raise  exception
+    aLock.acquire()
+    try:
+      print >>config.errorReportStream, "*** thread %s ***" % threading.currentThread().getName()
+      print >>config.errorReportStream,  "%s Caught Error: %s" %  (datetime.datetime.now(), exceptionType)
+      print  >>config.errorReportStream, "%s%s" % (' '*27, exception)
+      traceback.print_tb(tracebackInfo, None, config.errorReportStream)
+    finally:
+      aLock.release()
+  except Exception, x:
+    print x
+
+
+def reportExceptionAndAbort():
+  reportExceptionAndContinue()
+  sys.exit(-1)
+  
+def emptyFilter(x):
+  return (x, None)[x==""]
+
