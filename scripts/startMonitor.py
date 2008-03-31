ee4355918ad9309af4cbc8f3658ce80c45e3ab71
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-03-31 00:13:10 +0000

    added python logging to stderr and configurable rotating log files
    git-svn-id: https://socorro.googlecode.com/svn/trunk@329 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- scripts/startMonitor.py
+++ scripts/startMonitor.py
@@ -1,12 +1,40 @@
 #! /usr/bin/env python
 
-import socorro.monitor
-import socorro.config as config
+import sys
+import logging
+import logging.handlers
 
-socorro.monitor.startMonitor()
+import socorro.monitor.config as config
+import socorro.monitor.monitor as monitor
+import socorro.lib.ConfigurationManager as configurationManager
 
-print >>config.statusReportStream, "Done."
+try:
+  configurationContext = configurationManager.newConfiguration(configurationModule=config, applicationName="Socorro Monitor 2.0")
+except configurationManager.NotAnOptionError, x:
+  print >>sys.stderr, x
+  print >>sys.stderr, "for usage, try --help"
+  sys.exit()
 
+logger = logging.getLogger("monitor")
+logger.setLevel(logging.DEBUG)
 
+stderrLog = logging.StreamHandler()
+stderrLog.setLevel(configurationContext.stderrErrorLoggingLevel)
+stderrLogFormatter = logging.Formatter(configurationContext.stderrLineFormatString)
+stderrLog.setFormatter(stderrLogFormatter)
+logger.addHandler(stderrLog)
+
+rotatingFileLog = logging.handlers.RotatingFileHandler(configurationContext.logFilePathname, "a", configurationContext.logFileMaximumSize, configurationContext.logFileMaximumBackupHistory)
+rotatingFileLog.setLevel(logging.DEBUG)
+rotatingFileLogFormatter = logging.Formatter(configurationContext.logFileLineFormatString)
+rotatingFileLog.setFormatter(rotatingFileLogFormatter)
+logger.addHandler(rotatingFileLog)
+
+logger.info("current configuration\n%s", str(configurationContext))
+
+try:
+  monitor.startMonitor(configurationContext)
+finally:
+  logger.info("done.")
 
 
--- scripts/startProcessor.py
+++ scripts/startProcessor.py
@@ -1,11 +1,41 @@
 #! /usr/bin/env python
 
-import socorro.externalProcessor
-import socorro.config as config
+import sys
+import logging
+import logging.handlers
 
-p = socorro.externalProcessor.ProcessorWithExternalBreakpad()
-p.start()
-print >>config.statusReportStream, "Done."
+import socorro.processor.config as config
+import socorro.processor.externalProcessor as processor
+import socorro.lib.ConfigurationManager as configurationManager
 
+try:
+  configurationContext = configurationManager.newConfiguration(configurationModule=config, applicationName="Socorro Processor 2.0")
+except configurationManager.NotAnOptionError, x:
+  print >>sys.stderr, x
+  print >>sys.stderr, "for usage, try --help"
+  sys.exit()
+  
+logger = logging.getLogger("processor")
+logger.setLevel(logging.DEBUG)
+
+stderrLog = logging.StreamHandler()
+stderrLog.setLevel(configurationContext.stderrErrorLoggingLevel)
+stderrLogFormatter = logging.Formatter(configurationContext.stderrLineFormatString)
+stderrLog.setFormatter(stderrLogFormatter)
+logger.addHandler(stderrLog)
+
+rotatingFileLog = logging.handlers.RotatingFileHandler(configurationContext.logFilePathname, "a", configurationContext.logFileMaximumSize, configurationContext.logFileMaximumBackupHistory)
+rotatingFileLog.setLevel(logging.DEBUG)
+rotatingFileLogFormatter = logging.Formatter(configurationContext.logFileLineFormatString)
+rotatingFileLog.setFormatter(rotatingFileLogFormatter)
+logger.addHandler(rotatingFileLog)
+
+logger.info("current configuration\n%s", str(configurationContext))
+
+try:
+  p = processor.ProcessorWithExternalBreakpad(configurationContext)
+  p.start()
+finally:
+  logger.info("done.")
 
 
