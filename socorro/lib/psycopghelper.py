07d06e15b0819978379c3b1bb8994702da1210c2
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-08-12 17:08:37 +0000

    initial checkin - a few rountines to make psycopg slightly less painful
    git-svn-id: https://socorro.googlecode.com/svn/trunk@446 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ socorro/lib/psycopghelper.py
@@ -0,0 +1,49 @@
+
+import psycopg2
+import psycopg2.extensions
+import datetime
+
+class LoggingCursor(psycopg2.extensions.cursor):
+  def setLogger(self, logger):
+    self.logger = logger
+    self.logger.info("Now logging cursor: %s", str(cursor))
+
+  def execute(self, sql):
+    try:
+      self.logger.info(sql)
+    except AttributeError:
+      pass
+    super(LoggingCursor, self).execute(sql)
+
+
+def postgreSQLTypeConversion (x):
+  if type(x) == datetime.datetime:
+    return "'%4d-%2d-%2d %2d:%2d:%2d'" % (x.year, x.month, x.day, x.hour, x.minute, x.second)
+  if type(x) == str:
+    return "'%s'" % x
+  return str(x)
+
+class SQLDidNotReturnSingleValue (Exception):
+  pass
+
+def singleValueSql (aCursor, sql):
+  aCursor.execute(sql)
+  result = aCursor.fetchall()
+  try:
+    return result[0][0]
+  except Exception, x:
+    raise SQLDidNotReturnSingleValue("%s: %s" % (str(x), sql))
+
+class SQLDidNotReturnSingleRow (Exception):
+  pass
+
+def singleRowSql (aCursor, sql):
+  aCursor.execute(sql)
+  result = aCursor.fetchall()
+  try:
+    return result[0]
+  except Exception, x:
+    raise SQLDidNotReturnSingleRow("%s: %s" % (str(x), sql))
+
+class CannotDetermineTableLength (Exception):
+  pass
