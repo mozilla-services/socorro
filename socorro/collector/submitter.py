ff79a4117f43814ceb258df99941ac04767fa3fd
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-08-27 02:01:38 +0000

    reworked to make it more pythonic   added file closes in anticipation of using jython & grinder in the future   removed print to stdout   added exception handling
    git-svn-id: https://socorro.googlecode.com/svn/trunk@464 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/collector/submitter.py
+++ socorro/collector/submitter.py
@@ -3,31 +3,31 @@
 import urllib2
 import simplejson
 import sys
-
-def loadpost(filename):
-  dict = simplejson.load(open(filename,'r'))
-  if dict:
-    return dict
-  else:
-    print "unable to load json from",repr(options.jsonfile)
-    return {}
+import socorro.lib.filesystem
 
 def submitCrashReport (jsonFilePathName, binaryFilePathName, serverURL):
-  data = {}
-  if jsonFilePathName:
-    data = loadpost(jsonFilePathName)
-  if binaryFilePathName:
-    data['upload_file_minidump'] = open(binaryFilePathName,'r')
-  if data:
+  jsonFile = open(jsonFilePathName)
+  try:
+    data = simplejson.load(jsonFile)
+  finally:
+    jsonFile.close()
+  binaryFile = open(binaryFilePathName)
+  try:
+    data['upload_file_minidump'] = binaryFile
     urllib2.urlopen(serverURL,data)
+  finally:
+    binaryFile.close()
 
-def walkFileSystemSubmittingReports (fileSystemRoot, serverURL):
-  for aPath, aFileName, aJsonPathName in socorro.lib.filesystem(fileSystemRoot, lambda x: x[2].endswith("json")):
+def reportErrorToStderr (x):
+  print >>sys.stderr, x
+
+def walkFileSystemSubmittingReports (fileSystemRoot, serverURL, errorReporter=reportErrorToStderr):
+  for aPath, aFileName, aJsonPathName in socorro.lib.filesystem.findFileGenerator(fileSystemRoot, lambda x: x[2].endswith("json")):
     try:
       dumpfilePathName = os.path.join(aPath, "%s%s" % (aFileName[:-5], ".dump"))
       submitCrashReport(aJsonPathName, dumpfilePathName, serverURL)
-    except IOError:
-      pass
+    except Exception, x:
+      errorReporter(x)
 
 if __name__ == '__main__':
   import socorro.lib.ConfigurationManager
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     ('u',  'url', True, 'http://localhost/crash-reports/submit', "The url of the server to load test"),
     ('j',  'jsonfile', True, None, 'the pathname of a json file for POST'),
     ('d',  'dumpfile', False, None, 'the pathname of a dumpfile to upload with the POST'),
-    (None, 'searchRoot', True, None, 'a filesystem location to begin a search for json/dump combos'),
+    ('s',  'searchRoot', True, None, 'a filesystem location to begin a search for json/dump combos'),
     ]
 
   config = socorro.lib.ConfigurationManager.newConfiguration(configurationOptionsList=commandLineOptions)
@@ -46,7 +46,10 @@ if __name__ == '__main__':
   if config.searchRoot:
     walkFileSystemSubmittingReports(config.searchRoot, config.url)
   else:
-    submitCrashReport(config.jsonfile, config.dumpfile, config.url)
+    try:
+      submitCrashReport(config.jsonfile, config.dumpfile, config.url)
+    except Exception, x:
+      print >>sys.sterr, x
 
 
 
