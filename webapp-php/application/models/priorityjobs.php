c780ef132111753dd322f23c58b7156cfe0cb319
l.m.orchard <l.m.orchard@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-08-28 21:40:10 +0000

    Trapping and ignoring a duplicate key error on priority job insert
    git-svn-id: https://socorro.googlecode.com/svn/trunk@471 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/models/priorityjobs.php
+++ webapp-php/application/models/priorityjobs.php
@@ -11,7 +11,14 @@ class Priorityjobs_Model extends Model {
      * @return mixed  Result of the DB insert
      */
     public function add($uuid) {
-        return $this->db->query('INSERT INTO priorityjobs ( uuid ) VALUES (?)', $uuid); 
+        try {
+            $rv = @$this->db->query('INSERT INTO priorityjobs ( uuid ) VALUES (?)', $uuid); 
+        } catch (Kohana_Database_Exception $e) {
+            // HACK: Trap and ignore a potential duplicate key error here, but 
+            // raise any other errors.
+            if (strpos( (string) $e, 'duplicate key' ) === FALSE ) throw $e;
+        }
+        return true;
     }
 
 }
