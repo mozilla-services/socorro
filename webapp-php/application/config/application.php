fa71621e7f0b7db5e144cb79e3d6c92998a301f8
l.m.orchard <l.m.orchard@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-09-08 23:18:41 +0000

    Updating VCS mappings to reflect bug 440074, now including hg
    git-svn-id: https://socorro.googlecode.com/svn/trunk@504 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/config/application.php
+++ webapp-php/application/config/application.php
@@ -3,13 +3,16 @@
  * Application-specific settings separate from installation concerns.
  */
 
-/** 
- * Returned to the client with a uuid following
- */
 $config['dumpIDPrefix'] = 'bp-';
 
+// Mappings of source code types to web links
 $config['vcsMappings'] = array(
-    'cvs' => array(
-        'cvs.mozilla.org/cvsroot' => 'http://bonsai.mozilla.org/cvsblame.cgi?file=%(file)s&rev=%(revision)s&mark=%(line)s#%(line)s'
+    'cvs' => array( 
+        'cvs.mozilla.org/cvsroot' => 
+          'http://bonsai.mozilla.org/cvsblame.cgi?file=%(file)s&rev=%(revision)s&mark=%(line)s#%(line)s' 
+    ),
+    'hg' => array( 
+        'hg.mozilla.org/mozilla-central' => 
+            'http://hg.mozilla.org/mozilla-central/index.cgi/annotate/%(revision)s/%(file)s#l%(line)s' 
     )
 );
