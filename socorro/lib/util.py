39c307d95c88a019f31735773b4f798fb12949d9
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2008-10-24 15:32:57 +0000

    Add new routines & classes rearranged for aesthetic pleasure
    git-svn-id: https://socorro.googlecode.com/svn/trunk@646 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/lib/util.py
+++ socorro/lib/util.py
@@ -3,19 +3,21 @@ import traceback
 import datetime
 import cStringIO
 import threading
+import collections
 
 import logging
 
-aLock = threading.RLock()
-
+#-----------------------------------------------------------------------------------------------------------------
+loggingReportLock = threading.RLock()
 def report(message):
-  aLock.acquire()  #make sure these multiple log entries stay together
-  try:  
+  loggingReportLock.acquire()  #make sure these multiple log entries stay together
+  try:
     logger.info("*** thread %s ***", threading.currentThread().getName())
     logger.info(message)
   finally:
-    aLock.release()  
+    loggingReportLock.release()
 
+#-----------------------------------------------------------------------------------------------------------------
 def reportExceptionAndContinue(logger, loggingLevel=logging.ERROR, ignoreFunction=None):
   try:
     exceptionType, exception, tracebackInfo = sys.exc_info()
@@ -23,7 +25,7 @@ def reportExceptionAndContinue(logger, loggingLevel=logging.ERROR, ignoreFunctio
       return
     if exceptionType in (KeyboardInterrupt, SystemExit):
       raise  exception
-    aLock.acquire()   #make sure these multiple log entries stay together
+    loggingReportLock.acquire()   #make sure these multiple log entries stay together
     try:
       logger.log(loggingLevel, "%s Caught Error: %s", threading.currentThread().getName(), exceptionType)
       logger.log(loggingLevel, exception)
@@ -36,34 +38,38 @@ def reportExceptionAndContinue(logger, loggingLevel=logging.ERROR, ignoreFunctio
       finally:
         stringStream.close()
     finally:
-      aLock.release()
+      loggingReportLock.release()
   except Exception, x:
     print x
 
-
+#-----------------------------------------------------------------------------------------------------------------
 def reportExceptionAndAbort(logger):
   reportExceptionAndContinue(logger, logging.CRITICAL)
   logger.critical("cannot continue - quitting")
   raise SystemExit
-  
+
+#-----------------------------------------------------------------------------------------------------------------
 def emptyFilter(x):
   return (x, None)[x==""]
 
+#-----------------------------------------------------------------------------------------------------------------
 def limitStringOrNone(aString, maxLength):
   try:
     return aString[:maxLength]
   except TypeError:
     return None
-  
+
+#-----------------------------------------------------------------------------------------------------------------
 def lookupLimitedStringOrNone(aDict, aKey, maxLength):
   try:
     return limitStringOrNone(aDict[aKey], maxLength)
   except KeyError:
     return None
 
-import collections
 
+#=================================================================================================================
 class CachingIterator(object):
+  #-----------------------------------------------------------------------------------------------------------------
   def __init__(self, anIterator):
     self.theIterator = anIterator
     self.cache = []
@@ -71,7 +77,7 @@ class CachingIterator(object):
     self.secondaryCacheMaximumSize = 11
     self.secondaryCacheSize = 0
     self.useSecondary = False
-    
+  #-----------------------------------------------------------------------------------------------------------------
   def __iter__(self):
     #try:  #to be used in Python 2.5 or greater
       for x in self.theIterator:
@@ -87,17 +93,30 @@ class CachingIterator(object):
         yield x
     #finally:
     #  self.stopUsingSecondaryCache()
-      
+  #-----------------------------------------------------------------------------------------------------------------
   def useSecondaryCache(self):
     self.useSecondary = True
-    
+  #-----------------------------------------------------------------------------------------------------------------
   def stopUsingSecondaryCache(self):
     self.useSecondary = False
     self.cache.extend(self.secondaryLimitedSizeCache)
     self.secondaryCacheSize = 0
     self.secondaryLimitedSizeCache = collections.deque()
 
+#=================================================================================================================
+class FakeLogger(object):
+  def log(self,*x): print >>sys.stderr, x[1]
+  def debug(self,*x): print >>sys.stderr, x
+  def info(self,*x): print >>sys.stderr, x
+  def warning(self,*x): print >>sys.stderr, x
+  def error(self,*x): print >>sys.stderr, x
+  def critical(self,*x): print >>sys.stderr, x
 
-  
-
-    
+#=================================================================================================================
+class SilentFakeLogger(object):
+  def log(self,*x): pass
+  def debug(self,*x): pass
+  def info(self,*x): pass
+  def warning(self,*x): pass
+  def error(self,*x): pass
+  def critical(self,*x): pass
