ad2179d49576c6f50e8d3a116f64f75f2a826ff5
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-03-10 22:36:48 +0000

    initial check in
    git-svn-id: https://socorro.googlecode.com/svn/trunk@931 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ scripts/createPartitions.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import logging
+import logging.handlers
+import sys
+
+try:
+  import config.createpartitionsconfig as config
+except ImportError:
+    import createpartitionsconfig as config
+
+import socorro.lib.ConfigurationManager as configurationManager
+import socorro.database.schema as schema
+
+try:
+  configurationContext = configurationManager.newConfiguration(configurationModule=config, applicationName="startNextPartition")
+except configurationManager.NotAnOptionError, x:
+  print >>sys.stderr, x
+  print >>sys.stderr, "for usage, try --help"
+  sys.exit(1)
+
+logger = logging.getLogger("nextPartition")
+logger.setLevel(logging.DEBUG)
+
+stderrLog = logging.StreamHandler()
+stderrLog.setLevel(configurationContext.stderrErrorLoggingLevel)
+stderrLogFormatter = logging.Formatter(configurationContext.stderrLineFormatString)
+stderrLog.setFormatter(stderrLogFormatter)
+logger.addHandler(stderrLog)
+
+rotatingFileLog = logging.handlers.RotatingFileHandler(configurationContext.logFilePathname, "a", configurationContext.logFileMaximumSize, configurationContext.logFileMaximumBackupHistory)
+rotatingFileLog.setLevel(logging.DEBUG)
+rotatingFileLogFormatter = logging.Formatter(configurationContext.logFileLineFormatString)
+rotatingFileLog.setFormatter(rotatingFileLogFormatter)
+logger.addHandler(rotatingFileLog)
+
+logger.info("current configuration\n%s", str(configurationContext))
+
+try:
+  schema.createPartitions(configurationContext, logger)
+finally:
+  logger.info("done.")
+  rotatingFileLog.flush()
+  rotatingFileLog.close()
