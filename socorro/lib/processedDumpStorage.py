154fc9184379e0e770e6a971d66d888378adf28c
griswolf <griswolf@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-04-10 02:52:37 +0000

    Change processedDumpStorage.py to match spec for file type; modify testing to match, also rename test file to match
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1000 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/lib/processedDumpStorage.py
+++ socorro/lib/processedDumpStorage.py
@@ -1,106 +1,116 @@
+import gzip
 import logging
 import os
+import simplejson
 import stat
-import gzip
 
 import socorro.lib.ooid as socorro_ooid
 import socorro.lib.util as socorro_util
 
-class DmpStorage(object):
+class ProcessedDumpStorage(object):
   """
   This class, mirrored from JsonDumpStorage in March 2009, implements a gzipped file system storage
-  scheme forthe 'cooked raw dump data' from stackwalk_minidump. For historical reasons, these are
-  'dmpgz' files.
-  Data is stored in the file system using a radix structure based on the ooid or uuid of the data:
-  A (nearly) unique identifier assigned at time of collection. The ooid has three parts:
+  scheme for the 'cooked raw dump data' from stackwalk_minidump. The file format is gzipped json, with
+  default suffix '.jsonz'
+  Files are located using a radix structure based on the ooid or uuid of the data, a (nearly) unique
+  identifier assigned at time of collection. The ooid has three parts:
    - The uuid prefix
    - A suffix that encodes the date of assignment
-   - information about the appropriate depth of the radix tree (by default: 4, but now usually 2)
+   - information about the appropriate depth of the radix tree (by default: 4, but now always 2)
   The storage is a file whose name is ooid.suffix, whose path is determined by the ooid itself
   """
   def __init__(self, root = '.', **kwargs):
     """
     Set up the basic conditions for storing gmpgz files. Possible kwargs keys:
-     - 'dmpName': The (absolute or relative) path to the top of the storage tree. Default '.'
-     - 'dmpSuffix': The storage filename suffix. Default '.gmpgz'
+     - 'rootName': The (absolute or relative) path to the top of the storage tree. Default '.'
+     - 'fileSuffix': The storage filename suffix. Default '.gmpgz'
      - 'gzipCompression': The level of compression to use. Default = 9
-     - 'logger': A logger. Default: logging.getLogger('dmpStorage')
+     - 'logger': A logger. Default: logging.getLogger('dumpStorage')
     """
-    super(DmpStorage, self).__init__()
+    super(ProcessedDumpStorage, self).__init__()
     self.root = root.rstrip(os.sep)
-    self.dmpName = kwargs.get('dmpName','dmps')
-    self.dmpSuffix = kwargs.get('dmpSuffix','.dmpgz')
+    self.rootName = kwargs.get('rootName','.')
+    self.fileSuffix = kwargs.get('fileSuffix','.jsonz')
     self.gzipCompression = int(kwargs.get('gzipCompression',9))
-    if not self.dmpSuffix.startswith('.'):
-      self.dmpSuffix = ".%s" % (self.dmpSuffix)
-    self.dmpBranch = os.path.join(self.root,self.dmpName)
-    self.logger = kwargs.get('logger', logging.getLogger('dmpStorage'))
+    if not self.fileSuffix.startswith('.'):
+      self.fileSuffix = ".%s" % (self.fileSuffix)
+    self.storageBranch = os.path.join(self.root,self.rootName)
+    self.logger = kwargs.get('logger', logging.getLogger('dumpStorage'))
 
   def newEntry(self, uuid):
     """
-    Given a uuid, create an empty dmp file and hand back a writeable 'file' handle (actually GzipFile)
+    Given a uuid, create an empty file and hand back a writeable 'file' handle (actually GzipFile)
     """
-    dmpDir = self.__makeDmpDir(uuid)
-    dname = os.path.join(dmpDir,uuid+self.dmpSuffix)
+    dumpDir = self.__makeDumpDir(uuid)
+    dname = os.path.join(dumpDir,uuid+self.fileSuffix)
     df = None
     try:
       df = gzip.open(dname,'w',self.gzipCompression)
     except IOError,x:
       if 2 == x.errno:
         # We might have lost this directory during a cleanup in another thread or process. Do again.
-        dmpDir = self.__makeDmpDir(uuid)
+        dumpDir = self.__makeDumpDir(uuid)
         df = gzip.open(dname,'w',self.gzipCompression)
       else:
         raise x
     return df
 
-  def makeDmp(self,uuid,dmpIter):
+  def putDumpToFile(self,uuid,dumpObject):
     """
-    Given a uuid and a mini_stackdump iterator or equivalent,
-      create the appropriate dmp file
-      populate it with newline separated lines from the iterator.
+    Given a uuid and an dumpObject, create the appropriate dump file and fill it with object's data
     """
     fh = self.newEntry(uuid)
     try:
-      for line in dmpIter:
-        fh.write(line.strip()) # be sure we don't have multiple newlines or spaces
-        fh.write('\n') # be sure we do have one for each dump line
+      simplejson.dump(dumpObject,fh)
     finally:
       fh.close()
 
-  def getDmpFile(self,uuid):
+  def getDumpFromFile(self,uuid):
     """
-    Return an absolute path for the dmp file for a given uuid
+    Given a uuid, extract and return a dumpObject from the associated file if possible.
+    raises OSError if the file is missing or unreadable
+    """
+    df = None
+    try:
+      df = gzip.open(self.getDumpPath(uuid))
+      return simplejson.load(df)
+    finally:
+      if df:
+        df.close()
+
+  def getDumpPath(self,uuid):
+    """
+    Return an absolute path for the file for a given uuid
     Raise: OSError if the file is missing or unreadable
     """
-    path = "%s%s%s%s" % (self.__dmpPath(uuid),os.sep,uuid,self.dmpSuffix)
+    path = "%s%s%s%s" % (self.__dumpPath(uuid),os.sep,uuid,self.fileSuffix)
     # os.stat is moderately faster than trying to open for reading
     self.__readableOrThrow(path)
     return path
 
-  def removeDmpFile(self, uuid):
+  def removeDumpFile(self, uuid):
     """
-    Find and remove the dmp file for the given uuid.
+    Find and remove the dump file for the given uuid.
     Quietly continue if unfound. Log problem and continue if irremovable.
     """
     try:
-      filePath = self.getDmpFile(uuid)
+      filePath = self.getDumpPath(uuid)
       os.unlink(filePath)
     except OSError,x:
       if 2 != x.errno:
         socorro_util.reportExceptionAndContinue(self.logger)
 
-  def __dmpPath(self, uuid):
+  def __dumpPath(self, uuid):
     """Return the path to the directory for this uuid"""
     depth = socorro_ooid.depthFromOoid(uuid)
     if not depth: depth = 4
-    dirs = [self.dmpBranch]
+    dirs = [self.storageBranch]
     dirs.extend([uuid[2*x:2*x+2] for x in range(depth)])
     return os.sep.join(dirs)
 
-  def __makeDmpDir(self,uuid):
-    """Make sure the dmp directory exists, and return its path"""
-    dpath = self.__dmpPath(uuid)
+  def __makeDumpDir(self,uuid):
+    """Make sure the dump directory exists, and return its path"""
+    dpath = self.__dumpPath(uuid)
     try:
       os.makedirs(dpath)
     except OSError,e:
