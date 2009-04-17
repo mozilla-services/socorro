b6b66c3e19c25750dbb1c064bc4df06e28943108
austin.ok <austin.ok@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-04-17 22:05:20 +0000

    Bug 488859 Adding Camino, Changing active version for FF Thunderbird r=ss
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1027 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/config/dashboard.php
+++ webapp-php/application/config/dashboard.php
@@ -2,11 +2,11 @@
 
 $config['topcrashbysig_numberofsignatures'] = 3;
 $config['topcrashbysig_featured'] = array(
-    array('product' => 'Firefox',         'version' => '3.1b3pre'),
-    array('product' => 'Thunderbird', 'version' => '3.0b2pre'),
-
+    array('product' => 'Firefox',         'version' => '3.5b4pre'),
+    array('product' => 'Thunderbird', 'version' => '3.0b3pre'),
     array('product' => 'SeaMonkey',  'version' => '2.0a3pre'),
-    array('product' => 'Sunbird',        'version' => '1.0pre')
+    array('product' => 'Sunbird',        'version' => '1.0pre'),
+    array('product' => 'Camino',        'version' => '2.0b3pre')
 );
 $config['topcrashbyurl_numberofurls'] = 3;
 $config['topcrashbyurl_featured'] = array(
