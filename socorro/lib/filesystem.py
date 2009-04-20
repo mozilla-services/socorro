a322b75fd6c20adca9eaa60394f247886f2b6827
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-04-20 19:05:03 +0000

    coded a replacement for os.makedirs as it has a flaw.  The right most directory isn't protected from an EEXIST error.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1028 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/lib/filesystem.py
+++ socorro/lib/filesystem.py
@@ -1,3 +1,37 @@
+
+
+import os
+import os.path
+from os.path import curdir
+import errno
+
+#-----------------------------------------------------------------------------------------------------------------
+def makedirs(name, mode=0777):
+  """makedirs(path [, mode=0777])
+
+  Super-mkdir; create a leaf directory and all intermediate ones.
+  Works like mkdir, except that any intermediate path segment (not
+  just the rightmost) will be created if it does not exist.  This is
+  recursive.
+
+  picked up from os.py, this version switches the try:except: protection to the call to os.mkdir()
+  instead of the recursive call to makedirs()
+  """
+  head, tail = os.path.split(name)
+  if not tail:
+    head, tail = os.path.split(head)
+  if head and tail and not os.path.exists(head):
+    makedirs(head, mode)
+    if tail == curdir:           # xxx/newdir/. exists if xxx/newdir exists
+      return
+  try:
+    os.mkdir(name, mode)
+  except OSError, e:
+    #be happy if someone already created the path
+    if e.errno != errno.EEXIST:
+      raise
+
+#-----------------------------------------------------------------------------------------------------------------
 #!/usr/bin/python
 #
 # Copyright 2005 by Centaur Software Engineering, Inc.
@@ -18,9 +52,6 @@
 #    along with The CSE Python Library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-
-import os
-
 #-----------------------------------------------------------------------------------------------------------------
 def defaultAcceptanceFunction (dummy):
   return True
