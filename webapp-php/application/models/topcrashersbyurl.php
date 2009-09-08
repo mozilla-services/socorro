9812d4e727a4cd1861120be7a1e8853bf40e1723
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-09-08 19:02:29 +0000

    updated SQL didn't have proper column name to correspond with a template variable name..
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1320 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/models/topcrashersbyurl.php
+++ webapp-php/application/models/topcrashersbyurl.php
@@ -18,18 +18,18 @@ class TopcrashersByUrl_Model extends Model {
     $start_date = date("Y-m-d", $aTime - (60 * 60 * 24 * 14) + 1);
 
     $product_id = $this->getProductId($product, $version);
-    $sql = "/* soc.web tcbyrul geturls */ 
-           SELECT SUM(facts.count) AS count, urldims.url 
+    $sql = "/* soc.web tcbyrul geturls */
+           SELECT SUM(facts.count) AS count, urldims.url
            FROM top_crashes_by_url AS facts
-           JOIN urldims ON urldims.id = facts.urldims_id 
+           JOIN urldims ON urldims.id = facts.urldims_id
            WHERE TIMESTAMP WITHOUT TIME ZONE '$start_date' <= facts.window_end
-             AND facts.window_end <= TIMESTAMP WITHOUT TIME ZONE '$end_date' 
-             AND productdims_id = $product_id 
-           GROUP BY (urldims.url) 
-           ORDER BY count DESC 
+             AND facts.window_end <= TIMESTAMP WITHOUT TIME ZONE '$end_date'
+             AND productdims_id = $product_id
+           GROUP BY (urldims.url)
+           ORDER BY count DESC
            LIMIT 100 OFFSET $offset";
 
-    return array($start_date, $end_date, 
+    return array($start_date, $end_date,
       //list of crash objects, which have a url and count
       $this->fetchRows($sql)
       );
@@ -46,14 +46,14 @@ class TopcrashersByUrl_Model extends Model {
     $sql = "/* soc.web tcbyrul getdmns */
            SELECT SUM(facts.count) AS count, urldims.domain
            FROM top_crashes_by_url AS facts
-           JOIN urldims ON urldims.id = facts.urldims_id 
+           JOIN urldims ON urldims.id = facts.urldims_id
            WHERE TIMESTAMP WITHOUT TIME ZONE '$start_date' <= facts.window_end
-             AND facts.window_end <= TIMESTAMP WITHOUT TIME ZONE '$end_date' 
-             AND productdims_id = $product_id 
-           GROUP BY (urldims.domain) 
-           ORDER BY count DESC 
+             AND facts.window_end <= TIMESTAMP WITHOUT TIME ZONE '$end_date'
+             AND productdims_id = $product_id
+           GROUP BY (urldims.domain)
+           ORDER BY count DESC
            LIMIT 100 OFFSET $offset";
-      return array($start_date, $end_date, 
+      return array($start_date, $end_date,
       //list of crash objects, which have a url and count
       $this->fetchRows($sql)
       );
@@ -68,11 +68,11 @@ class TopcrashersByUrl_Model extends Model {
       $product_id = $this->getProductId($product, $version);
       $sqlFromReports =  "/* soc.web tcburl urlsbydomain */
 SELECT SUM(top_crashes_by_url.count) AS count, urldims.url FROM top_crashes_by_url
-JOIN urldims ON urldims.id = top_crashes_by_url.urldims_id 
-WHERE top_crashes_by_url.window_end <= '$end_date' AND 
+JOIN urldims ON urldims.id = top_crashes_by_url.urldims_id
+WHERE top_crashes_by_url.window_end <= '$end_date' AND
       top_crashes_by_url.window_end > '$start_date'
       AND productdims_id = 1 AND urldims.domain = $domain
-GROUP BY (urldims.url) 
+GROUP BY (urldims.url)
 ORDER BY count DESC LIMIT 50 OFFSET $offset";
       $signatures = $this->fetchRows($sqlFromReports);
 
@@ -84,13 +84,13 @@ ORDER BY count DESC LIMIT 50 OFFSET $offset";
       $url = $this->db->escape($tUrl);
       $offset = ($page -1) * 50;
       $aTime = time();
-      
+
       $end_date = date("Y-m-d", $aTime);
       $start_date = date("Y-m-d", $aTime - (60 * 60 * 24 * 14) + 1);
 
       $product_id = $this->getProductId($product, $version);
       $sqlFromReports = "/* soc.web tcburl sigbyurl */
-                         SELECT sum(tcu.count), tcus.signature 
+                         SELECT sum(tcu.count) as count, tcus.signature
                            FROM top_crashes_by_url tcu
                            JOIN urldims u ON tcu.urldims_id = u.id
                            JOIN top_crashes_by_url_signature tcus ON tcu.id = tcus.top_crashes_by_url_id
@@ -118,14 +118,14 @@ ORDER BY count DESC LIMIT 50 OFFSET $offset";
       //$aTime = strtotime('2008-11-20');
       $end_date = date("Y-m-d", $aTime);
       $start_date = date("Y-m-d", $aTime - (60 * 60 * 24 * 14) + 1);
-      $sql = "/* soc.web tbcurl comm4sig */ 
-              SELECT top_crashes_by_url_signature.signature, crashes.comments, crashes,uuid 
-              FROM top_crashes_by_url AS facts 
-              JOIN topcrashurlfactsreports AS crashes ON facts.id = crashes.topcrashurlfacts_id 
-              JOIN urldims ON facts.urldims_id = urldims.id 
+      $sql = "/* soc.web tbcurl comm4sig */
+              SELECT top_crashes_by_url_signature.signature, crashes.comments, crashes,uuid
+              FROM top_crashes_by_url AS facts
+              JOIN topcrashurlfactsreports AS crashes ON facts.id = crashes.topcrashurlfacts_id
+              JOIN urldims ON facts.urldims_id = urldims.id
               JOIN top_crashes_by_url_signature ON facts.id = top_crashes_by_url_signature.top_crashes_by_url_id
-              WHERE '$start_date' <= (facts.window_end - facts.window_size) AND  
-                facts.window_end < '$end_date' AND 
+              WHERE '$start_date' <= (facts.window_end - facts.window_size) AND
+                facts.window_end < '$end_date' AND
                 urldims.url = $url";
       $rows = $this->fetchRows($sql);
       $sigToCommentMap = array();
@@ -133,7 +133,7 @@ ORDER BY count DESC LIMIT 50 OFFSET $offset";
         if( ! array_key_exists( $row->signature, $sigToCommentMap )){
           $sigToCommentMap[$row->signature] = array();
 	}
-        array_push($sigToCommentMap[$row->signature], 
+        array_push($sigToCommentMap[$row->signature],
                                        array('comments' => $row->comments,
                                             'report-id' => $row->uuid));
       }
@@ -160,7 +160,7 @@ ORDER BY count DESC LIMIT 50 OFFSET $offset";
       }
     }
     /**
-     * Returns a list of existing reports based on the 
+     * Returns a list of existing reports based on the
      * product visibility table
      */
     public function listReports($aDate = NULL)
