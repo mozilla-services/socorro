d705f704879a7c81cf892a5eadc88732c2046364
austin.ok <austin.ok@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-09-24 16:47:03 +0000

    Updating to most recent versions with reports
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1354 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/config/dashboard.php
+++ webapp-php/application/config/dashboard.php
@@ -3,15 +3,15 @@ $config['default_product'] = "
 Firefox";
 $config['topcrashbysig_numberofsignatures'] = 3;
 $config['topcrashbysig_featured'] = array(
-    array('product' => 'Firefox',         'version' => '3.6a1pre'),
-    array('product' => 'Thunderbird', 'version' => '3.0b4pre'),
-    array('product' => 'SeaMonkey',  'version' => '2.0a3pre'),
-    array('product' => 'Sunbird',        'version' => '1.0pre'),
-    array('product' => 'Camino',        'version' => '2.0b4pre')
+    array('product' => 'Firefox',     'version' => '3.6a2pre'),
+    array('product' => 'Thunderbird', 'version' => '3.0b3'),
+    array('product' => 'SeaMonkey',   'version' => '2.1a1pre')
 );
 $config['topcrashbyurl_numberofurls'] = 3;
 $config['topcrashbyurl_featured'] = array(
-    array('product' => 'Firefox',         'version' => '3.5')
+    array('product' => 'Firefox',     'version' => '3.5.3'),
+    array('product' => 'Camino',      'version' => '2.0b4'),
+    array('product' => 'SeaMonkey',   'version' => '2.1a1pre')
 );
 $config['mtbf_featured'] = array(
     array('product' => 'Firefox',     'release' => 'development'),
