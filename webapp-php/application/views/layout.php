faef1fb5651faebbcb42a7d381cb612054a95cec
morgamic <morgamic@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-11-11 07:00:52 +0000

    Changing doctype to html5.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1445 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/views/layout.php
+++ webapp-php/application/views/layout.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link href="<?php echo url::base() ?>css/screen.css" rel="stylesheet" type="text/css" media="screen" />
