bc3f5050df61c1166797a5fd5ee8280157eb0e17
austin.ok <austin.ok@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2009-12-03 23:31:07 +0000

    Adding a note about the graphs dual-axis
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1579 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/views/common/list_topcrashers.php
+++ webapp-php/application/views/common/list_topcrashers.php
@@ -3,7 +3,7 @@
     Top <?php echo count($top_crashers) ?> Crashing Signatures
     <span class="start-date"><?= $start ?></span> through
     <span class="end-date"><?= $last_updated ?></span>.
-    The  report covers <span class="percentage" title="<?=$percentTotal?>"><?= number_format($percentTotal * 100, 2)?>%</span> of all <span class="total-num-crashes" title="<?= $total_crashes ?>"><?= number_format($total_crashes) ?></span> crashes during this period.
+	  The  report covers <span class="percentage" title="<?=$percentTotal?>"><?= number_format($percentTotal * 100, 2)?>%</span> of all <span class="total-num-crashes" title="<?= $total_crashes ?>"><?= number_format($total_crashes) ?></span> crashes during this period. Graphs below are dual-axis, having <strong>Count</strong> (Number of Crashes) on the left X axis and <strong>Percent</strong> of total of Crashes on the right X axis. 
 	<div id="duration-nav">
   	  <h3>Other Periods:</h3>
   	  <ul>
