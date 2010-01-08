80d185fade485babdc18a9de6a09bd57839f569d
austin.ok <austin.ok@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2010-01-08 23:16:26 +0000

    reverting debugging controller
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1693 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/controllers/debugging.php
+++ webapp-php/application/controllers/debugging.php
@@ -11,33 +11,6 @@ class Debugging_Controller extends Controller {
      */
     public function index() {
 	$this->auto_render = false;
-	echo "4 try\n";
-	$m = new Common_Model;
-	$rs = $m->fetchRows(/*"SELECT id FROM productdims WHERE product = 'Firefox' AND version = '3.5.6';");*/
-"select
-          CAST(ceil(EXTRACT(EPOCH FROM (window_end - timestamp without time zone '2009-12-25 00:00:00' - interval '8 hours')) / 86400) AS INT) * interval '24 hours' + timestamp without time zone '2009-12-25 00:00:00' as day,
-          case when os.os_name = 'Windows NT' then
-            'Windows'
-          when os.os_name = 'Mac OS X' then
-            'Mac'
-          else
-            os.os_name
-          end as os_name,
-          sum(count)
-      from
-          top_crashes_by_signature tcbs
-               join osdims os on tcbs.osdims_id = os.id
-                  and os.os_name in ('Windows','Mac OS X','Linux','Windows NT')
-      where
-          (timestamp without time zone '2009-12-25 00:00:00' - interval '8 hours') < window_end
-          and window_end <= (timestamp without time zone '2010-01-08 00:00:00' - interval '8 hours')
-          and productdims_id = 60
-      group by
-          day,
-          os_name
-      order by
-          1, 2;");
-
-	echo var_dump($rs);
+	echo "Nothing to see here... move along\n";
     }
 }
