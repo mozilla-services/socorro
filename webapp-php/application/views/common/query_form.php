082f4064f9727eccc7af65d926d6cd66c04b8049
bobmalooga <bobmalooga@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2010-01-14 21:35:15 +0000

    #525783 -  Reduce query form height
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1699 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/views/common/query_form.php
+++ webapp-php/application/views/common/query_form.php
@@ -7,7 +7,7 @@
 <fieldset>
     
 <ul>
-<li><label for="product">Product</label>
+<li><label class="basic" for="product">Product</label>
         <select id="product" name="product" size="4" multiple="multiple">
             <option value="ALL">All</option>
             <?php foreach ($all_products as $row): ?>
@@ -17,7 +17,7 @@
             <?php endforeach ?>
         </select>
 </li>
-<li><label for="version">Version: </label>
+<li><label class="basic" for="version">Version: </label>
         <select id="version" name="version" size="4" multiple="multiple">
             <option value="ALL:ALL">All</option>
             <?php foreach ($all_versions as $row): ?>
@@ -28,7 +28,7 @@
             <?php endforeach ?>
         </select>
 </li>
-<li><label for="platform">Operating System</label>
+<li><label class="basic" for="platform">Operating System</label>
         <select id="platform" name="platform" size="<?php echo count($all_platforms) ?>" multiple="multiple">
             <?php foreach ($all_platforms as $row): ?>
                 <option value="<?php out::H($row->id) ?>" <?php echo in_array($row->id, $params['platform']) ? 'selected="selected"' : '' ?>>
@@ -36,76 +36,71 @@
                 </option>
             <?php endforeach ?>
         </select>
-
 </li>
-<li><a href="#" id="advfiltertoggle" class="not-toggled">Advanced Filters</a>
-    <ul id="advfilter">
-        <li><label for="branch">Branch: </label>
-        <select id="branch" name="branch" size="3" multiple="multiple">
-            <?php foreach ($all_branches as $row): ?>
-                <option value="<?php out::H($row->branch) ?>" <?php echo in_array($row->branch, $params['branch']) ? 'selected="selected"' : '' ?>>
-                    <?php out::H($row->branch) ?>
-                </option>
-            <?php endforeach ?>
-        </select>
-        </li>
-        <li>
-          <ul id="occurs">
-	    <li><label for="date">Occurs before</label>
+<li><label class="basic" for="advfiltertoggle"><a href="#" id="advfiltertoggle" class="not-toggled">Advanced Filters</a></label> 
+
+    <div id="advfilter">
+        <p class="advanced">
+        <label for="branch">Branch</label>
+        <?php foreach ($all_branches as $row): ?>
+            <input type="checkbox" name="branch" value="<?php out::H($row->branch) ?>" <?php echo in_array($row->branch, $params['branch']) ? 'checked' : '' ?>>
+            <?php out::H($row->branch) ?> &nbsp;
+        <?php endforeach ?>
+        </p>
+        
+        <p class="advanced">
+        <label for="date">Before</label>
         <?php echo form::input(
-			       array('size' => '20', 'name'=>'date', 'id'=>'date', 'class'=>'date-pick'), 
+        	       array('size' => '20', 'name'=>'date', 'id'=>'date'), 
             $params['date']
         )?>
-        </li>
-        <li><label for="range_value">Within the last</label>
-	  <div id="within-fields">
+        
+       <label for="range_value">OR Within Last</label>
+          <?php echo form::input(
+              array('size' => '2', 'name'=>'range_value', 'id'=>'range_value'), 
+              $params['range_value']
+          )?>
+          <?php echo form::dropdown(
+              'range_unit',
+              array(
+                  'hours'=>'Hours', 'days'=>'Days', 'weeks'=>'Weeks'
+              ),
+              $params['range_unit']
+          )?>
+        </p>
+        
+        <p class="advanced">
+            <label for="query_type">Stack signature</label>
+            <input type="hidden" name="query_search" value="signature" />
+            <!-- input type="hidden" name="query_search" value="signature" stack  -->
+            <?php echo form::dropdown(
+                'query_type',
+                array(
+                    'exact'      => 'is exactly',
+                    'contains'   => 'contains',
+                    'startswith' => 'starts with'
+                ),
+                $params['query_type']
+            ) ?>	      
+            <?php echo form::input(
+                array('size' => '25', 'name'=>'query', 'id'=>'query'), 
+                trim($params['query'])
+            )?>
+        </p>
+        
+        <p class="advanced">
+        <label for="build_id">Build Id</label>
         <?php echo form::input(
-            array('size' => '2', 'name'=>'range_value', 'id'=>'range_value'), 
-            $params['range_value']
-        )?><?php echo form::dropdown(
-            'range_unit',
-            array(
-                'hours'=>'Hours', 'days'=>'Days', 'weeks'=>'Weeks'
-            ),
-            $params['range_unit']
-        )?>
-	  </div>
-	  </li>
-	  </ul></li>
-	<li>
-	  <ul id="stack">
-	    <li><label for="query_type">Stack Signature</label>
-	  
-          <input type="hidden" name="query_search" value="signature" />
-          <!-- input type="hidden" name="query_search" value="signature" stack  -->
-        <?php echo form::dropdown(
-            'query_type',
-            array(
-                'exact'      => 'is exactly',
-                'contains'   => 'contains',
-                'startswith' => 'starts with'
-            ),
-            $params['query_type']
-        ) ?>	      
-        <?php echo form::input(
-            array('size' => '40', 'name'=>'query', 'id'=>'query'), 
-            trim($params['query'])
-        )?>
-	    </li>
-            <li id="build-id-el"><label for="build_id">Build Id</label>
-<?php echo form::input(
             array('size' => '14', 'name'=>'build_id', 'id'=>'build_id'), 
             trim($params['build_id'])
         )?>
-            </li>
-	  </ul> <!-- /stack -->
-	</li></ul>
-      </li>
-      <li id="gofilter">
-          <button type="submit">Filter Crash Reports</button>
-          <input type="hidden" name="do_query" value="1" />
-      </li>
+        </p>
+    </div>
+            
+    <button type="submit">Filter Crash Reports</button>
+    <input type="hidden" name="do_query" value="1" />
+    </li>
 
     </ul>
     </fieldset>
-  </form>
+</form>
