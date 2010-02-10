288fddbf0084a254bb1cca19a8ebd950c07959e3
TristanBuckner <TristanBuckner@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2010-02-10 04:10:13 +0000

    Added script for the importing of jsonz files into hbase.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1769 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ tools/loadjsonz.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import sys
+import socorro.collector.hbase.hbase as hbase
+import gzip
+
+class JsonzLoader(object):
+  def __init__(self,host,port):
+    self.hbase_connection = hbase.HBaseConnectionForCrashReports(host,port)
+
+  def close(self):
+    self.hbase_connection.close()
+
+  def load_from_file(self, uuid, path):
+    jsonz_file = gzip.open(path, 'rb')
+    json_string = jsonz_file.read()
+    jsonz_file.close()
+    self.hbase_connection.create_ooid_from_jsonz(uuid,json_string)
+
+if __name__=="__main__":
+  if len(sys.argv) != 3:
+    print "Usage: loadjsonz.py <text file containing uuids and jsonz paths> <hbase host:port>\nText file should be uuid and file path seperated by a tab"
+    sys.exit(1)
+  input_file_path = sys.argv[1]
+  host, port = sys.argv[2].split(':')
+  loader = JsonzLoader(host,port)
+  input_file = open(path,'rb')
+  i = 0
+  for line in input_file:
+    uuid, path = line.strip().split('\t')
+    loader.load_from_file(uuid, path)
+    i += 1
+    if i % 1000 == 0:
+      print i,'reports loaded'
+  loader.close()
+  input_file.close()
+  
+    
