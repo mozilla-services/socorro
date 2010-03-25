3afa80a5d4353dea9d69ba0f07c93c60cfb3e5ea
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2010-03-25 19:44:09 +0000

    merge of hbase resubmit stuff: Bug 548362
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1889 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ scripts/startHbaseResubmit.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+import sys
+import logging
+import logging.handlers
+
+try:
+  import config.hbaseresubmitconfig as hbrconf
+except ImportError:
+  import hbaseresubmitconfig as  hbrconf
+
+import socorro.lib.ConfigurationManager as configurationManager
+import socorro.cron.hbaseResubmit as hbr
+
+try:
+  conf = configurationManager.newConfiguration(configurationModule=hbrconf, applicationName="HBase Resubmit")
+except configurationManager.NotAnOptionError, x:
+  print >>sys.stderr, x
+  print >>sys.stderr, "for usage, try --help"
+  sys.exit()
+
+logger = logging.getLogger("hbaseresubmit")
+logger.setLevel(logging.DEBUG)
+
+stderrLog = logging.StreamHandler()
+stderrLog.setLevel(conf.stderrErrorLoggingLevel)
+stderrLogFormatter = logging.Formatter(conf.stderrLineFormatString)
+stderrLog.setFormatter(stderrLogFormatter)
+logger.addHandler(stderrLog)
+
+rotatingFileLog = logging.handlers.RotatingFileHandler(conf.logFilePathname, "a", conf.logFileMaximumSize, conf.logFileMaximumBackupHistory)
+rotatingFileLog.setLevel(conf.logFileErrorLoggingLevel)
+rotatingFileLogFormatter = logging.Formatter(conf.logFileLineFormatString)
+rotatingFileLog.setFormatter(rotatingFileLogFormatter)
+logger.addHandler(rotatingFileLog)
+
+logger.info("current configuration\n%s", str(conf))
+
+conf.logger = logger
+
+try:
+  hbr.resubmit(conf)
+finally:
+  logger.info("done.")
+  rotatingFileLog.flush()
+  rotatingFileLog.close()
+
+
+
