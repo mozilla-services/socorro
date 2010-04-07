8813e47dbd043a62b8494da25d91179a123c7d7d
twobraids <twobraids@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2010-04-07 00:59:09 +0000

    Bug 555729 - tests for the new version tools module
    git-svn-id: https://socorro.googlecode.com/svn/trunk@1928 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ socorro/unittest/lib/testVerTools.py
@@ -0,0 +1,44 @@
+import socorro.lib.ver_tools as vtl
+
+phs = vtl._padding_high_string
+pl  = vtl._padding_list
+
+tests = [('3',            [3, phs, 0, phs] + pl * 3, '3'),
+         ('3.',           [3, phs, 0, phs] + pl * 3, '3'),
+         ('3.0',          [3, phs, 0, phs] + pl * 3, '3'),
+         ('3.0.0',        [3, phs, 0, phs] + pl * 3, '3'),
+         ('3.5',          [3, phs, 0, phs,
+                           5, phs, 0, phs] + pl * 2, '3.5'),
+         ('3.5pre',       [3, phs, 0, phs,
+                           5, 'pre', 0, phs] + pl * 2, '3.5pre'),
+         ('3.5b3',        [3, phs, 0, phs,
+                           5, 'b', 3, phs] + pl * 2, '3.5b3'),
+
+         ('3.6.4plugin3', [3, phs, 0, phs,
+                           6, phs, 0, phs,
+                           4, 'plugin', 3, phs] + pl, '3.6.4plugin3'),
+        ]
+
+def testNormalize():
+    for ver, expected, ver2 in tests:
+        got = vtl.normalize(ver)
+        assert got == expected, "expected %s, but got %s" % (expected, got)
+
+def testDenomalize():
+    for ver, norm, expected in tests:
+        got = vtl.denormalize(norm)
+        assert got == expected, "expected %s, but got %s" % (expected, got)
+
+def testCompare():
+    got = vtl.compare('3', '3.1')
+    assert got == -1, "expected %s, but got %s" % (-1, got)
+    got = vtl.compare('3', '3.0')
+    assert got == 0, "expected %s, but got %s" % (0, got)
+    got = vtl.compare('3', '3.0pre')
+    assert got == 1, "expected %s, but got %s" % (1, got)
+    got = vtl.compare('3.5b2', '3.5b1')
+    assert got == 1, "expected %s, but got %s" % (1, got)
+    got = vtl.compare('3.5', '3.5b1')
+    assert got == 1, "expected %s, but got %s" % (1, got)
+    got = vtl.compare('3.5.1', '3.5.1b3')
+    assert got == 1, "expected %s, but got %s" % (1, got)
