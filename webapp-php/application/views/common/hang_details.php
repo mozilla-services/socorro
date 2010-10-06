37bed1cb55efeac42beadd49fda8faa86b08fc2d
ryansnyder.me <ryansnyder.me@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2010-10-06 22:40:44 +0000

    #602118 - Add alt/title tags for Hang / OOPP icons
    git-svn-id: https://socorro.googlecode.com/svn/trunk@2562 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/views/common/hang_details.php
+++ webapp-php/application/views/common/hang_details.php
@@ -12,15 +12,15 @@ if (array_key_exists('link', $crash)) {
     $linked = true;
 }
 if ($crash['is_hang'] == true) { ?>
-    <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="hang-pair-btn"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/stop16x16.png')?>" width="16" height="16" alt="Hanged Crash" class="hang" /><?php if ($linked) { echo '</a>'; } ?>
+    <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="hang-pair-btn" title="Hanged Crash"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/stop16x16.png')?>" width="16" height="16" alt="Hanged Crash" title="Hanged Crash" class="hang" /><?php if ($linked) { echo '</a>'; } ?>
     <?php if ($crash['is_plugin'] == true) {?>
-	      <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="hang-pair-btn"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/brick16x16.png')?>" width="16" height="16" alt="Plugin Crash" class="plugin" /><?php if ($linked) { echo '</a>'; } ?>
+	      <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="hang-pair-btn" title="Plugin Crash"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/brick16x16.png')?>" width="16" height="16" alt="Plugin Crash" title="Plugin Crash" class="plugin" /><?php if ($linked) { echo '</a>'; } ?>
      <?php } else { ?>
-	      <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="hang-pair-btn"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/application16x16.png')?>" width="16" height="16" alt="Browser Crash" class="browser" /><?php if ($linked) { echo '</a>'; } ?>
+	      <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="hang-pair-btn" title="Browser Crash"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/application16x16.png')?>" width="16" height="16" alt="Browser Crash" title="Browser Crash" class="browser" /><?php if ($linked) { echo '</a>'; } ?>
     <?php }
 } else { ?>
     <?php if ($crash['is_plugin'] == true) {?>
-              <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="plugin-btn"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/brick16x16.png')?>" width="16" height="16" alt="Plugin Crash" class="plugin" /><?php if ($linked) { echo '</a>'; } ?>
+              <?php if ($linked) { ?><a href="<?= $crash['link'] ?>" class="plugin-btn" title="Plugin Crash"><?php } ?><img src="<?= url::site('/img/3rdparty/fatcow/brick16x16.png')?>" width="16" height="16" alt="Plugin Crash" title="Plugin Crash" class="plugin" class="plugin" /><?php if ($linked) { echo '</a>'; } ?>
      <?php } ?>
 <?php }
 ?>
