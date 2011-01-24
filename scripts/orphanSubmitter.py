6dfbb03d6ce22bd52c6ec438fc10622e40e83b7c
twobraids@gmail.com <twobraids@gmail.com@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-01-24 18:58:19 +0000

    new app that walks a JsonDumpStore name branches looking for orphan files.  Orphans are json/dump pairs that have no symbolic inks to the date branch.  Once it finds them, it tries to pass them on to HBase after first check to see if they are already there.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@2894 efcc4bdc-162a-0410-a8af-5b4b46c5376d

---
+++ scripts/orphanSubmitter.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import sys
+import logging
+import logging.handlers
+
+import config.crashmoverconfig as cmconf
+
+import socorro.lib.ConfigurationManager as configurationManager
+import socorro.storage.orphans as smover
+import socorro.lib.util as sutil
+
+try:
+  config = configurationManager.newConfiguration(configurationModule=cmconf,
+                                                 applicationName="Orphan Submitter")
+except configurationManager.NotAnOptionError, x:
+  print >>sys.stderr, x
+  print >>sys.stderr, "for usage, try --help"
+  sys.exit()
+
+logger = logging.getLogger("orphanSubmitter")
+logger.setLevel(logging.DEBUG)
+
+sutil.setupLoggingHandlers(logger, config)
+sutil.echoConfig(logger, config)
+
+config.logger = logger
+
+try:
+  smover.move(config)
+finally:
+  logger.info("done.")
+
