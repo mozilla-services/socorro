0a62f087fbce4c70572851df3b1836b9c76af925
rhelmer <rhelmer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-03-21 18:54:42 +0000

    bug 643504 - about:crashes linked, but forbidden by security. Tell user to type it in the address bar
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3013 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/views/about/throttling.php
+++ webapp-php/application/views/about/throttling.php
@@ -10,7 +10,7 @@
 <div class="panel">
     <div class="body notitle">
 
-    <p>You're seeing this page because the crash report you selected was held back from submission. For statistical purposes, we don't need every crash report and sometimes we decline the submission of a crash report as a form of <a href="http://en.wikipedia.org/wiki/Throttling_process_%28computing%29">throttling</a>. If you'd like to see your crash report, simply click on its link in <a href="about:crashes">about:crashes</a> and your report will be automatically submitted to our server where we'll give it priority processing.</p>
+    <p>You're seeing this page because the crash report you selected was held back from submission. For statistical purposes, we don't need every crash report and sometimes we decline the submission of a crash report as a form of <a href="http://en.wikipedia.org/wiki/Throttling_process_%28computing%29">throttling</a>. If you'd like to see your crash report, type about:crashes into your location bar and your report will be automatically submitted to our server where we'll give it priority processing.</p>
     
     <p>Thank you for reporting your crashes.</p>
     
