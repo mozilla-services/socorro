0bb676dd14aa6ee7298778df1bd922f432c8d506
brandonsavage020@gmail.com <brandonsavage020@gmail.com@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-04-19 14:04:52 +0000

    Bug 627815 - Changing the default sort for the reports page to show the newest build first.
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3066 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/js/socorro/report_list.js
+++ webapp-php/js/socorro/report_list.js
@@ -69,7 +69,7 @@ $(document).ready(function () {
             8: { sorter: "hexToInt" },  // Address
             10: { sorter: "digit" }      // Uptime
         }, 
-        sortList : [[12,1]]
+        sortList : [[4,1]]
     });
     
     $('#report-list-nav').tabs({selected: 2}).show();
