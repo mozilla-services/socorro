5bc59f15d41f213c2868a822b9ffb72b7e4e3fd2
rhelmer <rhelmer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-04-20 22:05:25 +0000

    bug 651582 - add backfill support to signatures cron
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3075 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/cron/signatures.py
+++ socorro/cron/signatures.py
@@ -18,13 +18,24 @@ import socorro.lib.util as util
 
 def update_signatures(config):
   databaseConnectionPool = psy.DatabaseConnectionPool(config.databaseHost, config.databaseName, config.databaseUserName, config.databasePassword, logger)
-  sql = """
-    --- time, hours_back, hours_window
-    SELECT update_signature_matviews('%s', 3, 2)
-  """
   try:
     databaseConnection, databaseCursor = databaseConnectionPool.connectionCursorPair()
-    databaseCursor.execute(sql % (datetime.now()))
-    databaseConnection.commit()
+    databaseCursor.execute("""
+    SELECT max(first_report) FROM signature_build
+    """);
+    last_run = databaseCursor.fetchone()[0]
+    now = datetime.now()
+    delta = now - last_run
+    total_seconds = delta.days * 86400 + delta.seconds
+    total_hours = total_seconds / 3600
+    logger.info("total_hours: %s" % total_hours)
+    for hour in xrange(total_hours):
+      hour = int(hour) + 1
+      timestamp = now - timedelta(hours=hour)
+      databaseCursor.execute("""
+        --- time, hours_back, hours_window
+        SELECT update_signature_matviews('%s', 3, 2)
+      """ % (timestamp))
+      databaseConnection.commit()
   finally:
     databaseConnectionPool.cleanup()
