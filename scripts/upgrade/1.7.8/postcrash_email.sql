fa878e0cb39dcebb1053362dcd7b8ff3f4540071
rhelmer <rhelmer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-05-26 22:45:38 +0000

    bug 612596 - remove accidental second copy
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3179 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- scripts/upgrade/1.7.8/postcrash_email.sql
+++ scripts/upgrade/1.7.8/postcrash_email.sql
@@ -5,10 +5,3 @@ BEGIN;
 ALTER TABLE email_contacts ADD COLUMN ooid TEXT;
 ALTER TABLE email_contacts ADD COLUMN crash_date TIMESTAMP with time zone;
 COMMIT;
--- bug 612596
-\set ON_ERROR_STOP true
-
-BEGIN;
-ALTER TABLE email_contacts ADD COLUMN ooid TEXT;
-ALTER TABLE email_contacts ADD COLUMN crash_date TIMESTAMP with time zone;
-COMMIT;
