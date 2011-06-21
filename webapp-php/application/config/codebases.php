9954b8da4e5aa7427870d429e529ec1e09b6ac33
rhelmer <rhelmer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-06-21 19:10:36 +0000

    bug 649418 - mapping for beta and aurora
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3231 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/config/codebases.php
+++ webapp-php/application/config/codebases.php
@@ -30,6 +30,10 @@ $config['vcsMappings'] = array(
 	    'http://hg.mozilla.org/releases/comm-1.9.1/annotate/%(revision)s/%(file)s#l%(line)s',
         'hg.mozilla.org/releases/comm-1.9.2' =>
             'http://hg.mozilla.org/releases/comm-1.9.2/annotate/%(revision)s/%(file)s#l%(line)s'
+        'hg.mozilla.org/mozilla-aurora' => 
+            'http://hg.mozilla.org/mozilla-aurora/annotate/%(revision)s/%(file)s#l%(line)s'
+        'hg.mozilla.org/mozilla-beta' => 
+            'http://hg.mozilla.org/mozilla-beta/annotate/%(revision)s/%(file)s#l%(line)s'
 ));
 $config['bugTrackingUrl'] = 'https://bugzilla.mozilla.org/show_bug.cgi?id=';
 ?>
