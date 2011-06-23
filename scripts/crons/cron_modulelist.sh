5d6c350a9f3380221de071698e37a40becd6d4a0
rhelmer <rhelmer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-06-23 00:15:15 +0000

    bug 598908 - redirect stderr too, gets printed to during normal operation
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3237 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- scripts/crons/cron_modulelist.sh
+++ scripts/crons/cron_modulelist.sh
@@ -5,7 +5,7 @@
 NAME=`basename $0 .sh`
 lock $NAME
 DATE=`date -d 'yesterday' +%Y%m%d`
-${SOCORRO_DIR}/analysis/modulelist.sh Firefox "Windows NT" $DATE >> /var/log/socorro/cron_modulelist.log
+${SOCORRO_DIR}/analysis/modulelist.sh Firefox "Windows NT" $DATE >> /var/log/socorro/cron_modulelist.log 2>&1
 EXIT_CODE=$?
 unlock $NAME
 
