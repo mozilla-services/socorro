58a347dac9ff8f77635b6a1dc7e1dafd72b548d5
rhelmer <rhelmer@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-08-05 20:57:21 +0000

    bug 676612 - adjust for new product_info view, and allow optional arg to return only featured versions
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3342 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/services/currentVersions.py
+++ socorro/services/currentVersions.py
@@ -17,24 +17,40 @@ class CurrentVersions(webapi.JsonServiceBase):
     logger.debug('CurrentVersions __init__')
 
   # curl 'http://localhost:8085/201106/current/versions'
-  uri = '/201106/current/versions'
+  uri = '/201106/current/versions/(.*)'
 
   def get(self, *args):
+
     connection = self.database.connection()
     cursor = connection.cursor()
+
+    featured_only = False
+    if 'featured' in args:
+      featured_only = True
 
     # use the last date that we have data for as the end
     currentVersions = """
                       /* socorro.services.CurrentVersions curentVersions */
-                     SELECT product_name, version_string
-                     FROM product_selector"""
+                     SELECT product_name, version_string, is_featured,
+                            start_date, end_date
+                     FROM product_info"""
+
+    if featured_only:
+      currentVersions += """ WHERE is_featured"""
 
     cursor.execute(currentVersions)
 
     defaultdict_factory_with_list = ft.partial(col.defaultdict, list)
 
     result = col.defaultdict(defaultdict_factory_with_list)
-    for product, version in cursor.fetchall():
-      result[product]['version'].append(version)
+    for product, version, featured, start, end in cursor.fetchall():
+      releases = { 'version': version,
+                   'start_date': str(start),
+                   'end_date': str(end) }
+
+      if not featured_only:
+        releases['featured'] = featured
+
+      result[product]['releases'].append(releases)
 
     return {'currentversions': result}
