e5ac32047b016cfea96af003740c97a0f12b89d6
Chris.Lonnen <Chris.Lonnen@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-08-14 19:34:08 +0000

    Bug 677974 - modern aduHistory fix to return something acceptably close to the classic implementation
    git-svn-id: https://socorro.googlecode.com/svn/branches/releases/2.2/@3437 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/services/sighistory/modern.py
+++ socorro/services/sighistory/modern.py
@@ -17,7 +17,7 @@ class SignatureHistoryModern(webapi.JsonServiceBase):
   uri = ('/200911/topcrash/sig/trend/history/p/(.*)/v/(.*)/sig/(.*)/end/(.*)/'
         'duration/(.*)/steps/(.*)')
 
-  def fetchSignatureHistory (self, parameters):
+  def fetchSigHistory(self, parameters):
     if parameters['signature'] == '##null##':
       signatureCriterionPhrase = '          and signature is null'
     else:
@@ -25,42 +25,57 @@ class SignatureHistoryModern(webapi.JsonServiceBase):
     if parameters['signature'] == '##empty##':
       parameters['signature'] = ''
     sql = """
+      WITH hist as (
       select
-          CAST(ceil(EXTRACT(EPOCH FROM (window_end - %%(startDate)s)) / %%(stepSize)s) AS INT) as bucket_number,
-          sum(count)
+          report_date,
+          report_count
       from
-          tcbs
+          tcbs join signatures using (signature_id)
+               join product_versions using (product_version_id)
       where
-          %%(startDate)s < window_end
-          and window_end <= %%(endDate)s
-          and productdims_id = %%(productdims_id)s
+          report_date between %%(startDate)s and %%(endDate)s
+          and product_name = %%(product)s
+          and version_string = %%(version)s
           %s
       group by
-          bucket_number
-      order by
-          1""" % signatureCriterionPhrase
+          report_date, report_count
+      order by 1),
+      scaling_window AS (
+          select 
+              hist.*,
+              sum(report_count) over () as total_crashes
+          from 
+              hist
+      )
+      SELECT 
+          report_date,
+          report_count,
+          report_count / total_crashes::float as percent_of_total
+      from scaling_window
+      order by report_date DESC
+    """ % signatureCriterionPhrase
     #logger.debug('%s', self.connection.cursor().mogrify(sql, parameters))
-    return dict(((bucket, count) for bucket, count in db.execute(self.connection.cursor(), sql, parameters)))
+    return db.execute(self.connection.cursor(), sql, parameters)
+
 
   def signatureHistory (self, parameters, connection):
     self.connection = connection
     #logger.debug('signatureHistory %s  %s', parameters, self.connection)
     parameters.startDate = parameters.endDate - parameters.duration
     parameters.stepSize = dtutil.timeDeltaToSeconds(parameters.duration / parameters.steps)
-    signatureHistory = self.fetchSignatureHistory(parameters)
     listOfEntries = []
-    for bucket, total in self.fetchTotalsForRange(parameters):
+    for date, count, percent in self.fetchSigHistory(parameters):
       #logger.debug('signatureHistory fetchTotalsForRange %s  %s', bucket, total)
-      d = { 'date': str(dt.timedelta(seconds=parameters.stepSize * bucket) + parameters.startDate),
-            'count': signatureHistory.setdefault(bucket, 0),
-            'percentOfTotal': signatureHistory.setdefault(bucket, 0) / float(total),
+      d = { 'date': date.isoformat(),
+            'count': count,
+            'percentOfTotal': percent,
           }
       listOfEntries.append(d)
       #logger.debug(listOfEntries)
     result = { 'signatureHistory': listOfEntries,
                'signature': parameters.signature,
-               'start_date': str(parameters.startDate),
-               'end_date': str(parameters.endDate),
+               'start_date': parameters.startDate.isoformat(),
+               'end_date': parameters.endDate.isoformat(),
              }
     #logger.debug(result)
     return result
