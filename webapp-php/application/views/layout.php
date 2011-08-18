d38c476ef52552ec7a6d3011d52a2305c177758b
Chris.Lonnen <Chris.Lonnen@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-08-18 20:15:10 +0000

    bug 674380 - begone urchin
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3465 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- webapp-php/application/views/layout.php
+++ webapp-php/application/views/layout.php
@@ -7,7 +7,6 @@
     <?php 
     	echo html::script(
     	    array(
-    	        'js/__utm.js',
                 'js/jquery/jquery-1.3.2.min.js',
                 'js/jquery/plugins/jquery.cookies.2.2.0.js',
                 'js/socorro/nav.js?v=1.7.6'
