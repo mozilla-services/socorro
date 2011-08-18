ebb7c4a08e0af37879aef9fe615203cd6a18d4a1
adrian.gaudebert@gmail.com <adrian.gaudebert@gmail.com@efcc4bdc-162a-0410-a8af-5b4b46c5376d>
2011-08-18 21:28:37 +0000

    Bug 678408 - [PEP8] Rewrite the webapi module
    git-svn-id: https://socorro.googlecode.com/svn/trunk@3466 efcc4bdc-162a-0410-a8af-5b4b46c5376d

--- socorro/webapi/classPartial.py
+++ socorro/webapi/classPartial.py
@@ -1,10 +1,14 @@
-#=================================================================================================================
-def classWithPartialInit (C, *args, **kwargs):
-  """This function creates a new class W as a subclass of C.  W's __init__ is
-  effectively a partial function of C's __init__.  This allows a class to be
-  embued with its initialization parameters long before actual instantiation."""
-  class W(C):
-    def __init__(self):
-      super(W,self).__init__(*args, **kwargs)
-  W.__name__ = C.__name__
-  return W
+def classWithPartialInit(C, *args, **kwargs):
+    """
+    Return a new subclass of C.
+
+    This function creates a new class W as a subclass of C.  W's __init__ is
+    effectively a partial function of C's __init__.  This allows a class to be
+    embued with its initialization parameters long before actual instantiation.
+
+    """
+    class W(C):
+        def __init__(self):
+            super(W, self).__init__(*args, **kwargs)
+    W.__name__ = C.__name__
+    return W
