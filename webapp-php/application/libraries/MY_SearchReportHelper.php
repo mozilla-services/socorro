c183b4d831e8b67398abfd676d6c018412388948
Adrian Gaudebert <adrian@gaudebert.fr>
2011-09-08 20:05:22 -0700

    Applied patch from bug 669699.

--- webapp-php/application/libraries/MY_SearchReportHelper.php
+++ webapp-php/application/libraries/MY_SearchReportHelper.php
@@ -45,7 +45,7 @@ class SearchReportHelper{
             'version'      => array(),
             'platform'     => array(),
             'build_id'     => '',
-            
+
             'query_search' => 'signature',
             'query_type'   => 'contains',
             'query'        => '',
@@ -54,14 +54,16 @@ class SearchReportHelper{
             'date'         => '',
             'range_value'  => '1',
             'range_unit'   => 'weeks',
-            
+
             'hang_type'    => 'any',
             'process_type' => 'any',
             'plugin_field' => '',
             'plugin_query_type' => '',
             'plugin_query' => '',
-            
-            'do_query'     => FALSE
+
+            'do_query'     => FALSE,
+
+            'force_api_impl' => ''
         );
     }
 
@@ -69,7 +71,7 @@ class SearchReportHelper{
      * Normalizes the params for a search against the reports table.
      * This includes limiting the scope of the search by changing
      * the param values.
-     * 
+     *
      * @param array An array of $_GET parameters
      */
     public function normalizeParams( &$params ){
@@ -79,24 +81,24 @@ class SearchReportHelper{
     }
 
     /**
-     * Normalizes the range_value and range_unit parameters which 
+     * Normalizes the range_value and range_unit parameters which
      * handle date ranges for the query.
-     * 
-     * @bizrule Only a user considered an admin may perform a query with 
+     *
+     * @bizrule Only a user considered an admin may perform a query with
      *          a date range greater than 31 days.
      * @bizrule Admins must select a product in order to perform a query with
      *          a date range greater than 31 days.
      * @bizrule Queries may not be greater than 120 days in length; once
-     *          we feel comfortable with performance this may be increased. 
+     *          we feel comfortable with performance this may be increased.
      * @param array An array of $_GET parameters
      */
     public function normalizeDateUnitAndValue(&$params){
-		$range_defaults = Kohana::config('application.query_range_defaults');
-		$range_key = (isset($params['admin']) && $params['admin']) ? 'admin' : 'user';
+        $range_defaults = Kohana::config('application.query_range_defaults');
+        $range_key = (isset($params['admin']) && $params['admin']) ? 'admin' : 'user';
 
-		$range_default_value = $range_defaults[$range_key]['range_default_value'];
-		$range_default_unit = $range_defaults[$range_key]['range_default_unit'];
-		$range_limit_value_in_days = $range_defaults[$range_key]['range_limit_value_in_days'];
+        $range_default_value = $range_defaults[$range_key]['range_default_value'];
+        $range_default_unit = $range_defaults[$range_key]['range_default_unit'];
+        $range_limit_value_in_days = $range_defaults[$range_key]['range_limit_value_in_days'];
 
         $valueInDays = 0;
         if ($params['range_unit'] == 'hours') {
@@ -106,45 +108,45 @@ class SearchReportHelper{
         } elseif ($params['range_unit'] == 'weeks') {
             $valueInDays = intval( $params['range_value'] ) * 7;
         } elseif ($params['range_unit'] == 'months') {
-            $valueInDays = intval( $params['range_value'] ) * 28; // low end of a month            
-        } 
+            $valueInDays = intval( $params['range_value'] ) * 28; // low end of a month
+        }
 
         if ($valueInDays <= 0) {
             $params['range_value'] = $range_default_value;
             $params['range_unit'] = $range_default_unit;
         }
         elseif ($valueInDays > $range_limit_value_in_days && !$params['admin']) {
-			$error_message = 'The maximum query date range you can perform is ' . $range_limit_value_in_days . ' days. Admins may log in to increase query date range limits.';
-        } 
-        elseif ($valueInDays > $range_limit_value_in_days && $params['admin']) {
-			$error_message = 'The maximum query date range you can perform is ' . $range_limit_value_in_days . ' days.';  
+            $error_message = 'The maximum query date range you can perform is ' . $range_limit_value_in_days . ' days. Admins may log in to increase query date range limits.';
         }
-		elseif (
-            $valueInDays > $range_defaults['user']['range_limit_value_in_days'] && 
-            $params['admin'] && 
+        elseif ($valueInDays > $range_limit_value_in_days && $params['admin']) {
+            $error_message = 'The maximum query date range you can perform is ' . $range_limit_value_in_days . ' days.';
+        }
+        elseif (
+            $valueInDays > $range_defaults['user']['range_limit_value_in_days'] &&
+            $params['admin'] &&
             (!isset($params['product']) || (isset($params['product']) && empty($params['product'])))
         ) {
-			$error_message = 'You must select a product in order to perform a query with a date range greater than ' . $range_defaults['user']['range_limit_value_in_days'] . ' days.';
-		}
+            $error_message = 'You must select a product in order to perform a query with a date range greater than ' . $range_defaults['user']['range_limit_value_in_days'] . ' days.';
+        }
 
-		if (isset($error_message) && !empty($error_message)) {
-			$error_message .= ' Query results have been narrowed to the default range of ' . $range_default_value . ' ' . $range_default_unit . '.';
+        if (isset($error_message) && !empty($error_message)) {
+            $error_message .= ' Query results have been narrowed to the default range of ' . $range_default_value . ' ' . $range_default_unit . '.';
             $params['range_value'] = $range_default_value;
             $params['range_unit'] = $range_default_unit;
-            client::messageSend($error_message, E_USER_WARNING);			
-		}
+            client::messageSend($error_message, E_USER_WARNING);
+        }
     }
 
     /**
      * Normalizes the product, version and build_id values for the query.
-     * 
+     *
      * @param array An array of $_GET parameters
      */
     public function normalizeProduct(&$params){
         $productEmpty = $this->empty_param($params['product']);
         $versionEmpty = $this->empty_param($params['version']);
         $buildIdEmpty = $this->empty_param($params['build_id']);
-        
+
         if ($productEmpty && $versionEmpty && $buildIdEmpty) {
             $params['product'] = array();
         }
@@ -155,7 +157,7 @@ class SearchReportHelper{
 
     /**
      * Handle empty parameters.
-     * 
+     *
      * @param array An array of $_GET parameters
      */
     function empty_param($anArray){
@@ -172,10 +174,10 @@ class SearchReportHelper{
 
     /**
      * Returns a string YYYY-MM-DD for the current time.
-     * Method mainly exists to decouple system time from 
+     * Method mainly exists to decouple system time from
      * code.
      *
-     * @return string YYYY-MM-DD 
+     * @return string YYYY-MM-DD
      */
     function currentDate(){
         if (isset($this->currentDateForTest)) {
@@ -192,5 +194,5 @@ class SearchReportHelper{
          $this->currentDateForTest = $aDate;
     }
 
-	/* */
+    /* */
 }
