f490e6cc96faf2b262a7e51da0644cc04e9e68f9
Stephen Donner <stephen.donner@gmail.com>
2011-09-23 14:48:23 -0700

    Dropping the number of paginated results-per-page from 500 -> 250.  Totally untested, but this should work?

--- webapp-php/application/config/search.php
+++ webapp-php/application/config/search.php
@@ -2,7 +2,7 @@
 /**
  * Configuration parameters for customizing the behavior of search
  */
-$config['number_report_list'] = 500;
+$config['number_report_list'] = 250;
 
 /**
  * Configuration parameters for customizing the behavior of advanced search
