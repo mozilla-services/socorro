caf56897a003b8a54a2f312ab291e7ba932dc167
Josh Berkus <josh@agliodbs.com>
2011-10-12 20:19:45 -0700

    Some cleanup.  Completed upgrade script and backfill scripts.  Ready for testing on stage.

---
+++ sql/upgrade/2.3.1/backfill_matviews.sql
@@ -0,0 +1,58 @@
+\set ON_ERROR_STOP 1
+
+-- function
+
+CREATE OR REPLACE FUNCTION backfill_matviews (
+	firstday date,
+	forproduct text default '',
+	lastday date default NULL )
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+AS $f$
+DECLARE thisday DATE := firstday;
+BEGIN
+-- this producedure is meant to be called manually
+-- by administrators in order to clear and backfill
+-- the various matviews in order to recalculate old
+-- data which was erroneous.
+-- it requires a start date, and optionally takes
+-- a product name and an end date
+
+-- set optional end date
+lastday := coalesce(lastday, current_date );
+
+-- check parameters
+IF firstday > current_date OR lastday > current_date or firstday > lastday THEN
+	RAISE EXCEPTION 'date parameter error: cannot backfill into the future';
+END IF;
+
+-- fill in products
+PERFORM update_product_versions();
+
+IF forproduct <> '' THEN
+	PERFORM 1 FROM products WHERE product_name = forproduct;
+	IF NOT FOUND THEN
+		RAISE EXCEPTION 'product % does not exist or is misspelled',forproduct;
+	END IF;
+END IF;
+
+-- loop through the days, backfilling one at a time
+WHILE thisday <= lastday LOOP
+	RAISE INFO 'backfilling for %',thisday;
+	RAISE INFO 'adu';
+	PERFORM backfill_adu(thisday, forproduct);
+	RAISE INFO 'tcbs';
+	PERFORM backfill_tcbs(thisday, forproduct);
+	DROP TABLE IF EXISTS new_tcbs;
+	RAISE INFO 'daily crashes';
+	PERFORM backfill_daily_crashes(thisday, forproduct);
+	RAISE INFO 'signatures';
+	PERFORM update_signatures(thisday, FALSE);
+	DROP TABLE IF EXISTS new_signatures;
+
+	thisday := thisday + 1;
+
+END LOOP;
+
+RETURN true;
+END; $f$;
--- sql/upgrade/2.3.1/upgrade.sh
+++ sql/upgrade/2.3.1/upgrade.sh
@@ -9,4 +9,35 @@ CURDIR=$(dirname $0)
 echo 'alter the releases_raw table so that it can accept nightlies'
 psql -f $(CURDIR)/alter_releases_raw.sql breakpad
 
+echo 'update products now with aurora and nightlies'
+psql -f $(CURDIR)/update_products.sql breakpad
+
+echo 'create table if not exists function'
+psql -f $(CURDIR)/create_table_if_not_exists.sql breakpad
+
+echo 'new tables and minor functions for reports_clean'
+psql -f $(CURDIR)/reports_clean_new_tables.sql breakpad
+
+echo 'lookup list populating function'
+psql -f $(CURDIR)/insert_into_lookup_lists.sql breakpad
+
+echo 'adjust reports_duplicates to make it faster'
+psql -f $(CURDIR)/update_reports_duplicates.sql breakpad
+
+echo 'os_versions function for reports_clean'
+psql -f $(CURDIR)/update_os_versions_new_reports.sql breakpad
+
+echo 'partition manager for reports_clean'
+psql -f $(CURDIR)/reports_clean_weekly.sql breakpad
+
+echo 'update function for reports_clean'
+psql -f $(CURDIR)/alter_releases_raw.sql breakpad
+
+echo 'fix gap in backfill_matviews'
+psql -f $(CURDIR)/alter_releases_raw.sql breakpad
+
+echo 'now backfill data back to 9/1.  This will take hours'
+psql -f $(CURDIR)/backfill_everything.sql breakpad
+
+
 exit 0
