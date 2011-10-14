85bba30eff38f98aa2d63252025426d8400f1472
Adrian Gaudebert <adrian@gaudebert.fr>
2011-10-14 23:07:30 +0200

    Refactored search service to inherit and use the service base class.

--- socorro/middleware/search_service.py
+++ socorro/middleware/search_service.py
@@ -1,13 +1,13 @@
 import logging
 
-import socorro.webapi.webapiService as webapi
+import socorro.middleware.service
 import socorro.search.elasticsearch as es
 import socorro.search.postgresql as pg
 
 logger = logging.getLogger("webapi")
 
 
-class Search(webapi.JsonServiceBase):
+class Search(service.DataAPIService):
 
     """
     Search API interface
@@ -16,54 +16,25 @@ class Search(webapi.JsonServiceBase):
     call the API implementation to execute the query.
 
     """
+    service_name = "search"
+    uri = "/201105/search/([^/.]*)/(.*)"
 
     def __init__(self, config):
         """
         Constructor
         """
         super(Search, self).__init__(config)
-        self.api_impl = config.searchImplClass(config)
-        logger.debug('Search __init__')
-
-    uri = '/201105/search/([^/.]*)/(.*)'
+        logger.debug('Search service __init__')
 
     def get(self, *args):
         """
         Called when a get HTTP request is executed to /search
         """
         # Parse parameters
-        params = self._parse_query_string(args[1])
-        types = args[0]
+        params = self.parse_query_string(args[1])
+        params["type"] = args[0]
 
-        # If one wants to choose the api to use, then force it
-        forced_api_impl = None
-        if "force_api_impl" in params:
-            if params["force_api_impl"] == "es":
-                forced_api_impl = es.ElasticSearchAPI(self.context)
-            elif params["force_api_impl"] == "pg":
-                forced_api_impl = pg.PostgresAPI(self.context)
+        module = self.get_module()
+        impl = module.Search(self.config)
 
-        # API to actually call
-        api = forced_api_impl or self.api_impl
-
-        return api.search(types, **params)
-
-    def _parse_query_string(self, query_string):
-        """
-        Take a string of parameters and return a dictionary of key, value.
-        """
-        terms_sep = "+"
-        params_sep = "/"
-
-        args = query_string.split(params_sep)
-
-        params = {}
-        for i in xrange(0, len(args), 2):
-            if args[i] and args[i + 1]:
-                params[args[i]] = args[i + 1]
-
-        for i in params:
-            if params[i].find(terms_sep) > -1:
-                params[i] = params[i].split(terms_sep)
-
-        return params
+        return impl.search(**params)
