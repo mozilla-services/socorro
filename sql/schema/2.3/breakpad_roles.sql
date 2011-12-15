c5c3303c927a38ef57e1f6d68b86b857815337fd
Robert Helmer <rhelmer@rhelmer.org>
2011-12-15 11:28:38 -0800

    upgrade schema dump to latest 2.3 (2.3.4.1)

---
+++ sql/schema/2.3/breakpad_roles.sql
@@ -0,0 +1,10 @@
+CREATE USER breakpad WITH PASSWORD 'aPassword';
+CREATE USER breakpad_metrics WITH PASSWORD 'aPassword';
+CREATE USER breakpad_ro WITH PASSWORD 'aPassword';
+CREATE USER breakpad_rw WITH PASSWORD 'aPassword';
+CREATE USER monitor WITH PASSWORD 'aPassword';
+CREATE USER monitoring WITH PASSWORD 'aPassword';
+CREATE USER nagiosdaemon WITH PASSWORD 'aPassword';
+CREATE USER postgres WITH PASSWORD 'aPassword';
+CREATE USER processor WITH PASSWORD 'aPassword';
+CREATE USER ganglia WITH PASSWORD 'aPassword';
