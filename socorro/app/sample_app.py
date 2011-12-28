eb4aa6ce6d7d91399e6dcd574bcbcc13328f03d8
K Lars Lohn <twobraids@gmail.com>
2011-12-27 17:20:39 -0800

    added example app for use as a basis for future Socorro apps.

---
+++ socorro/app/sample_app.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+"""demonstrates using configman to fetch algorithms and implementations"""
+
+# This app can be invoked like this:
+#     .../socorro/app/sampleApp.py --help
+# set your path to make that simpler
+# set both socorro and configman in your PYTHONPATH
+
+import configman.config_manager as cm
+import datetime
+import configman.converters as conv
+import socorro.lib.util as sutil
+
+
+class SampleApp(cm.RequiredConfig):
+    app_name = 'sample'
+    app_version = '0.1'
+    app_doc = __doc__
+
+    required_config = cm.Namespace()
+    required_config.add_option('name',
+                               default='Wilma',
+                               doc='a name to echo')
+    required_config.add_option('time',
+                               default=datetime.datetime.now(),
+                               doc='the time of day')
+
+    def __init__(self, config):
+        super(SampleApp, self).__init__()
+        self.config = config
+
+    def main(self):
+        # this is where we'd implement the app
+        print 'hello, %s. The time is: %s' % (self.config.name,
+                                              self.config.time)
+
+
+if __name__ == '__main__':
+    import socorro.app.generic_app as gapp
+    gapp.main(SampleApp)
