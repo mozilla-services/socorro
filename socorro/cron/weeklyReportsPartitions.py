564f1d46dd0cbe640090198a856cefd6e10f7cae
Peter Bengtsson <mail@peterbe.com>
2012-01-12 10:30:50 -0800

    work in progress (commit msg TBD)

--- socorro/cron/weeklyReportsPartitions.py
+++ socorro/cron/weeklyReportsPartitions.py
@@ -6,29 +6,30 @@ logger = logging.getLogger("weeklyReportsPartitions")
 
 from configman import Namespace, ConfigurationManager
 from socorro.database.transaction_executor import TransactionExecutorWithBackoff
+from socorro.app.generic_app import App, main
 
 """
 See http://socorro.readthedocs.org/en/latest/databaseadminfunctions.html#weekly-report-partitions
 See https://bugzilla.mozilla.org/show_bug.cgi?id=701253
 """
 
-def run_query(connection):
-    connection.query('SELECT weekly_report_partitions();')
 
+class WeeklyReportsPartitions(App):
+    app_name = 'weekly_reports_partitions'
+    app_version = '0.1'
+    app_description = __doc__
 
-def run(config):
     definition_source = Namespace()
     definition_source.add_option('transaction_executor_class',
                                  default=TransactionExecutorWithBackoff,
                                  doc='a class that will execute transactions')
 
-    app_name = os.path.splitext(os.path.basename(__file__))[0]
-    c = ConfigurationManager(definition_source,
-                             app_name=app_name,
-                             app_description=__doc__)
+    def run_query(self, connection):
+        connection.query('SELECT weekly_report_partitions();')
 
-    with c.context() as config:
-        # the configuration has a class that can execute transactions
-        # we instantiate it here.
-        executor = config.transaction_executor_class(config)
-        executor.do_transaction(run_query)
+    def main(self):
+        with self.config.context() as config:
+            # the configuration has a class that can execute transactions
+            # we instantiate it here.
+            executor = config.transaction_executor_class(config)
+            executor.do_transaction(run_query)
