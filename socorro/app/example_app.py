69b65f367653b3814be64539bc5b9ce3b8e2fa5d
K Lars Lohn <twobraids@gmail.com>
2012-01-11 16:18:42 -0800

    fix erroneous comment

--- socorro/app/example_app.py
+++ socorro/app/example_app.py
@@ -2,7 +2,7 @@
 """demonstrates using configman to make a Socorro app"""
 
 # This app can be invoked like this:
-#     .../socorro/app/sampleApp.py --help
+#     .../socorro/app/example_app.py --help
 # set your path to make that simpler
 # set both socorro and configman in your PYTHONPATH
 
