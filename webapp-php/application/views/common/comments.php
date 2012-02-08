4262ede9e063c3ff42945f5ba2884505b8f3266e
Adrian Gaudebert <adrian@gaudebert.fr>
2012-02-08 16:10:09 +0100

    Fixes bug 725278 - Used date_processed instead of client_crash_date when displaying comments.

--- webapp-php/application/views/common/comments.php
+++ webapp-php/application/views/common/comments.php
@@ -3,7 +3,7 @@
 		<?php if (!empty($comments)) { ?>
 			<?php foreach ($comments as $comment) { ?>
 				<p class="crash_comments"><?php echo nl2br(out::h($comment->user_comments)); ?></p>
-				<p class="crash_comments"><a href="<?= url::site('report/index/' . $comment->uuid)?>">Submitted: <?php out::h($comment->client_crash_date); ?></a></p>
+				<p class="crash_comments"><a href="<?= url::site('report/index/' . $comment->uuid)?>">Submitted: <?php out::h($comment->date_processed); ?></a></p>
 				<?php if ($logged_in) { ?>
 					<p class="crash_comments">Email: <a href="mailto:<?php out::h($comment->email); ?>"><?php out::h($comment->email); ?></a></p>
 				<?php } ?>
