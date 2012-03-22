c4f72860f5787179a53f1aa2aa24a2cc8b957015
Brandon Savage <brandon@brandonsavage.net>
2012-03-22 11:59:30 -0400

    Fixes Bug 729359 - Clean up of the services I wrote to be PEP8 compliant.

--- socorro/middleware/signature_summary_service.py
+++ socorro/middleware/signature_summary_service.py
@@ -1,9 +1,7 @@
-import logging
-
 from socorro.middleware.service import DataAPIService
 
-class SignatureSummary(DataAPIService):
 
+class SignatureSummary(DataAPIService):
 
     service_name = "signature_summary"
     uri = "/signaturesummary/(.*)"
@@ -11,9 +9,8 @@ class SignatureSummary(DataAPIService):
     def __init__(self, config):
         super(SignatureSummary, self).__init__(config)
 
-
     def get(self, *args):
         params = self.parse_query_string(args[0])
         module = self.get_module(params)
         impl = module.SignatureSummary(config=self.context)
-        return impl.get(**params) 
+        return impl.get(**params)
