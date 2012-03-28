30571dae27c68d3d7dad11c1dd7ca595d0f0ced1
Schalk Neethling <sneethling@mozilla.com>
2012-03-28 13:46:32 +0200

    using new column added to tcbs instead of sig summary service prevents the 100 queries horror

--- webapp-php/application/controllers/topcrasher.php
+++ webapp-php/application/controllers/topcrasher.php
@@ -317,7 +317,9 @@ class Topcrasher_Controller extends Controller {
                     $top_crasher->{'display_percent'} = number_format($top_crasher->percentOfTotal * 100, 2) . "%";
                     $top_crasher->{'display_previous_percent'} = number_format($top_crasher->previousPercentOfTotal * 100, 2) . "%";
                     $top_crasher->{'display_change_percent'} = number_format($top_crasher->changeInPercentOfTotal * 100, 2) . "%";
-                    $top_crasher->{'startup_crash'} = $this->topcrashers_model->isStartupCrasher($top_crasher->signature, $resp->start_date, $params->end_date);
+                    if(isset($top_crasher->startup_percent) && !empty($top_crasher->startup_percent)) {
+                        $top_crasher->{'startup_crash'} = (round($top_crasher->startup_percent) > 50);
+                    }
                     
                     array_push($signatures, $top_crasher->signature);
 
@@ -410,7 +412,9 @@ class Topcrasher_Controller extends Controller {
                     $top_crasher->{'display_percent'} = number_format($top_crasher->percentOfTotal * 100, 2) . "%";
                     $top_crasher->{'display_previous_percent'} = number_format($top_crasher->previousPercentOfTotal * 100, 2) . "%";
                     $top_crasher->{'display_change_percent'} = number_format($top_crasher->changeInPercentOfTotal * 100, 2) . "%";
-                    $top_crasher->{'startup_crash'} = $this->topcrashers_model->isStartupCrasher($top_crasher->signature, $resp->start_date, $params->end_date);
+                    if(isset($top_crasher->startup_percent) && !empty($top_crasher->startup_percent)) {
+                        $top_crasher->{'startup_crash'} = (round($top_crasher->startup_percent) > 50);
+                    }
 
                     array_push($signatures, $top_crasher->signature);
                     $top_crasher->{'correlation_os'} = Correlation::correlationOsName($top_crasher->win_count, $top_crasher->mac_count, $top_crasher->linux_count);
