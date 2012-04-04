fd158ff2f6e8d837df605f41312c4ece823ba908
Josh Berkus <josh@agliodbs.com>
2012-04-03 17:58:03 -0700

    Added explosive crashes to 5.0.

---
+++ sql/upgrade/5.0/explosive_crashes.sql
@@ -0,0 +1,238 @@
+\set ON_ERROR_STOP 1
+
+SELECT create_table_if_not_exists ( 
+-- new table name here
+'explosiveness', 
+-- full SQL create table statement here
+$q$
+create table explosiveness (
+	product_version_id INT NOT NULL,
+	signature_id INT NOT NULL,
+	report_date DATE not null,
+	oneday NUMERIC NOT NULL DEFAULT 0,
+	threeday NUMERIC NOT NULL DEFAULT 0,
+	constraint explosiveness_key primary key ( product_version_id, signature_id, report_date )
+);$q$, 
+-- owner of table; always breakpad_rw
+'breakpad_rw', 
+-- array of indexes to create
+ARRAY [ 'product_version_id','signature_id' ]);
+
+
+-- daily update function
+CREATE OR REPLACE FUNCTION update_explosiveness (
+	updateday DATE, checkdata BOOLEAN default TRUE )
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+SET work_mem = '512MB'
+SET temp_buffers = '512MB'
+SET client_min_messages = 'ERROR'
+AS $f$
+-- set stats parameters per Kairo
+DECLARE 
+	-- minimum crashes/mil.adu to show up
+	minrate INT := 10;	
+	-- minimum comparitor figures if there are no
+	-- or very few proir crashes to smooth curves
+	-- mostly corresponds to Kairo "clampperadu"
+	mindiv_one INT := 30;
+	mindiv_three INT := 15;
+BEGIN
+-- this function populates a daily matview
+-- for explosiveness
+-- depends on tcbs and product_adu
+
+-- check if we've been run
+IF checkdata THEN
+	PERFORM 1 FROM explosiveness
+	WHERE report_date = updateday
+	LIMIT 1;
+	IF FOUND THEN
+		RAISE EXCEPTION 'explosiveness has already been run for %.',updateday;
+	END IF;
+END IF;
+
+-- check if product_adu and tcbs are updated
+SELECT 1
+FROM tcbs JOIN product_adu
+   ON tcbs.report_date = product_adu.adu_date
+WHERE tcbs.report_date = updateday
+LIMIT 1;
+
+IF NOT FOUND THEN
+	IF checkdata THEN
+		RAISE EXCEPTION 'Either product_adu or tcbs have not been updated to the end of %',updateday;
+	ELSE
+		RAISE NOTICE 'Either product_adu or tcbs has not been updated, skipping.'
+		RETURN TRUE;
+	END IF;
+END IF;
+
+-- compute dates 
+-- note that dates are inclusive
+-- last date of measured period
+mes_edate := updateday;
+-- first date of the measured period for 3-day
+mes_b3date := updateday - 2;
+-- last date of the comparison period for 1-day
+comp_e1date := updateday - 1;
+-- last date of the comparison period for 3-day
+comp_e3date := mes_b3date - 1;
+-- first date of the comparison period
+comp_bdate := mes_edate - 9;
+
+-- create oneday temp table
+CREATE TEMPORARY TABLE explosive_oneday
+ON COMMIT DROP
+AS 
+WITH adusum AS (
+	SELECT adu_date, sum(adu_count) as adu_count,
+		( mindiv_one * 1000000::numeric / sum(adu_count)) as mindivisor,
+		product_version_id
+	FROM product_adu
+	WHERE adu_date BETWEEN comp_bdate and mes_edate
+		AND adu_count > 0
+	GROUP BY adu_date, product_version_id 
+),
+reportsum AS (
+	SELECT report_date, sum(report_count) as report_count,
+		product_version_id, signature_id
+	FROM tcbs
+	WHERE report_date BETWEEN comp_bdate and mes_edate
+	GROUP BY report_date, product_version_id, signature_id
+),
+crash_madu AS (
+	SELECT ( report_count * 1000000::numeric ) / adu_count AS crash_madu,
+		reportsum.product_version_id, reportsum.signature_id, 
+		report_date, mindivisor
+	FROM adusum JOIN reportsum
+		ON adu_date = report_date
+		AND adusum.product_version_id = reportsum.product_version_id
+),
+sum1day AS ( 
+	SELECT product_version_id, signature_id, crash_madu as sum1day,
+		mindivisor
+	FROM crash_madu
+	WHERE report_date = mes_edate
+	AND crash_madu > 10
+),
+agg9day AS (
+	SELECT product_version_id, signature_id,
+		AVG(crash_madu) AS avg9day,
+		MAX(crash_madu) as max9day
+	FROM crash_madu
+	WHERE report_date BETWEEN comp_bdate and comp_e1date
+	GROUP BY product_version_id, signature_id
+)
+SELECT sum1day.signature_id,
+	sum1day.product_version_id ,
+	round (
+		( sum1day.sum1day - agg9day.avg9day ) 
+			/
+		GREATEST ( agg9day.max9day - agg9day.avg9day, sum1day.mindivisor )
+		, 2 )
+	as explosive_1day
+FROM sum1day 
+	LEFT OUTER JOIN agg9day USING ( signature_id, product_version_id );
+
+-- create threeday temp table
+CREATE TEMPORARY TABLE explosive_threeday
+ON COMMIT DROP
+AS
+WITH adusum AS (
+	SELECT adu_date, sum(adu_count) as adu_count,
+		( mindiv_three * 1000000::numeric / sum(adu_count)) as mindivisor,
+		product_version_id
+	FROM product_adu
+	WHERE adu_date BETWEEN comp_bdate and mes_edate
+		AND adu_count > 0
+	GROUP BY adu_date, product_version_id 
+),
+reportsum AS (
+	SELECT report_date, sum(report_count) as report_count,
+		product_version_id, signature_id
+	FROM tcbs
+	WHERE report_date BETWEEN comp_bdate and mes_edate
+	GROUP BY report_date, product_version_id, signature_id
+),
+crash_madu AS (
+	SELECT ( report_count * 1000000::numeric ) / adu_count AS crash_madu,
+		reportsum.product_version_id, reportsum.signature_id, 
+		report_date, mindivisor
+	FROM adusum JOIN reportsum
+		ON adu_date = report_date
+		AND adusum.product_version_id = reportsum.product_version_id
+),
+avg3day AS ( 
+	SELECT product_version_id, signature_id, avg(crash_madu) as avg3day,
+		avg(mindivisor) as mindivisor
+	FROM crash_madu
+	WHERE report_date BETWEEN mes_b3date and mes_edate
+	AND crash_madu > 10
+	GROUP BY product_version_id, signature_id
+),
+agg7day AS (
+	SELECT product_version_id, signature_id,
+		AVG(crash_madu) AS avg7day,
+		STDDEV(crash_madu) as sdv7day
+	FROM crash_madu
+	WHERE report_date BETWEEN comp_bdate and comp_e3date
+	GROUP BY product_version_id, signature_id
+)
+SELECT avg3day.signature_id,
+	avg3day.product_version_id ,
+	round (
+		( avg3day - avg7day ) 
+			/
+		GREATEST ( sdv7day - avg7day, avg3day.mindivisor )
+		, 2 )
+	as explosive_3day
+FROM avg3day LEFT OUTER JOIN agg7day 
+	USING ( signature_id, product_version_id );
+	
+-- truncate explosiveness
+DELETE FROM explosiveness;
+
+-- merge the two tables and insert
+INSERT INTO explosiveness (
+	report_date, signature_id, product_version_id, 
+	oneday, threeday
+SELECT signature_id, product_version_id, explosive_1day, explosive_3day
+FROM explosive_oneday FULL OUTER JOIN explosive_threeday
+	USING ( signature_id, product_version_id )
+ORDER BY product_version_id;
+	
+RETURN TRUE;
+END; $f$;
+
+-- now create a backfill function 
+-- so that we can backfill missing data
+CREATE OR REPLACE FUNCTION backfill_explosiveness(
+	updateday DATE )
+RETURNS BOOLEAN
+LANGUAGE plpgsql AS
+$f$
+BEGIN
+
+PERFORM update_explosiveness(updateday, false);
+
+RETURN TRUE;
+END; $f$;
+
+-- sample backfill script
+-- for initialization
+SELECT update_explosiveness(current_date - 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
---
+++ sql/upgrade/5.0/upgrade.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#please see README
+
+set -e
+
+CURDIR=$(dirname $0)
+VERSION=5.0
+
+#echo '*********************************************************'
+#echo 'support functions'
+#psql -f ${CURDIR}/support_functions.sql breakpad
+
+echo '*********************************************************'
+echo 'data source for explosiveness'
+echo 'bug 733021'
+psql -f ${CURDIR}/explosive_crashes.sql breakpad
+
+#change version in DB
+psql -c "SELECT update_socorro_db_version( '$VERSION' )" breakpad
+
+echo "$VERSION upgrade done"
+
+exit 0
