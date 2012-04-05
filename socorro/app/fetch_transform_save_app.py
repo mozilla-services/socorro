0abceb3ae44a57746a860c6d0410b8051418694c
K Lars Lohn <twobraids@gmail.com>
2012-04-05 13:20:16 -0700

    added threaded_task_manager - a threaded producer/consumer system added app base for multithreaded apps

---
+++ socorro/app/fetch_transform_save_app.py
@@ -0,0 +1,147 @@
+"""this is the basis for any app that follows the fetch/transform/save model
+
+* the configman versions of the crash mover and the processor apps will
+  derive from this class
+
+The form of fetch/transform/save, of course, in three parts
+1) fetch - some iterating or streaming function or object fetches packets of
+           from data a source
+2) transform - some function transforms each packet of data into a new form
+3) save - some function or class saves or streams the packet to some data
+           sink.
+
+For the crash mover, the fetch phase is reading new crashes from the
+collector's file system datastore.  The transform phase is the degenerate
+case of identity: no transformation.  The save phase is just sending the
+crashes to HBase.
+
+For the processor, the fetch phase is reading from the new crash queue.  In,
+2012, that's the union of reading a postgres jobs/ooid table and fetching the
+crash from HBase.  The transform phase is the running of minidump stackwalk
+and production of the processed crash data.  The save phase is the union of
+sending new crash records to Postgres; sending the processed crash to HBase;
+the the submission of the ooid to Elastic Search."""
+
+import signal
+
+from configman import Namespace
+from configman.converters import class_converter
+
+from socorro.lib.threaded_task_manager import ThreadedTaskManager, \
+                                              respond_to_SIGTERM
+from socorro.app.generic_app import App
+
+
+#==============================================================================
+class FetchTransformSaveApp(App):
+    """base class for apps that follow the fetch/transform/save model"""
+    app_name = 'generic_fetch_transform_save_app'
+    app_version = '0.1'
+    app_description = __doc__
+
+    required_config = Namespace()
+    # the required config is broken into two parts: the source and the
+    # destination.  Each of those gets assigned a crasnstorage class.
+    required_config.source = Namespace()
+    # For source, the storage class should be one that defines a method
+    # of fetching new crashes through the three storage api methods: the
+    # iterator 'new_ooids' and the accessors 'get_raw_crash' and 'get_dump'
+    required_config.source.add_option(
+      'crashstorage',
+      doc='the source storage class',
+      default=None,
+      from_string_converter=class_converter
+    )
+    required_config.destination = Namespace()
+    # For destination, the storage class should implement the 'save_raw_crash'
+    # method.  Of course, a subclass may redefine either the source_iterator
+    # or transform methods and therefore completely redefine what api calls
+    # are relevant.
+    required_config.destination.add_option(
+      'crashstorage',
+      doc='the destination storage class',
+      default=None,
+      from_string_converter=class_converter
+    )
+
+    #--------------------------------------------------------------------------
+    def source_iterator(self):
+        """this iterator yields individual ooids from the source crashstorage
+        class's 'new_ooids' method."""
+        while(True):  # loop forever and never raise StopIteration
+            for x in self.source.new_ooids():
+                if x is None:
+                    yield None
+                else:
+                    yield ((x,), {})  # (args, kwargs)
+            else:
+                yield None  # if the inner iterator yielded nothing at all,
+                            # yield None to give the caller the chance to sleep
+
+    #--------------------------------------------------------------------------
+    def transform(self, ooid):
+        """this default transform function only transfers raw data from the
+        source to the destination without changing the data.  While this may
+        be good enough for the raw crashmover, the processor would override
+        this method to create and save processed crashes"""
+        raw_crash = self.source.get_raw_crash(ooid)
+        dump = self.source.get_dump(ooid)
+        self.destination.save_raw_crash(raw_crash, dump)
+
+    #--------------------------------------------------------------------------
+    def quit_check(self):
+        self.task_manager.quit_check()
+
+    #--------------------------------------------------------------------------
+    def signal_quit(self):
+        self.task_manager.stop()
+
+    #--------------------------------------------------------------------------
+    def _setup_source_and_destination(self):
+        """instantiate the classes that implement the source and destination
+        crash storage systems."""
+        try:
+            self.source = self.config.source.crashstorage(
+              self.config.source,
+              quit_check_callback=self.quit_check
+            )
+        except Exception:
+            self.config.logger.critical(
+              'Error in creating crash source',
+              exc_info=True
+            )
+            raise
+        try:
+            self.destination = self.config.destination.crashstorage(
+              self.config.destination,
+              quit_check_callback=self.quit_check
+            )
+        except Exception:
+            self.config.logger.critical(
+              'Error in creating crash destination',
+              exc_info=True
+            )
+            raise
+
+    #--------------------------------------------------------------------------
+    def _setup_task_manager(self):
+        """instantiate the threaded task manager to run the producer/consumer
+        queue that is the heart of the processor."""
+        signal.signal(signal.SIGTERM, respond_to_SIGTERM)
+        signal.signal(signal.SIGHUP, respond_to_SIGTERM)
+        self.task_manager = ThreadedTaskManager(
+          self.config,
+          job_source_iterator=self.source_iterator,
+          task_func=self.transform
+        )
+
+    #--------------------------------------------------------------------------
+    def main(self):
+        """this main routine sets up the signal handlers, the source and
+        destination crashstorage systems at the  theaded task manager.  That
+        starts a flock of threads that are ready to shepherd crashes from
+        the source to the destination."""
+
+        self._setup_task_manager()
+        self._setup_source_and_destination()
+        self.task_manager.blocking_start()
---
+++ socorro/unittest/app/test_generic_fetch_transform_save_app.py
@@ -0,0 +1,229 @@
+import unittest
+from mock import Mock
+
+from configman import ConfigurationManager
+
+from socorro.app.fetch_transform_save_app import FetchTransformSaveApp
+
+from socorro.lib.util import DotDict, SilentFakeLogger
+
+
+class TestFetchTransformSaveApp(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_bogus_source_iter_and_worker(self):
+        class TestFTSAppClass(FetchTransformSaveApp):
+            def __init__(self, config):
+                super(TestFTSAppClass, self).__init__(config)
+                self.the_list = []
+
+            def _setup_source_and_destination(self):
+                pass
+
+            def source_iterator(self):
+                for x in xrange(5):
+                    yield ((x,), {})
+
+            def transform(self, anItem):
+                self.the_list.append(anItem)
+
+        logger = SilentFakeLogger()
+        config = DotDict({ 'logger': logger,
+                           'number_of_threads': 2,
+                           'maximum_queue_size': 2,
+                           'source': DotDict({'crashstorage': None}),
+                           'destination':DotDict({'crashstorage': None})
+                         })
+
+        fts_app = TestFTSAppClass(config)
+        fts_app.main()
+        self.assertTrue(len(fts_app.the_list) == 5,
+                        'expected to do 5 inserts, '
+                          'but %d were done instead' % len(fts_app.the_list))
+        self.assertTrue(sorted(fts_app.the_list) == range(5),
+                        'expected %s, but got %s' % (range(5),
+                                                     sorted(fts_app.the_list)))
+
+
+    def test_bogus_source_and_destination(self):
+        class NonInfiniteFTSAppClass(FetchTransformSaveApp):
+            def source_iterator(self):
+                for x in self.source.new_ooids():
+                    yield ((x,), {})
+
+        class FakeStorageSource(object):
+            def __init__(self, config, quit_check_callback):
+                self.store = DotDict({'1234': DotDict({'ooid': '1234',
+                                                       'Product': 'FireFloozy',
+                                                       'Version': '1.0'}),
+                                      '1235': DotDict({'ooid': '1235',
+                                                       'Product': 'ThunderRat',
+                                                       'Version': '1.0'}),
+                                      '1236': DotDict({'ooid': '1236',
+                                                       'Product': 'Caminimal',
+                                                       'Version': '1.0'}),
+                                      '1237': DotDict({'ooid': '1237',
+                                                       'Product': 'Fennicky',
+                                                       'Version': '1.0'}),
+                                     })
+            def get_raw_crash(self, ooid):
+                return self.store[ooid]
+            def get_dump(self, ooid):
+                return 'this is a fake dump'
+            def new_ooids(self):
+                for k in self.store.keys():
+                    yield k
+
+
+        class FakeStorageDestination(object):
+            def __init__(self, config, quit_check_callback):
+                self.store = DotDict()
+                self.dumps = DotDict()
+            def save_raw_crash(self, raw_crash, dump):
+                self.store[raw_crash.ooid] = raw_crash
+                self.dumps[raw_crash.ooid] = dump
+
+        logger = SilentFakeLogger()
+        config = DotDict({ 'logger': logger,
+                           'number_of_threads': 2,
+                           'maximum_queue_size': 2,
+                           'source': DotDict({'crashstorage':
+                                                  FakeStorageSource}),
+                           'destination':DotDict({'crashstorage':
+                                                      FakeStorageDestination})
+                         })
+
+        fts_app = NonInfiniteFTSAppClass(config)
+        fts_app.main()
+
+        source = fts_app.source
+        destination = fts_app.destination
+
+        self.assertEqual(source.store, destination.store)
+        self.assertEqual(len(destination.dumps), 4)
+        self.assertEqual(destination.dumps['1237'], source.get_dump('1237'))
+
+
+    def test_source_iterator(self):
+        class FakeStorageSource(object):
+            def __init__(self):
+                self.first = True
+
+            def new_ooids(self):
+                if self.first:
+                    self.first = False
+                else:
+                    for k in range(999):
+                        yield k
+                    for k in range(2):
+                        yield None
+
+
+        class FakeStorageDestination(object):
+            def __init__(self, config, quit_check_callback):
+                self.store = DotDict()
+                self.dumps = DotDict()
+            def save_raw_crash(self, raw_crash, dump):
+                self.store[raw_crash.ooid] = raw_crash
+                self.dumps[raw_crash.ooid] = dump
+
+        logger = SilentFakeLogger()
+        config = DotDict({ 'logger': logger,
+                           'number_of_threads': 2,
+                           'maximum_queue_size': 2,
+                           'source': DotDict({'crashstorage':
+                                                  FakeStorageSource}),
+                           'destination':DotDict({'crashstorage':
+                                                      FakeStorageDestination})
+                         })
+
+        fts_app = FetchTransformSaveApp(config)
+        fts_app.source = FakeStorageSource()
+        fts_app.destination = FakeStorageDestination
+        error_detected = False
+        for x, y in zip(xrange(1002), (a for a in fts_app.source_iterator())):
+            if x == 0:
+                self.assertTrue(y is None)
+            elif x < 1000:
+                if x - 1 != y[0][0] and not error_detected:
+                    error_detected = True
+                    self.assertEqual(x, y,
+                                     'iterator fails on iteration %d' % x)
+            else:
+                print x, y
+                if y is not None and not error_detected:
+                    error_detected = True
+                    self.assertTrue(x is None,
+                                    'iterator fails on iteration %d' % x)
+
+
+    def test_no_source(self):
+        class FakeStorageDestination(object):
+            def __init__(self, config, quit_check_callback):
+                self.store = DotDict()
+                self.dumps = DotDict()
+            def save_raw_crash(self, raw_crash, dump):
+                self.store[raw_crash.ooid] = raw_crash
+                self.dumps[raw_crash.ooid] = dump
+
+        logger = SilentFakeLogger()
+        config = DotDict({ 'logger': logger,
+                           'number_of_threads': 2,
+                           'maximum_queue_size': 2,
+                           'source': DotDict({'crashstorage':
+                                                  None}),
+                           'destination':DotDict({'crashstorage':
+                                                      FakeStorageDestination})
+                         })
+
+        fts_app = FetchTransformSaveApp(config)
+
+        self.assertRaises(TypeError, fts_app.main)
+
+
+
+    def test_no_destination(self):
+        class FakeStorageSource(object):
+            def __init__(self, config, quit_check_callback):
+                self.store = DotDict({'1234': DotDict({'ooid': '1234',
+                                                       'Product': 'FireFloozy',
+                                                       'Version': '1.0'}),
+                                      '1235': DotDict({'ooid': '1235',
+                                                       'Product': 'ThunderRat',
+                                                       'Version': '1.0'}),
+                                      '1236': DotDict({'ooid': '1236',
+                                                       'Product': 'Caminimal',
+                                                       'Version': '1.0'}),
+                                      '1237': DotDict({'ooid': '1237',
+                                                       'Product': 'Fennicky',
+                                                       'Version': '1.0'}),
+                                     })
+            def get_raw_crash(self, ooid):
+                return self.store[ooid]
+            def get_dump(self, ooid):
+                return 'this is a fake dump'
+            def new_ooids(self):
+                for k in self.store.keys():
+                    yield k
+
+        logger = SilentFakeLogger()
+        config = DotDict({ 'logger': logger,
+                           'number_of_threads': 2,
+                           'maximum_queue_size': 2,
+                           'source': DotDict({'crashstorage':
+                                                  FakeStorageSource}),
+                           'destination':DotDict({'crashstorage':
+                                                      None})
+                         })
+
+        fts_app = FetchTransformSaveApp(config)
+
+        self.assertRaises(TypeError, fts_app.main)
+
+
