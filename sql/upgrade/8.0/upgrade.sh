4eac877775656c9df687c4ca559ba11eadbbb465
Josh Berkus <josh@agliodbs.com>
2012-04-24 17:18:19 -0700

    Fix typo in upgrade.sh.

--- sql/upgrade/8.0/upgrade.sh
+++ sql/upgrade/8.0/upgrade.sh
@@ -14,7 +14,7 @@ psql -f ${CURDIR}/explosive_crashes.sql breakpad
 echo '*********************************************************'
 echo 'restrict product_version_builds to main repositories'
 echo 'bug 748194'
-psql -f ${CURDIR}/explosive_crashes.sql breakpad
+psql -f ${CURDIR}/update_products_repos.sql breakpad
 
 #change version in DB
 psql -c "SELECT update_socorro_db_version( '$VERSION' )" breakpad
