a17fd547c22bf8c2e82b074f73f19edc97ecd518
Josh Berkus <josh@agliodbs.com>
2012-05-02 11:28:11 -0700

    Backed nightly builds changes out of 8.0 due to testing issues.

--- sql/upgrade/8.0/README.rst
+++ sql/upgrade/8.0/README.rst
@@ -6,9 +6,6 @@ This batch makes the following database changes:
 bug #744492
 	Fixes math for explosiveness calculation
 	
-bug #748194
-	Restrict product_version_builds to primary repositories.
-	
 bug #749842
 	Fix math in crash_ratio
 	
--- sql/upgrade/8.0/upgrade.sh
+++ sql/upgrade/8.0/upgrade.sh
@@ -11,10 +11,11 @@ echo 'fix math for explosive crashes '
 echo 'bug 744492'
 psql -f ${CURDIR}/explosive_crashes.sql breakpad
 
-echo '*********************************************************'
-echo 'restrict product_version_builds to main repositories'
-echo 'bug 748194'
-psql -f ${CURDIR}/update_products_repos.sql breakpad
+# bumped to 9.0 due to test failure
+#echo '*********************************************************'
+#echo 'restrict product_version_builds to main repositories'
+#echo 'bug 748194'
+#psql -f ${CURDIR}/update_products_repos.sql breakpad
 
 echo '*********************************************************'
 echo 'fix crash_ratio'
