5318a88c25d7f0a930a33fd36308a9e458e19838
Schalk Neethling <sneethling@mozilla.com>
2012-05-17 21:31:47 +0200

    added tab and form to branch data sources for adding new products fixes bug 755230

--- webapp-php/js/socorro/utils.js
+++ webapp-php/js/socorro/utils.js
@@ -1,6 +1,20 @@
 (function( window, undefined ) {
     "use strict";
     var socorro = {
+        ui: {
+            setLoader: function(container) {
+                var loader = new Image();
+                //set the id, alt and src attributes of the loading image
+                loader.setAttribute("id", "loading");
+                loader.setAttribute("alt", "graph loading...");
+                loader.setAttribute("src", "/img/icons/ajax-loader.gif");
+
+                //append loader to it's container
+                console.log(container);
+                console.log(loader);
+                $(container).append(loader);
+            }
+        },
         date: {
             ONE_DAY: 1000 * 60 * 60 * 24,
             now: function() {
