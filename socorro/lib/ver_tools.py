3994bf1476a265aa4b8f6ba06abeeeb36fc97b56
Gervase Markham <gerv@mink.(none)>
2012-05-17 15:22:16 +0100

    Upgrade licence to MPL 2

--- socorro/lib/ver_tools.py
+++ socorro/lib/ver_tools.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """A function and supporting structure to turn Mozilla version strings into
 a sortable normalized format.
 
--- sql/upgrade/1.7.7/backfill_dups.sql
+++ sql/upgrade/1.7.7/backfill_dups.sql
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 -- one-time function to backfill possible dups for all history
 
 \set ON_ERROR_STOP true
