3fd293d0fdef58dc0362c9adea3b8df9c060c400
Josh Berkus <josh@agliodbs.com>
2012-05-21 17:13:31 -0700

    Added 11.0 DB changes, including some tweakes to version editing and requested sort column for product views.  Needs testing.

---
+++ sql/upgrade/11.0/upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#please see README
+
+set -e
+
+CURDIR=$(dirname $0)
+VERSION=11.0
+
+echo '*********************************************************'
+echo 'functions for adding new releases manually and changing featured versions'
+echo 'bug 752074'
+psql -f ${CURDIR}/add_release.sql breakpad
+psql -f ${CURDIR}/feature_versions.sql breakpad
+
+echo '*********************************************************'
+echo 'add product sorting column to product_info'
+echo 'bug ??????'
+psql -f ${CURDIR}/product_views.sql breakpad
+
+#change version in DB
+psql -c "SELECT update_socorro_db_version( '$VERSION' )" breakpad
+
+echo "$VERSION upgrade done"
+
+exit 0
