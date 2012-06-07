46da1ec6a4c1b37c9a22e844a02bc92fadf5a0f4
Josh Berkus <josh@agliodbs.com>
2012-06-06 17:24:33 -0700

    ready for test of oldtcbs purge

---
+++ sql/upgrade/15.0/drop_oldtcbs.sql
@@ -0,0 +1,17 @@
+\set ON_ERROR_STOP 1
+
+DROP TABLE IF EXISTS alexa_topsites CASCADE;
+DROP TABLE IF EXISTS top_crashes_by_signature CASCADE;
+DROP TABLE IF EXISTS top_crashes_by_url CASCADE;
+DROP TABLE IF EXISTS top_crashes_by_url_signature CASCADE;
+DROP TABLE IF EXISTS signature_build CASCADE;
+DROP TABLE IF EXISTS signature_first CASCADE;
+DROP TABLE IF EXISTS signature_bugs_rollup CASCADE;
+DROP TABLE IF EXISTS signature_productdims CASCADE;
+DROP TABLE IF EXISTS release_build_type_map CASCADE;
+DROP TABLE IF EXISTS builds CASCADE;
+DROP TABLE IF EXISTS osdims CASCADE;
+DROP TABLE IF EXISTS productdims_version_sort CASCADE;
+DROP TABLE IF EXISTS product_visibility CASCADE;
+DROP TABLE IF EXISTS productdims CASCADE;
+DROP TABLE IF EXISTS urldims CASCADE;
--- sql/upgrade/15.0/upgrade.sh
+++ sql/upgrade/15.0/upgrade.sh
@@ -17,6 +17,19 @@ echo 'add field to track auto refresh date'
 echo 'bug 768300'
 psql -f ${CURDIR}/create_refresh_date.sql $DBNAME
 
+echo '*********************************************************'
+echo 'support functions'
+psql -f ${CURDIR}/support_functions.sql $DBNAME
+
+echo '*********************************************************'
+echo 'copy old products to new tables'
+psql -f ${CURDIR}/copy_old_products_to_product_versions.sql $DBNAME
+
+echo '*********************************************************'
+echo 'purge oldtcbs tables'
+psql -f ${CURDIR}/drop_oldtcbs.sql $DBNAME
+psql -f ${CURDIR}/product_views.sql $DBNAME
+
 #change version in DB
 psql -c "SELECT update_socorro_db_version( '$VERSION' )" $DBNAME
 
