8d427541d1b7a6bfe6a6de18d7d727ec3e052c67
Josh Berkus <josh@pgexperts.com>
2012-06-12 09:06:40 -0700

    More mobeta migration code.

---
+++ sql/upgrade/15.0/rapid_beta_version.sql
@@ -0,0 +1,12 @@
+\set ON_ERROR_STOP 1
+
+SELECT alter_column_if_not_exists('products','rapid_beta_version','major_version');
+
+UPDATE products SET rapid_beta_version = '16.0'
+WHERE product_name = 'Firefox';
+
+UPDATE products SET rapid_beta_version = '1000.0'
+WHERE product_name <> 'Firefox';
+
+UPDATE products SET rapid_release_version = '2.1'
+WHERE product_name = 'Camino';
