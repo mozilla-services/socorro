f40b971d6b62e85eaf70229d29316518ade5bfe5
Josh Berkus <josh@pgexperts.com>
2012-06-11 17:06:15 -0700

    More mobeta database migrations.  Not nearly done yet!

--- sql/upgrade/15.0/support_functions.sql
+++ sql/upgrade/15.0/support_functions.sql
@@ -10,3 +10,32 @@ SELECT substring($1 from $x$^(\d+\.\d+)$x$)::major_version;
 $f$;
 
 
+-- new reports_clean_done, adjusts time interval for some users
+
+begin;
+
+drop function if exists reports_clean_done ( date );
+
+create or replace function reports_clean_done(
+    updateday date, check_period interval default interval '1 hour' )
+returns boolean
+language plpgsql
+as $f$
+-- this function checks that reports_clean has been updated
+-- all the way to the last hour of the UTC day
+BEGIN
+
+PERFORM 1 
+    FROM reports_clean
+    WHERE date_processed BETWEEN ( ( updateday::timestamp at time zone 'utc' ) 
+            +  ( interval '24 hours' - check_period ) )
+        AND ( ( updateday::timestamp at time zone 'utc' ) + interval '1 day' )
+    LIMIT 1;
+IF FOUND THEN
+    RETURN TRUE;
+ELSE
+    RETURN FALSE;
+END IF;
+END; $f$;
+
+commit;
