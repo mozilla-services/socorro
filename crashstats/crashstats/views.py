mozilla-services/socorro

commit 14b0a97a3fa0a16dcaa81105cf80a0a88520d529

progress on tidying up hardcoded urls

crashstats/crashstats/views.py
@@ -4,8 +4,9 @@ import datetime
 import functools
 from collections import defaultdict
 from django import http
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from session_csrf import anonymous_csrf
 
@@ -155,6 +156,16 @@ def topcrasher(request, product=None, versions=None, days=None,
                crash_type=None, os_name=None):
     data = {}
 
+    if not versions:
+        # :(
+        # simulate what the nav.js does which is to take the latest version
+        # for this product.
+        for release in request.currentversions:
+            if release['product'] == product and release['featured']:
+                url = reverse('crashstats.topcrasher',
+                              kwargs=dict(product=product, versions=release['version']))
+                return redirect(url)
+
     if days not in ['1', '3', '7', '14', '28']:
         days = 7
     days = int(days)
@@ -247,14 +258,19 @@ def hangreport(request, product=None, version=None):
 
 
 @set_base_data
-def topchangers(request, product=None, versions=None):
+def topchangers(request, product=None, versions=None, duration=7):
     data = {}
 
-    try:
-        duration = int(request.GET.get('duration', 7))
-        if duration not in (3, 7, 14, 28):
-            raise ValueError('not recognized duration')
-    except ValueError, msg:
+    if request.GET.get('duration'):
+        # the old URL
+        url = reverse('crashstats.topchangers',
+                      kwargs=dict(product=product,
+                                  versions=versions,
+                                  duration=duration))
+        return redirect(url)
+
+    duration = int(duration)
+    if duration not in (3, 7, 14, 28):
         return http.HttpResponseBadRequest(str(msg))
     data['duration'] = duration
 

crashstats/base/templates/crashstats_base.html
@@ -79,18 +79,16 @@
                 <label>Report:</label>
                 <select id="report_select" >
                     <optgroup label=" ">
-                        <option selected value="/products/{{ product }}" {% if report == 'products' %}SELECTED{% endif %}>Overview</option>
+                        <option value="{{ url('crashstats.products', product) }}" {% if report == 'products' %}selected{% endif %}>Overview</option>
                     </optgroup>
                     <optgroup label=" ">
-                        <option value="/daily?p={{ product }}&amp;v[]=" {% if report == 'daily' %}SELECTED{% endif %}>Crashes per User</option>
-                        <option value="/products/{{ product }}/builds" {% if report == 'builds' %}SELECTED{% endif %}>Nightly Builds</option>
-                        <option value="/hangreport/byversion/{{ product }}/" {% if report == 'hangreport' %}SELECTED{% endif %}>Hang Report</option>
+                        <option value="{{ url('crashstats.daily') }}?p={{ product }}&amp;v[]=" {% if report == 'daily' %}selected{% endif %}>Crashes per User</option>
+                        <option value="{{ url('crashstats.builds', product) }}" {% if report == 'builds' %}selected{% endif %}>Nightly Builds</option>
+                        <option value="{{ url('crashstats.hangreport', product) }}" {% if report == 'hangreport' %}selected{% endif %}>Hang Report</option>
                     </optgroup>
                     <optgroup label=" ">
-                        {% if product %}
-                        <option value="{{ url('crashstats.topchangers', product) }}" {% if report == 'topchangers' %}SELECTED{% endif %}>Top Changers</option>
-                        {% endif %}
-                        <option value="/topcrasher/byversion/{{ product }}/" {% if report == 'topcrasher' %}SELECTED{% endif %}>Top Crashers</option>
+                        <option value="{{ url('crashstats.topchangers', product) }}" {% if report == 'topchangers' %}selected{% endif %}>Top Changers</option>
+                        <option value="{{ url('crashstats.topcrasher', product) }}" {% if report == 'topcrasher' %}selected{% endif %}>Top Crashers</option>
                     </optgroup>
                 </select>
             </li>

crashstats/crashstats/templates/crashstats/buginfo.html
@@ -1,1 +1,0 @@
-{{ bugs|safe }}

crashstats/crashstats/templates/crashstats/products.html
@@ -14,9 +14,9 @@
     <div class="page-heading">
         <h2>{{ product }} Crash Data</h2>
          <ul class="options">
-                <li><a href="/products/{{ product }}?duration=3" {% if duration == 3 %} class="selected" {% endif %}>3 days</a></li>
-                <li><a href="/products/{{ product }}?duration=7" {% if duration == 7 %} class="selected" {% endif %}>7 days</a></li>
-                <li><a href="/products/{{ product }}?duration=14" {% if duration == 14 %} class="selected" {% endif %}>14 days</a></li>
+                <li><a href="?duration=3" {% if duration == 3 %} class="selected" {% endif %}>3 days</a></li>
+                <li><a href="?duration=7" {% if duration == 7 %} class="selected" {% endif %}>7 days</a></li>
+                <li><a href="?duration=14" {% if duration == 14 %} class="selected" {% endif %}>14 days</a></li>
             </ul>
     </div>
 
@@ -41,21 +41,21 @@
                 {% if r[0] == product %}
                     {% for v in r[1] %}
                         {% if version == v.version %}
-                <div class="release_channel  border_right">
+                <div class="release_channel border_right">
                     <h4>{{ r[0] }} {{ v.version }}</h4>
                     <ul>
-                      <li><a href="/topcrasher/byversion/{{ r[0] }}/{{ v.version }}/{{ duration }}">Top Crashers</a></li>
+                      <li><a href="{{ url('crashstats.topcrasher', r[0], v.version, duration) }}">Top Crashers</a></li>
                       <li><a href="{{ url('crashstats.topchangers', r[0], v.version) }}?duration={{ duration }}">Top Changers</a></li>
-                      <li><a href="/topcrasher/byversion/{{ r[0] }}/{{ v.version }}/{{ duration }}/plugin">Top Plugin Crashers</a></li>
+                      <li><a href="{{ url('crashstats.topcrasher', r[0], v.version, duration, 'plugin') }}">Top Plugin Crashers</a></li>
                     </ul>
                 </div>
                         {% elif not version and v.featured %}
                 <div class="release_channel  border_right">
                     <h4>{{ r[0] }} {{ v.version }}</h4>
                     <ul>
-                      <li><a href="/topcrasher/byversion/{{ r[0] }}/{{ v.version }}/{{ duration }}">Top Crashers</a></li>
-                      <li><a href="{{ url('crashstats.topchangers', r[0], v.version) }}?duration={{ duration }}">Top Changers</a></li>
-                      <li><a href="/topcrasher/byversion/{{ r[0] }}/{{ v.version }}/{{ duration }}/plugin">Top Plugin Crashers</a></li>
+                      <li><a href="{{ url('crashstats.topcrasher', r[0], v.version, duration) }}">Top Crashers</a></li>
+                      <li><a href="{{ url('crashstats.topchangers', r[0], v.version, duration) }}">Top Changers</a></li>
+                      <li><a href="{{ url('crashstats.topcrasher', r[0], v.version, duration, 'plugin') }}">Top Plugin Crashers</a></li>
                     </ul>
                 </div>
                         {% endif %}

crashstats/crashstats/urls.py
@@ -6,12 +6,15 @@ from . import views
 
 
 urlpatterns = patterns('',
-    url(r'^products/(?P<product>\w+)$', views.products,
+    url(r'^products/(?P<product>\w+)/?$', views.products,
         name='crashstats.products'),
-    url(r'^products/(?P<product>\w+)/versions/(?P<versions>[;\w\.()]+)$',
+    url(r'^products/(?P<product>\w+)/versions/(?P<versions>[;\w\.()]+)/?$',
         views.products,
         name='crashstats.products'),
-    url(r'^topcrasher/byversion/(?P<product>\w+)/(?P<versions>[;\w\.()]+)$',
+    url(r'^topcrasher/byversion/(?P<product>\w+)/?$',
+        views.topcrasher,
+        name='crashstats.topcrasher'),
+    url(r'^topcrasher/byversion/(?P<product>\w+)/(?P<versions>[;\w\.()]+)/?$',
         views.topcrasher,
         name='crashstats.topcrasher'),
     url(r'^topcrasher/byversion/(?P<product>\w+)/(?P<versions>[;\w\.()]+)/(?P<days>\d+)$',
@@ -37,11 +40,14 @@ urlpatterns = patterns('',
     url(r'^products/(?P<product>\w+)/versions/(?P<versions>[\w\.()]+)/topchangers$',
         views.topchangers,
         name='crashstats.topchangers'),
+    url(r'^products/(?P<product>\w+)/versions/(?P<versions>[\w\.()]+)/topchangers/(?P<duration>\d+)/?$',
+        views.topchangers,
+        name='crashstats.topchangers'),
     url(r'^report/list$', views.report_list,
         name='crashstats.report_list'),
     url(r'^report/index/(?P<crash_id>.*)$', views.report_index,
         name='crashstats.report_index'),
-    url(r'^query$', views.query,
+    url(r'^query/?$', views.query,
         name='crashstats.query'),
     url(r'^buginfo/bug', views.buginfo,
         name='crashstats.buginfo'),

media/js/crashstats/socorro/nav.js
@@ -33,14 +33,17 @@ $(document).ready(function () {
     if ($("#report_select")) {
         $("#report_select").change(function () {
             report = $("#report_select").val();
-            
+
             // Handle top crasher selection. If no version was selected in the version drop-down
             // select the top most version and append to the URL.
             if(report.indexOf('topcrasher') !== -1) {
                 var selectedVersion = $("#product_version_select").val();
-                
+
                 if(selectedVersion === "Current Versions") {
                     selectedVersion = $("#product_version_select").find("option:eq(1)").val();
+                    if (report.charAt(report.length - 1) != '/') {
+                        report += '/';
+                    }
                     window.location = report + selectedVersion;
                 } else {
                     window.location = report;

commit 31a7d1202beabef7cf867e4bb5f859dcd666e05f

fix topcrasher version passing regression from 14b0a97

crashstats/crashstats/views.py
@@ -165,6 +165,11 @@ def topcrasher(request, product=None, versions=None, days=None,
                 url = reverse('crashstats.topcrasher',
                               kwargs=dict(product=product, versions=release['version']))
                 return redirect(url)
+    else:
+        versions = versions.split(';')
+
+    if len(versions) == 1:
+        data['version'] = versions[0]
 
     if days not in ['1', '3', '7', '14', '28']:
         days = 7
@@ -184,7 +189,7 @@ def topcrasher(request, product=None, versions=None, days=None,
     data['os_name'] = os_name
 
     api = models.TCBS()
-    tcbs = api.get(product, versions, crash_type, end_date,
+    tcbs = api.get(product, data['version'], crash_type, end_date,
                     duration=(days * 24), limit='300')
 
     signatures = [c['signature'] for c in tcbs['crashes']]