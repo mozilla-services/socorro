b629fbb1c5f7faf45b5f1e9f88cf592676e00dde
Lonnen <chris.lonnen@gmail.com>
2012-06-15 19:20:12 -0700

    move the mpl 2 declarations into safe places

--- webapp-php/js/socorro/admin.js
+++ webapp-php/js/socorro/admin.js
@@ -16,24 +16,24 @@ function hideShow(hideId, showId) {
 }
 
 $(document).ready(function(){
-    
+
     var dataSourcesTabs = $('#data_sources');
-    
+
 	/* Emails */
     $('input[name=email_start_date][type=text], input[name=email_end_date][type=text]').datepicker({
 		dateFormat: "dd/mm/yy"
 	});
-	
+
 	/* Add new */
 	$("#start_date, #end_date").datepicker({
 		dateFormat: "yy/mm/dd"
 	});
-	
+
 	/* Update */
 	$("#update_start_date, #update_end_date").datepicker({
 		dateFormat: "yy/mm/dd"
 	});
-	
+
     $('input[name=submit][type=submit][value="OK, Send Emails"]').click(function(){
       postData = {token: $('input[name=token]').val(),
                   campaign_id: $('input[name=campaign_id]').val(),
@@ -48,7 +48,7 @@ $(document).ready(function(){
     });
 
     $('.admin tbody tr:odd').css('background-color', '#efefef');
-    
+
     if(dataSourcesTabs.length) {
         dataSourcesTabs.tabs({
             cookie: {
@@ -56,7 +56,7 @@ $(document).ready(function(){
             }
         }).show();
     }
-    
+
     //hide the loader
     $("#loading-bds").hide();
 });
