eb557d49b8359e59b5c7159f1702bdd5da24c8d9
Josh Berkus <josh@agliodbs.com>
2012-06-27 14:45:25 -0700

    fixed bug.

--- sql/upgrade/15.0/create_refresh_date.sql
+++ sql/upgrade/15.0/create_refresh_date.sql
@@ -4,9 +4,10 @@ SELECT add_column_if_not_exists('socorro_db_version','refreshed_at','timestamptz
 
 CREATE OR REPLACE FUNCTION socorro_db_data_refresh(
 	refreshtime timestamptz default null )
-RETURNS BOOLEAN
+RETURNS TIMESTAMPTZ
 LANGUAGE sql AS $f$
-UPDATE socorro_db_version SET refreshed_at = COALESCE($1, now());
+UPDATE socorro_db_version SET refreshed_at = COALESCE($1, now())
+RETURNING refreshed_at;
 $f$;
 
 SELECT socorro_db_data_refresh('2012-07-01');
