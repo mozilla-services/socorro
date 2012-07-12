c07a2c641c7cf71538f3222bad598d8a8c497ff3
Josh Berkus <josh@agliodbs.com>
2012-07-12 15:21:35 -0700

    Bugfixes per testing on crash-stats-dev.

--- sql/upgrade/18.0/backfill_mobeta.py
+++ sql/upgrade/18.0/backfill_mobeta.py
@@ -12,7 +12,7 @@ parser.add_option("-D", "--database", dest="dbname",
                   default="breakpad")
 parser.add_option("-w", "--weeks", dest="weeks",
                   help="weeks to backfill", metavar="WEEKS",
-                  default="2")
+                  type="int", default=2)
 (options, args) = parser.parse_args()
 
 # calculate weeks
@@ -24,7 +24,8 @@ def funcdateloop( mycur, starts, ends, fillfunc ):
 	curdate = starts
 	while (curdate <= ends):
 		qrytext = """SELECT %s('%s',false)""" % ( fillfunc, curdate.strftime("%Y-%m-%d"), )
-		mycur.execute(qrytxt)
+		print qrytext
+		mycur.execute(qrytext)
 		curdate = curdate + datetime.timedelta(days=1)
 
 conn = psycopg2.connect("dbname=%s user=postgres"
@@ -33,7 +34,7 @@ cur = conn.cursor()
 
 # populate build_adu back X weeks
 
-print 'backfilling %d weeks of build_adu'
+print 'backfilling %d weeks of build_adu' % options.weeks
 funcdateloop(cur, startdate, enddate, 'update_build_adu');
 
 # copy over daily_crashes back to the beginning of product_adu
@@ -42,36 +43,38 @@ funcdateloop(cur, startdate, enddate, 'update_build_adu');
 
 print 'copying data from daily_crashes to crashes_by_user'
 
-cur.execute("""INSERT INTO crashes_by_user (
+cur.execute("""INSERT INTO crashes_by_user 
 	( product_version_id, os_short_name, crash_type_id,
 		report_date, report_count, adu )
-	SELECT productdims_id, os_short_name, crash_type_id,
+	SELECT productdims_id, os_names.os_short_name, crash_type_id,
 		adu_day, count, adu_count
 	FROM daily_crashes JOIN product_versions
 		ON productdims_id = product_versions.product_version_id
 		JOIN crash_types ON report_type = old_code
+            JOIN os_names ON daily_crashes.os_short_name = os_names.os_short_name
 	    JOIN product_adu ON productdims_id = product_adu.product_version_id
+                 and product_adu.os_name = os_names.os_name
 	WHERE adu_day < %s """, ( startdate, ) )
 
 # now backfill the rest
-print 'backfilling %d weeks of crash_by_user'
-funcdateloop(cur, startdate, enddate, 'update_crash_by_user');
+print 'backfilling %d weeks of crash_by_user' % options.weeks
+funcdateloop(cur, startdate, enddate, 'update_crashes_by_user');
 
 # populate crash_by_user_build back X weeks
 
-print 'backfilling %d weeks of crash_by_user_build'
-funcdateloop(cur, startdate, enddate, 'update_crash_by_user_build');
+print 'backfilling %d weeks of crash_by_user_build' % options.weeks
+funcdateloop(cur, startdate, enddate, 'update_crashes_by_user_build');
 
 # populate home_page_graph back X weeks
-print 'backfilling %d weeks of home_page_graph'
+print 'backfilling %d weeks of home_page_graph' % options.weeks
 funcdateloop(cur, startdate, enddate, 'update_home_page_graph');
 
 # populate home_page_graph_build back X weeks
-print 'backfilling %d weeks of home_page_graph_build'
+print 'backfilling %d weeks of home_page_graph_build' % options.weeks
 funcdateloop(cur, startdate, enddate, 'update_home_page_graph_build');
 
 # populate tcbs_build back X weeks
-print 'backfilling %d weeks of tcbs_build'
+print 'backfilling %d weeks of tcbs_build' % options.weeks
 funcdateloop(cur, startdate, enddate, 'update_tcbs_build');
 
 print 'done backfilling'
