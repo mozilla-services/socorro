b1d340928aa675630a478a2fc56b01d18cd2a47c
Robert Helmer <rhelmer@rhelmer.org>
2012-07-25 16:18:33 -0700

    bug 777549 - add a quick wrapper for crontabber to add proper locking

---
+++ scripts/crons/crontabber.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+. /etc/socorro/socorrorc
+
+NAME=`basename $0 .sh`
+lock $NAME
+${APPDIR}/socorro/cron/crontabber.py --admin.conf=/data/socorro/application/config/crontabber.ini >> /var/log/socorro/crontabber.log 2>&1
+EXIT_CODE=$?
+unlock $NAME
+
+exit $EXIT_CODE
