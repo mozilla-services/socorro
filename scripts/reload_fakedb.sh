f59445bf25bc308e9ead06c0d5cd685caabed5af
Robert Helmer <rhelmer@rhelmer.org>
2012-08-07 16:08:03 -0700

    load old-fashioned way until setupdb_app is fixed

--- scripts/reload_fakedb.sh
+++ scripts/reload_fakedb.sh
@@ -3,8 +3,12 @@
 source socorro-virtualenv/bin/activate
 export PYTHONPATH=.
 
-# create empty DB named "fakedata"
-./socorro/external/postgresql/setupdb_app.py --database_name=fakedata --database_password=fakedata --dropdb
+#  create empty DB named "fakedata"
+# FIXME - setupdb_app doesn't seem to honor psql escape settings, load the old fashioned way for now
+#./socorro/external/postgresql/setupdb_app.py --database_name=fakedata --database_password=fakedata --dropdb
+dropdb fakedata
+createdb fakedata
+psql -f sql/schema.sql fakedata > schema.log
 
 # generate fakedata
 ./socorro/external/postgresql/fakedata.py > load.sql
