53e2b28214346f81e422bd4ed6770f7d71f8b49a
Josh Berkus <josh@agliodbs.com>
2012-09-14 17:38:10 -0700

    Modifications to minidb creation following mobeta, initial version for testing.

--- scripts/staging/postsql/performance_check_1.sql
+++ scripts/staging/postsql/performance_check_1.sql
@@ -1,8 +1,21 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-CREATE OR REPLACE VIEW performance_check_1 AS
-SELECT 1;
 
-ALTER VIEW performance_check_1 OWNER TO ganglia;
+CREATE VIEW performance_check_1 AS
+    SELECT sum(report_count) FROM tcbs
+    WHERE report_date BETWEEN ( current_date - 7 ) and current_date;
+
+
+ALTER TABLE public.performance_check_1 OWNER TO ganglia;
+
+--
+-- Name: performance_check_1; Type: ACL; Schema: public; Owner: ganglia
+--
+
+REVOKE ALL ON TABLE performance_check_1 FROM PUBLIC;
+REVOKE ALL ON TABLE performance_check_1 FROM ganglia;
+GRANT ALL ON TABLE performance_check_1 TO ganglia;
+GRANT SELECT ON TABLE performance_check_1 TO breakpad;
+GRANT SELECT ON TABLE performance_check_1 TO breakpad_ro;
+GRANT ALL ON TABLE performance_check_1 TO monitor;
+
+
