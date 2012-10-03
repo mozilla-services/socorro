8f0f55c00e3a3395efb950ebb6948756ed7c8dd3
Robert Helmer <rhelmer@rhelmer.org>
2012-10-03 11:23:45 -0700

    bug 791218 - add metrofirefox to socorro

---
+++ sql/upgrade/34.0/upgrade.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#please see README
+
+set -e
+
+CURDIR=$(dirname $0)
+DBNAME=$1
+: ${DBNAME:="breakpad"}
+VERSION=34.0
+
+#echo '*********************************************************'
+#echo 'support functions'
+#psql -f ${CURDIR}/support_functions.sql $DBNAME
+
+echo '*********************************************************'
+echo 'add MetroFirefox to database'
+echo 'bug 791218'
+psql -f ${CURDIR}/add_metrofirefox.sql $DBNAME
+psql -f ${CURDIR}/update_product_versions.sql $DBNAME
+
+#change version in DB
+psql -c "SELECT update_socorro_db_version( '$VERSION' )" $DBNAME
+
+echo "$VERSION upgrade done"
+
+exit 0
