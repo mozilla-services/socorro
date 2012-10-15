15b1a1a1b8cc7e69b97ea587a7c91b72242da9bf
Selena Deckelmann <selenamarie@gmail.com>
2012-10-15 09:16:35 -0700

    Missing a BEGIN\!

--- sql/upgrade/21.0/update_hang_report.sql
+++ sql/upgrade/21.0/update_hang_report.sql
@@ -1,5 +1,7 @@
 \set ON_ERROR_STOP 1
 
+BEGIN;
+
 CREATE OR REPLACE FUNCTION public.update_hang_report(updateday date, checkdata boolean DEFAULT true, check_period interval DEFAULT '01:00:00'::interval)
  RETURNS boolean
  LANGUAGE plpgsql
