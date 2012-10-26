6138496ef530c8cb01bcbfc8c752eb1fdbd35d7d
ossreleasefeed <sneethling@mozilla.com>
2012-10-26 12:16:26 +0200

    ensure top plugin crasher links for build date works fixes bug 805617

--- crashstats/crashstats/static/crashstats/js/socorro/homepage_tmpl.js
+++ crashstats/crashstats/static/crashstats/js/socorro/homepage_tmpl.js
@@ -6,5 +6,5 @@ var crashReportsByVersionTmpl = "{{#product_versions}}<div class='release_channe
     crashReportsByBuildDateTmpl = "{{#product_versions}}<div class='release_channel'><h4>{{product}} {{version}}</h4><ul>" +
                                   "<li><a href='/topcrasher/products/{{product}}/versions/{{version}}/date_range_type/{{date_range_type}}?days={{duration}}'>Top Crashers</a></li>" +
                                   "<li><a href='/topchangers/products/{{product}}/versions/{{version}}?days={{duration}}'>Top Changers</a></li>" +
-                                  "<li><a href='/topcrasher/products/{{product}}/versions/{{version}}/crash_type/plugin/date_range_type/{{date_range_type}}?days={{duration}}'>Top Plugin Crashers</a></li>" +
+                                  "<li><a href='/topcrasher/products/{{product}}/versions/{{version}}/date_range_type/{{date_range_type}}/crash_type/plugin?days={{duration}}'>Top Plugin Crashers</a></li>" +
                                   "</div>{{/product_versions}}";
