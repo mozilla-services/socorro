837f197261642a671ec2904d6aabac41fe678512
Selena Deckelmann <selenamarie@gmail.com>
2012-10-31 13:35:28 -0700

    Added copyleft header to schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,6 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
 -- PostgreSQL database dump
 --
@@ -9876,5 +9879,3 @@ ALTER DEFAULT PRIVILEGES FOR ROLE breakpad_rw GRANT SELECT ON TABLES  TO breakpa
 --
 -- PostgreSQL database dump complete
 --
-
--- CREATE EXTENSION citext from unpackaged;
