45d081fbddee691e4d71b9509b0db339d324cb81
Peter Bengtsson <mail@peterbe.com>
2013-04-22 16:02:48 -0700

    fixes bug 864548 - lib64 to sys.path by default, r=rhelmer

--- manage.py
+++ manage.py
@@ -24,13 +24,19 @@ except ImportError:
 
 manage.setup_environ(__file__, more_pythonic=True)
 
-# FIXME funfactory should add this too
-# (see https://github.com/mozilla/funfactory/pull/50)
+# We build binary packages on jenkins which installs itself
+# in vendor-local/lib64/python
+# Add it to sys.path just after vendor-local/lib/python which
+# funfactory already added
+_new_path = manage.path('vendor-local/lib64/python')
 site.addsitedir(
     os.path.abspath(
-        manage.path('vendor-local/lib64/python')
+        _new_path
     )
 )
+# now re-arrange so the order is right
+_other_path = manage.path('vendor-local/lib/python')
+sys.path.insert(sys.path.index(_other_path) + 1, _new_path)
 
 
 if __name__ == "__main__":
