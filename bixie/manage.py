fa1d409c167d01586abbc4d36a4f07b0fb1cd3bf
lonnen <chris.lonnen@gmail.com>
2013-04-24 13:33:43 -0700

    create the bixie reporter
    created a new playdoh app and exported it using `git archive` piped into
    `tar`. this did not include the submodule vendor from funfactory. next,
    we'll yank out funfactory and remove any submodules from this dir.

---
+++ bixie/manage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import sys
+
+# Edit this if necessary or override the variable in your environment.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bixie.settings')
+
+# Add a temporary path so that we can import the funfactory
+tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'vendor', 'src', 'funfactory')
+# Comment out to load funfactory from your site packages instead
+sys.path.insert(0, tmp_path)
+
+from funfactory import manage
+
+# Let the path magic happen in setup_environ() !
+sys.path.remove(tmp_path)
+
+
+manage.setup_environ(__file__, more_pythonic=True)
+
+if __name__ == "__main__":
+    manage.main()
