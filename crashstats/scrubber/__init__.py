983cce83937ceff68f8e5780fae7b0a911daec7b
Adrian Gaudebert <adrian@gaudebert.fr>
2013-04-25 23:03:51 +0200

    Bug 858245 - Added a scrubber app to use in the API. r=peterbe

---
+++ crashstats/scrubber/__init__.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+
+import re
+
+
+# source: http://stackp.online.fr/?p=19
+EMAIL = re.compile('([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)')
+
+
+# source: http://stackoverflow.com/questions/520031
+URL = re.compile(
+    r"((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.‌​]"
+    "[a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]"
+    "+|(‌​([^\s()<>]+)))*)|[^\s`!()[]{};:'\".,<>?«»“”‘’]))",
+    re.DOTALL
+)
+
+
+def scrub_data(data, **kwargs):
+    """Return a scrubbed copy of a list of dictionaries.
+
+    See `scrub_dict` for parameters.
+    """
+    scrubbed = list(data)
+    for i, item in enumerate(scrubbed):
+        scrubbed[i] = scrub_dict(item, **kwargs)
+    return scrubbed
+
+
+def scrub_dict(
+    data,
+    remove_fields=None,
+    replace_fields=None,
+    clean_fields=None
+):
+    """Return a scrubbed copy of a dictionary.
+
+    Several options are available:
+    * remove_fields
+        * list or tuple of strings
+        * remove those fields from the dictionary
+        * example: remove_fields=['email', 'phone']
+    * replace_fields
+        * list or tuple of 2-uples
+        * replace the value of those fields with some content
+        * example: replace_fields=[('email', 'scrubbed email'), ('phone', '')]
+    * clean_fields
+        * list or tuple of 2-uples
+        * search for patterns in those fields and remove what matches
+        * example: clean_fields=[('comment', EMAIL), ('comment', URL)]
+
+    Any number of those options can be used in the same call. If none is used,
+    return the dictionary unchanged.
+    """
+    scrubbed = data.copy()
+    for key in remove_fields or []:
+        if key in scrubbed:
+            del scrubbed[key]
+
+    for key in scrubbed:
+        for field in replace_fields or []:
+            if field[0] == key:
+                scrubbed[key] = field[1]
+
+        for field in clean_fields or []:
+            if field[0] == key:
+                scrubbed[key] = scrub_string(scrubbed[key], field[1])
+
+    return scrubbed
+
+
+def scrub_string(data, pattern):
+    """Return a copy of a string where everything that matches the pattern is
+    removed.
+    """
+    for i in pattern.findall(data):
+        data = data.replace(i[0], '')
+    return data
