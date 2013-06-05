mozilla-services/socorro

commit e927e7e0b56c9503e93b217940a23a40665b48f1

Tersen lambda handling in TransactionExecutor constructor.

socorro/database/transaction_executor.py
@@ -14,13 +14,10 @@ class TransactionExecutor(RequiredConfig):
 
     #--------------------------------------------------------------------------
     def __init__(self, config, db_conn_context_source,
-                 quit_check_callback=None):
+                 quit_check_callback=lambda: False):
         self.config = config
         self.db_conn_context_source = db_conn_context_source
-        if quit_check_callback:
-            self.quit_check = quit_check_callback
-        else:
-            self.quit_check = lambda: False
+        self.quit_check = quit_check_callback
         self.do_quit_check = True
 
     #--------------------------------------------------------------------------

commit fcd8369e2f22d295b4cf577a7aa27dee4a11a38d

Fix quit_check defaulting I broke in e927e7e0b56c9503e93b217940a23a40665b48f1.
Not sure how my tests passed before; maybe something changed between when I wrote it and when I rebased and landed it.

socorro/database/transaction_executor.py
@@ -14,10 +14,10 @@ class TransactionExecutor(RequiredConfig):
 
     #--------------------------------------------------------------------------
     def __init__(self, config, db_conn_context_source,
-                 quit_check_callback=lambda: False):
+                 quit_check_callback=None):
         self.config = config
         self.db_conn_context_source = db_conn_context_source
-        self.quit_check = quit_check_callback
+        self.quit_check = quit_check_callback or (lambda: False)
         self.do_quit_check = True
 
     #--------------------------------------------------------------------------