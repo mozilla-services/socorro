603cbbb497f26f853f70bab560cc41f8dd0f1375
Adrian Gaudebert <adrian@gaudebert.fr>
2013-06-07 15:56:57 +0200

    Fixes bug 872223 - Added a field service to return data_dictionary information. r=peterbe

---
+++ socorro/external/postgresql/field.py
@@ -0,0 +1,55 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+import logging
+
+from socorro.external import MissingOrBadArgumentError
+from socorro.external.postgresql.base import PostgreSQLBase
+from socorro.lib import external_common
+
+
+logger = logging.getLogger('webapi')
+
+
+class Field(PostgreSQLBase):
+    '''Implement the /field service with PostgreSQL. '''
+
+    def get(self, **kwargs):
+        '''Return data about a field from its name. '''
+        filters = [
+            ('name', None, 'str'),
+        ]
+        params = external_common.parse_arguments(filters, kwargs)
+
+        if not params.name:
+            raise MissingOrBadArgumentError(
+                'Mandatory parameter "name" is missing or empty'
+            )
+
+        sql = '''/* socorro.external.postgresql.field.Field.get */
+            SELECT
+                raw_field,
+                transforms,
+                product
+            FROM data_dictionary
+            WHERE raw_field=%(name)s
+        '''
+
+        error_message = 'Failed to retrieve field data from PostgreSQL'
+        results = self.query(sql, params, error_message=error_message)
+
+        field_data = {
+            'name': None,
+            'transforms': None,
+            'product': None
+        }
+
+        if not results:
+            return field_data
+
+        field_data = dict(zip(('name', 'transforms', 'product'), results[0]))
+        field_data['transforms'] = json.loads(field_data['transforms'])
+
+        return field_data
---
+++ socorro/unittest/external/postgresql/test_field.py
@@ -0,0 +1,82 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from nose.plugins.attrib import attr
+
+from socorro.external import MissingOrBadArgumentError
+from socorro.external.postgresql.field import Field
+from .unittestbase import PostgreSQLTestCase
+
+
+@attr(integration='postgres')  # for nosetests
+class IntegrationTestField(PostgreSQLTestCase):
+    '''Test socorro.external.postgresql.field.Field class. '''
+
+    def setUp(self):
+        super(IntegrationTestField, self).setUp()
+
+        cursor = self.connection.cursor()
+
+        cursor.execute('''
+            INSERT INTO data_dictionary
+            (raw_field, transforms, product)
+            VALUES
+            (
+                'field1',
+                '{}',
+                'WaterWolf'
+            ),
+            (
+                'field2',
+                '{"processor": "some notes"}',
+                'WaterWolf'
+            );
+        ''')
+
+        self.connection.commit()
+
+    def tearDown(self):
+        '''Clean up the database, delete tables and functions. '''
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            TRUNCATE data_dictionary CASCADE
+        ''')
+        self.connection.commit()
+        super(IntegrationTestField, self).tearDown()
+
+    def test_get(self):
+        api = Field(config=self.config)
+
+        # expect a result
+        res = api.get(name='field1')
+        res_expected = {
+            'name': 'field1',
+            'transforms': {},
+            'product': 'WaterWolf'
+        }
+
+        self.assertEqual(res, res_expected)
+
+        # expect a result
+        res = api.get(name='field2')
+        res_expected = {
+            'name': 'field2',
+            'transforms': {'processor': 'some notes'},
+            'product': 'WaterWolf'
+        }
+
+        self.assertEqual(res, res_expected)
+
+        # expect no result
+        res = api.get(name='i-do-not-exist')
+        res_expected = {
+            'name': None,
+            'transforms': None,
+            'product': None
+        }
+
+        self.assertEqual(res, res_expected)
+
+        # expect a failure
+        self.assertRaises(MissingOrBadArgumentError, api.get)
