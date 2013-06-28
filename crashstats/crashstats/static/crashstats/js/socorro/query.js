67afc0d60212f73047de2fa5ccb75c148dc7529b
Adrian Gaudebert <adrian@gaudebert.fr>
2013-06-28 02:27:28 +0200

    Fixes bug 853633 - Upgraded jQuery to version 2.0.2 and jQuery UI to version 1.10.3.

--- crashstats/crashstats/static/crashstats/js/socorro/query.js
+++ crashstats/crashstats/static/crashstats/js/socorro/query.js
@@ -119,7 +119,7 @@
         }).trigger('change');
 
         // Results table sorting
-        $('#dateHelp *').tooltip();
+        $('#date').tooltip();
         $('#signatureList').tablesorter();
 
         // Upon submitting the form, hide the submit button and disable the
