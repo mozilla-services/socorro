c63f7107ccab65958c9024cea530116f5fbd9b9f
Peter Bengtsson <mail@peterbe.com>
2013-07-29 16:11:27 -0700

    allow more fields in processed and raw crash

--- webapp-django/crashstats/crashstats/models.py
+++ webapp-django/crashstats/crashstats/models.py
@@ -712,6 +712,24 @@ class ProcessedCrash(SocorroMiddleware):
         'user_comments',
         'uuid',
         'version',
+        'install_age'
+        'startedDateTime',
+        'java_stack_trace',
+        'crashedThread',
+        'cpu_info',
+        'pluginVersion',
+        'hang_type',
+        'distributor',
+        'topmost_filenames',
+        'additional_minidumps',
+        'processor_notes',
+        'app_notes',
+        'crash_time',
+        'Winsock_LSP',
+        'productid',
+        'pluginFilename',
+        'pluginName',
+        'addons',
     )
 
     API_CLEAN_SCRUB = (
@@ -802,6 +820,7 @@ class RawCrash(SocorroMiddleware):
         'Android_Manufacturer',
         'Android_CPU_ABI',
         'Android_CPU_ABI2',
+        'throttle_rate',
     )
 
     def get(self, **kwargs):
