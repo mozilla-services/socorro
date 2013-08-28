132a177459a7bfdad23890e0f49aa76f2befdb18
Selena Deckelmann <selenamarie@gmail.com>
2013-08-28 09:36:49 -0700

    Bugfix and doc update for build-related matviews

--- socorro/external/postgresql/raw_sql/procs/update_crashes_by_user_build.sql
+++ socorro/external/postgresql/raw_sql/procs/update_crashes_by_user_build.sql
@@ -44,95 +44,161 @@ IF NOT FOUND THEN
 END IF;
 
 -- now insert the new records
--- first, nightly and aurora are fairly straightforwards
+-- first, nightly and aurora are fairly straightforward
 
-INSERT INTO crashes_by_user_build
-    ( product_version_id, report_date,
-      build_date, report_count, adu,
-      os_short_name, crash_type_id )
-SELECT product_version_id, updateday,
-    count_reports.build_date, report_count, adu_sum,
-    os_short_name, crash_type_id
-FROM ( select product_versions.product_version_id,
-            count(DISTINCT reports_clean.uuid) as report_count,
-            os_names.os_name, os_short_name, crash_type_id,
-            build_date(build_id) as build_date
-      FROM product_versions
-      	JOIN product_version_builds USING (product_version_id)
-      	CROSS JOIN crash_types
-      	CROSS JOIN os_names
-      	LEFT OUTER JOIN reports_clean ON
-      		product_versions.product_version_id = reports_clean.product_version_id
-      		and utc_day_is(date_processed, updateday)
-      		AND reports_clean.process_type = crash_types.process_type
-      		AND ( reports_clean.hang_id IS NOT NULL ) = crash_types.has_hang_id
-      		AND reports_clean.os_name = os_names.os_name
-      		AND reports_clean.release_channel IN ('nightly','aurora')
-      		AND product_version_builds.build_id = reports_clean.build
-      WHERE
-          -- only accumulate data for each build for 7 days after build
-          updateday <= ( build_date(build_id) + 6 )
-      GROUP BY product_versions.product_version_id, os_names.os_name, os_short_name, crash_type_id,
-      	build_date(build_id)
-    ) as count_reports
-      JOIN
-    ( select product_version_id,
-        sum(adu_count) as adu_sum,
-        os_name, build_date
-        from build_adu
-        where adu_date = updateday
-        group by product_version_id, os_name, build_date ) as sum_adu
-      USING ( product_version_id, os_name, build_date )
-      JOIN product_versions USING ( product_version_id )
-ORDER BY product_version_id;
-
--- rapid beta needs to be inserted with the productid of the
--- parent beta product_version instead of its
--- own product_version_id.
-
-INSERT INTO crashes_by_user_build
-    ( product_version_id, report_date,
-      build_date, report_count, adu,
-      os_short_name, crash_type_id )
-SELECT rapid_beta_id, updateday,
-    count_reports.build_date, report_count, adu_sum,
-    os_short_name, crash_type_id
-FROM ( select rapid_beta_id AS product_version_id,
-            count(distinct reports_clean.uuid) as report_count,
-            os_names.os_name, os_short_name, crash_type_id,
-            build_date(build_id) as build_date
-      FROM product_versions
+INSERT INTO crashes_by_user_build (
+    product_version_id
+    , report_date
+    , build_date
+    , report_count
+    , adu
+    , os_short_name
+    , crash_type_id
+)
+WITH count_reports AS (
+    select
+        product_versions.product_version_id
+        , count(DISTINCT reports_clean.uuid) as report_count
+        , os_names.os_name
+        , os_short_name
+        , crash_type_id
+        , build_date(build_id) as build_date
+    FROM product_versions
         JOIN product_version_builds USING (product_version_id)
-      	CROSS JOIN crash_types
-      	CROSS JOIN os_names
-      	LEFT OUTER JOIN reports_clean ON
-      		product_versions.product_version_id = reports_clean.product_version_id
-      		and utc_day_is(date_processed, updateday)
-      		AND reports_clean.process_type = crash_types.process_type
-      		AND ( reports_clean.hang_id IS NOT NULL ) = crash_types.has_hang_id
-      		AND reports_clean.os_name = os_names.os_name
-      		AND reports_clean.release_channel = 'beta'
-      		AND reports_clean.build = product_version_builds.build_id
-      WHERE
-          -- only accumulate data for each build for 7 days after build
-          updateday <= ( build_date(build_id) + 6 )
-          AND product_versions.rapid_beta_id IS NOT NULL
-      GROUP BY rapid_beta_id, os_names.os_name, os_short_name, crash_type_id,
-      	build_date(build_id)
-      	) as count_reports
-      JOIN
-    ( select product_version_id,
+        CROSS JOIN crash_types
+        CROSS JOIN os_names
+        LEFT OUTER JOIN reports_clean ON
+            product_versions.product_version_id = reports_clean.product_version_id
+        AND utc_day_is(date_processed, updateday)
+        AND reports_clean.process_type = crash_types.process_type
+        AND ( reports_clean.hang_id IS NOT NULL ) = crash_types.has_hang_id
+        AND reports_clean.os_name = os_names.os_name
+        AND reports_clean.release_channel IN ('nightly','aurora')
+        AND product_version_builds.build_id = reports_clean.build
+    WHERE
+        -- only accumulate data for each build for 7 days after build
+        updateday <= ( build_date(build_id) + 6 )
+    GROUP BY
+        product_versions.product_version_id
+        , os_names.os_name
+        , os_short_name
+        , crash_type_id
+        , build_date(build_id)
+),
+sum_adu AS (
+    select
+        product_version_id,
         sum(adu_count) as adu_sum,
         os_name, build_date
-        from build_adu
-        where adu_date = updateday
-        group by product_version_id, os_name, build_date ) as sum_adu
-      USING ( product_version_id, os_name, build_date )
-      JOIN product_versions USING ( product_version_id )
+    FROM build_adu
+    WHERE adu_date = updateday
+    group by
+        product_version_id
+        , os_name
+        , build_date
+)
+SELECT
+    product_version_id
+    , updateday
+    , count_reports.build_date
+    , report_count
+    , adu_sum
+    , os_short_name
+    , crash_type_id
+FROM
+    count_reports
+    JOIN sum_adu USING ( product_version_id, os_name, build_date )
+    JOIN product_versions USING ( product_version_id )
 ORDER BY product_version_id;
 
+-- The following query only inserts counts for product_versions with
+-- is_rapid_beta == True and for beta versions which are 7 days old or newer,
+-- and have their rapid_beta_id set.
+
+INSERT INTO crashes_by_user_build (
+    product_version_id
+    , report_date
+    , build_date
+    , report_count
+    , adu
+    , os_short_name
+    , crash_type_id
+)
+WITH count_reports AS (
+    select
+        rapid_beta_id AS product_version_id
+        -- This is a confusing alias that allows us to utilize USING
+        -- in a later query. Possibly ill-advised renaming.
+        , count(distinct reports_clean.uuid) as report_count
+        , os_names.os_name
+        -- Including the os_name because build_adu uses this instead of
+        -- the short name. Might want to change this to just os_short_name.
+        , os_short_name
+        , crash_type_id
+        , build_date(build_id) as build_date
+    FROM product_versions
+        JOIN product_version_builds USING (product_version_id)
+        CROSS JOIN crash_types
+        CROSS JOIN os_names
+        -- These cross joins produce all the possible combinations of
+        -- crash types and os_names, resulting in some counts being zero
+        -- if we didn't see any crashes for those combos on a given day.
+        -- Which explains the left outer join below -- which allows us
+        -- to have zero crashes for certain combos.
+        LEFT OUTER JOIN reports_clean ON
+            product_versions.product_version_id = reports_clean.product_version_id
+            and utc_day_is(date_processed, updateday)
+            AND reports_clean.process_type = crash_types.process_type
+            AND ( reports_clean.hang_id IS NOT NULL ) = crash_types.has_hang_id
+            AND reports_clean.os_name = os_names.os_name
+            AND reports_clean.release_channel = 'beta'
+            AND reports_clean.build = product_version_builds.build_id
+      WHERE
+            updateday <= ( build_date(build_id) + 6 )
+            -- only accumulate data for each build for 7 days after build
+            AND rapid_beta_id IS NOT NULL
+            -- Query is exclusive to betas which have been associated
+            -- with a product_version with is_rapid_beta == True
+      GROUP BY
+        rapid_beta_id
+        , os_names.os_name
+        , os_short_name
+        , crash_type_id
+        , build_date(build_id)
+),
+sum_adu AS (
+    select
+        product_version_id
+        , sum(adu_count) as adu_sum
+        , os_name
+        , build_date
+    FROM build_adu
+        JOIN product_versions USING (product_version_id)
+    WHERE
+        adu_date = updateday
+        and is_rapid_beta
+        -- We're only concerned about rapid_beta releases.
+        -- update_build_adu calculates aggregates of the last
+        -- 7 days of betas for a particular group of releases
+    GROUP BY
+        product_version_id
+        , os_name
+        , build_date
+        -- We don't need a group by for crash type because
+        -- ADU is global, unrelated to kind of crash
+)
+SELECT
+    product_version_id
+    , updateday
+    , count_reports.build_date
+    , report_count
+    , adu_sum
+    , os_short_name
+    , crash_type_id
+FROM
+    count_reports
+    JOIN sum_adu USING ( product_version_id, os_name, build_date )
+ORDER BY product_version_id;
 
 RETURN TRUE;
 END; $$;
-
-
