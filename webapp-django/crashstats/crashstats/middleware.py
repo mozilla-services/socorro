d1ca4d1dbc3edbd848a776c45c36479a1507a38f
Gabi Thume <gabithume@gmail.com>
2013-09-04 14:04:41 -0300

    fixes Bug 912552 - statsd failing on non-ascii characters

--- webapp-django/crashstats/crashstats/middleware.py
+++ webapp-django/crashstats/crashstats/middleware.py
@@ -1,4 +1,5 @@
 from django_statsd.clients import statsd
+import urllib
 
 
 class AnalyticsMiddleware(object):
@@ -8,6 +9,8 @@ class AnalyticsMiddleware(object):
 
     def process_response(self, request, response):
         path = request.path_info.lstrip('/').replace('.', '-')
+        path = urllib.quote(path.encode('utf-8'))
+
         # avoid sending lots of low-volume or unique keys
         prefixes = ('rawdumps/', 'report/index/', 'report/pending/bp',
                     'report/pending/')
@@ -17,10 +20,12 @@ class AnalyticsMiddleware(object):
                 path = prefix
                 break
 
-        metric = "analytics.{0}.{1}.{2}".format(
+        metric = u"analytics.{0}.{1}.{2}".format(
             request.method,
             path,
             response.status_code
         )
+
+        metric = metric.encode('utf-8')
         statsd.incr(metric)
         return response
