60d8a9157f5ae4ebc1af91b7d311af641d4f00ec
ossreleasefeed <sneethling@mozilla.com>
2013-10-06 18:09:51 -0700

    Fix Bug 923966, ensure better contrast between graph line colors and background

--- webapp-django/crashstats/crashstats/static/crashstats/js/socorro/dashboard_graph.js
+++ webapp-django/crashstats/crashstats/static/crashstats/js/socorro/dashboard_graph.js
@@ -13,7 +13,7 @@ $(function() {
         dateRangeTypeValPattern = /(?!=)[a-zA-Z]{1,6}(?=:|$)/i,
         durationPattern = /\d{1,2}(?=:|$)/,
         durationValPattern = /(?!=)\d{1,6}(?=:|$)/,
-        colours = ['#0578a9', '#bc400f', '#3a8324', '#990099'],
+        colours = ['#6a3d9a', '#e31a1c', '#008800', '#1f78b4'],
         chartOpts = {
             xaxis: {
               mode: 'time',
