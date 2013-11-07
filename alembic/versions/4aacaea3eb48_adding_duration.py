02d16684dfc77651930690c8724638386fdca17c
Peter Bengtsson <mail@peterbe.com>
2013-11-07 12:48:29 -0800

    fixes bug 932064 - Add duration of jobs to crontabber_log table

---
+++ alembic/versions/4aacaea3eb48_adding_duration.py
@@ -0,0 +1,30 @@
+"""Adding duration
+
+Revision ID: 4aacaea3eb48
+Revises: 477e1c6e6df3
+Create Date: 2013-11-07 11:33:42.125557
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4aacaea3eb48'
+down_revision = '477e1c6e6df3'
+
+from alembic import op
+from socorro.lib import citexttype, jsontype
+from socorro.lib.migrations import fix_permissions, load_stored_proc
+
+import sqlalchemy as sa
+from sqlalchemy import types
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql import table, column
+
+
+
+
+def upgrade():
+    op.add_column(u'crontabber_log', sa.Column(u'duration', sa.NUMERIC(), nullable=True))
+
+
+def downgrade():
+    op.drop_column(u'crontabber_log', u'duration')
