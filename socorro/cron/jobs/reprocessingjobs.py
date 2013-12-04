2d9dafa4534f4fc5e18e4e076720b7547009be5e
Selena Deckelmann <selenamarie@gmail.com>
2013-12-04 10:15:39 -0800

    Added rollback to exception handling r=twobraids

--- socorro/cron/jobs/reprocessingjobs.py
+++ socorro/cron/jobs/reprocessingjobs.py
@@ -3,16 +3,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import datetime
-
 import pika
 
 from configman import Namespace
 from configman.converters import class_converter
-from socorro.lib.datetimeutil import utc_now
 from socorro.cron.base import PostgresTransactionManagedCronApp
 
 _reprocessing_sql = """ DELETE FROM reprocessing_jobs RETURNING crash_id """
+
 
 class ReprocessingJobsApp(PostgresTransactionManagedCronApp):
     app_name = 'reprocessing-jobs'
@@ -25,14 +23,13 @@ class ReprocessingJobsApp(PostgresTransactionManagedCronApp):
     required_config = Namespace()
     required_config.add_option(
         'queue_class',
-        default='socorro.external.rabbitmq.connection_context.ConnectionContext',
+        default='socorro.external.rabbitmq'
+        '.connection_context.ConnectionContext',
         doc='Queue class reprocessing queue',
         from_string_converter=class_converter
     )
 
     def run(self, connection):
-        logger = self.config.logger
-
         _basic_properties = pika.BasicProperties(
             delivery_mode=2,  # make message persistent
         )
@@ -40,18 +37,19 @@ class ReprocessingJobsApp(PostgresTransactionManagedCronApp):
         rabbit_connection = self.config.queue_class(self.config)
 
         cursor = connection.cursor()
-        cursor.execute(_reprocessing_sql)
         try:
+            cursor.execute(_reprocessing_sql)
             for crash_id in cursor.fetchone():
                 rabbit_connection.channel.basic_publish(
                     exchange='',
-                    routing_key=rabbit_connection.config.reprocessing_queue_name,
+                    routing_key=rabbit_connection.config.
+                    reprocessing_queue_name,
                     body=crash_id,
                     properties=_basic_properties
                 )
 
         except Exception as e:
-            # No results found, and that's ok
+            connection.rollback()
             raise e
 
         connection.commit()
