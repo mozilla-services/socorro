c26a17e5c6ac10c8b41730dba832f96a2f39d31b
Selena Deckelmann <selenamarie@gmail.com>
2013-12-11 09:35:59 -0800

    Fixes bug 822304 - migration for removal of priority jobs tables

--- alembic/versions/4aacaea3eb48_adding_duration.py
+++ alembic/versions/4aacaea3eb48_adding_duration.py
@@ -23,7 +23,7 @@ from sqlalchemy.sql import table, column
 
 
 def upgrade():
-    op.add_column(u'crontabber_log', sa.Column(u'duration', sa.INTERVAL(), nullable=True))
+    op.add_column(u'crontabber_log', sa.Column(u'duration', postgresql.INTERVAL(), nullable=True))
 
 
 def downgrade():
