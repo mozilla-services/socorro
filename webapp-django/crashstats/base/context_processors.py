b8e643d8323175dcf644d9c7a4f0c90659f5c0c4
lonnen <chris.lonnen@gmail.com>
2013-12-13 16:00:57 -0800

    949711-update-analytics-urchin

--- webapp-django/crashstats/base/context_processors.py
+++ webapp-django/crashstats/base/context_processors.py
@@ -4,7 +4,12 @@ from django.conf import settings
 
 def google_analytics(request):
     return {
-        'GOOGLE_ANALYTICS_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID', None)
+        'GOOGLE_ANALYTICS_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID', None),
+        'GOOGLE_ANALYTICS_DOMAIN': getattr(
+            settings,
+            'GOOGLE_ANALYTICS_DOMAIN',
+            'auto'
+        )
     }
 
 
