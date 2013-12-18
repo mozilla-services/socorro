84a7edd55b531a4b4d477cb6c81aa4ba89d83310
ossreleasefeed <sneethling@mozilla.com>
2013-12-19 01:06:21 +0200

    pep8 fixes and a missed semicolon

--- webapp-django/crashstats/crashstats/static/crashstats/js/socorro/tcr_bybug.js
+++ webapp-django/crashstats/crashstats/static/crashstats/js/socorro/tcr_bybug.js
@@ -14,4 +14,24 @@ $(function() {
         parentContainer.toggleClass('initially-hidden');
         parentContainer.attr('aria-expanded', expandedState);
     });
+
+    var bugSignatures = $('#bug_signatures');
+    var pattern = /^\d+$/g;
+
+    bugSignatures.submit(function(event) {
+        var bugNumber = $.trim($('#bug_number').val());
+
+        if (pattern.test(bugNumber)) {
+            socorro.ui.removeUserMsg('#signature-details');
+        } else {
+            event.preventDefault();
+
+            var response = {
+                status: 'error',
+                message: 'Only numbers are allowed as the bug number.'
+            };
+
+            socorro.ui.setUserMsg('#signature-details', response);
+        }
+    });
 });
