d579d598dd99b9ca4abb2a12e537c79251674242
Selena Deckelmann <selenamarie@gmail.com>
2014-01-10 12:20:02 -0800

    Update fake_raw_adu.py to include update_channel

--- scripts/staging/fake_raw_adu.py
+++ scripts/staging/fake_raw_adu.py
@@ -3,8 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import os
-from os.path import join, getsize
 import sys
 import psycopg2
 import psycopg2.extensions
@@ -13,15 +11,33 @@ import psycopg2.extras
 #connect to CSD database
 csd = psycopg2.connect("dbname=breakpad user=postgres port=5432")
 csd_cur = csd.cursor()
+
 # check if we already have ADU for the day
-csd_cur.execute("""SELECT COUNT(*) FROM raw_adu WHERE "date" = 'yesterday'::date""")
+csd_cur.execute("""
+    SELECT COUNT(*) FROM raw_adu WHERE "date" = 'yesterday'::date
+""")
 
 if csd_cur.fetchone()[0] > 0:
     sys.stderr.write('raw_adu has already been exported for yesterday\n')
     sys.exit(-1)
 
 #dump raw_adu to file
-csd_cur.execute("""INSERT into raw_adu (select adu_count, 'yesterday'::date as "date", product_name, product_os_platform, product_os_version, product_version, build, build_channel, product_guid from raw_adu where date in (select max(date) from raw_adu) ) ;""")
+csd_cur.execute("""
+    INSERT into raw_adu
+        (SELECT adu_count,
+        'yesterday'::date as "date",
+        product_name,
+        product_os_platform,
+        product_os_version,
+        product_version,
+        build,
+        build_channel,
+        product_guid,
+        update_channel
+        FROM raw_adu
+        WHERE date in (select max(date) from raw_adu)
+    )
+""")
 csd.commit()
 csd.close()
 
