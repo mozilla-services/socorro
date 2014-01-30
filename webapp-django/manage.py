df9cf99e3067836ed080500ef2d824292fd61246
lonnen <chris.lonnen@gmail.com>
2014-01-29 17:20:45 -0800

    remove funfactory references from management.py

--- webapp-django/manage.py
+++ webapp-django/manage.py
@@ -1,43 +1,25 @@
 #!/usr/bin/env python
-import os
 import sys
-import site
 
-# Edit this if necessary or override the variable in your environment.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crashstats.settings')
+
+from django.core.management import execute_manager, setup_environ
 
 try:
-    # For local development in a virtualenv:
-    from funfactory import manage
+    from kitsune import settings_local as settings
 except ImportError:
-    # Production:
-    # Add a temporary path so that we can import the funfactory
-    tmp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                            'vendor', 'src', 'funfactory')
-    sys.path.append(tmp_path)
-
-    from funfactory import manage
-
-    # Let the path magic happen in setup_environ() !
-    sys.path.remove(tmp_path)
+    try:
+        from crashstats import settings  # Assumed to be in the same directory.
+    except ImportError:
+        sys.stderr.write(
+            "Error: Tried importing 'settings_local.py' and 'settings.py' "
+            "but neither could be found (or they're throwing an ImportError)."
+            " Please come back and try again later.")
+        raise
 
 
-manage.setup_environ(__file__, more_pythonic=True)
-
-# We build binary packages on jenkins which installs itself
-# in vendor-local/lib64/python
-# Add it to sys.path just after vendor-local/lib/python which
-# funfactory already added
-_new_path = manage.path('vendor-local/lib64/python')
-site.addsitedir(
-    os.path.abspath(
-        _new_path
-    )
-)
-# now re-arrange so the order is right
-_other_path = manage.path('vendor-local/lib/python')
-sys.path.insert(sys.path.index(_other_path) + 1, _new_path)
-
+# The first thing execute_manager does is call `setup_environ`. Logging config
+# needs to access settings, so we'll setup the environ early.
+setup_environ(settings)
 
 if __name__ == "__main__":
-    manage.main()
+    execute_manager(settings)
