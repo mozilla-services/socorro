6426951e957023fb8a36c2559bbec3ead27354c5
phrawzty <phrawzty+github@mozilla.com>
2014-02-25 16:31:10 +0100

    clean up syntax to match style guide; bug 976547

--- puppet/manifests/socorro.pp
+++ puppet/manifests/socorro.pp
@@ -1,99 +1,103 @@
+# Set up basic Socorro requirements.
 class webapp::socorro {
 
-    service {
-        'rabbitmq-server':
-            enable => true,
-            ensure => running,
-            require => Package['rabbitmq-server'];
+  service {
+    'rabbitmq-server':
+      ensure  => running,
+      enable  => true,
+      require => Package['rabbitmq-server'];
 
-        'postgresql-9.3':
-            enable => true,
-            ensure => running,
-            require => [
-                        Package['postgresql93-server'],
-                        Exec['postgres-initdb'],
-                       ];
-        'elasticsearch':
-            enable => true,
-            ensure => running,
-            require => Package['elasticsearch'];
-    }
+    'postgresql-9.3':
+      ensure  => running,
+      enable  => true,
+      require => [
+          Package['postgresql93-server'],
+          Exec['postgres-initdb'],
+        ];
 
-    exec {
-        'postgres-initdb':
-            command => '/sbin/service postgresql-9.3 initdb';
-    }
+    'elasticsearch':
+      ensure  => running,
+      enable  => true,
+      require => Package['elasticsearch'];
+  }
 
-    yumrepo {
-        'PGDG':
-            baseurl => 'http://yum.postgresql.org/9.3/redhat/rhel-$releasever-$basearch',
-            descr => 'PGDG',
-            enabled => 1,
-            gpgcheck => 0;
+  exec {
+    'postgres-initdb':
+      command => '/sbin/service postgresql-9.3 initdb'
+  }
 
-        'EPEL':
-            baseurl => 'http://download.fedoraproject.org/pub/epel/$releasever/$basearch',
-            descr => 'EPEL',
-            enabled => 1,
-            gpgcheck => 0;
-        'devtools':
-            baseurl => 'http://people.centos.org/tru/devtools-1.1/$releasever/$basearch/RPMS',
-            enabled => 1,
-            gpgcheck => 0;
-    }
+  yumrepo {
+    'PGDG':
+      baseurl  => 'http://yum.postgresql.org/9.3/redhat/rhel-$releasever-$basearch',
+      descr    => 'PGDG',
+      enabled  => 1,
+      gpgcheck => 0;
 
-    package {
-        [
-         'subversion',
-         'make',
-         'rsync',
-         'time',
-         'gcc-c++',
-         'python-devel',
-         'git',
-         'libxml2-devel',
-         'libxslt-devel',
-         'openldap-devel',
-         'java-1.7.0-openjdk',
-         'yum-plugin-fastestmirror',
-        ]:
-        ensure => latest;
-    }
+    'EPEL':
+      baseurl  => 'http://download.fedoraproject.org/pub/epel/$releasever/$basearch',
+      descr    => 'EPEL',
+      enabled  => 1,
+      gpgcheck => 0;
 
-    package {
-        [
-         'postgresql93-server',
-         'postgresql93-plperl',
-         'postgresql93-contrib',
-         'postgresql93-devel',
-        ]:
-        ensure => latest,
-        require => [ Yumrepo['PGDG'], Package['yum-plugin-fastestmirror']];
-    }
+    'devtools':
+      baseurl  => 'http://people.centos.org/tru/devtools-1.1/$releasever/$basearch/RPMS',
+      enabled  => 1,
+      gpgcheck => 0;
+  }
 
-    package {
-        [
-         'python-virtualenv',
-         'supervisor',
-         'rabbitmq-server',
-         'python-pip',
-         'npm',
-        ]:
-        ensure => latest,
-        require => [ Yumrepo['EPEL'], Package['yum-plugin-fastestmirror']];
-    }
+  package {
+    [
+      'subversion',
+      'make',
+      'rsync',
+      'time',
+      'gcc-c++',
+      'python-devel',
+      'git',
+      'libxml2-devel',
+      'libxslt-devel',
+      'openldap-devel',
+      'java-1.7.0-openjdk',
+      'yum-plugin-fastestmirror',
+    ]:
+    ensure => latest
+  }
 
-    package {
-        'elasticsearch':
-            provider => rpm,
-            source => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.4.noarch.rpm',
-            require => Package['java-1.7.0-openjdk'],
-            ensure => 'present';
-    }
+  package {
+    [
+      'postgresql93-server',
+      'postgresql93-plperl',
+      'postgresql93-contrib',
+      'postgresql93-devel',
+    ]:
+    ensure  => latest,
+    require => [ Yumrepo['PGDG'], Package['yum-plugin-fastestmirror']]
+  }
 
-    package {
-        'devtoolset-1.1-gcc-c++':
-            ensure => latest,
-            require => [ Yumrepo['devtools'], Package['yum-plugin-fastestmirror']];
-    }
+  package {
+    [
+      'python-virtualenv',
+      'supervisor',
+      'rabbitmq-server',
+      'python-pip',
+      'npm',
+    ]:
+    ensure  => latest,
+    require => [ Yumrepo['EPEL'], Package['yum-plugin-fastestmirror']]
+  }
+
+  package {
+    'elasticsearch':
+      ensure   => present,
+      provider => 'rpm',
+      source   => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.4.noarch.rpm',
+      require  => Package['java-1.7.0-openjdk']
+  }
+
+  package {
+    'devtoolset-1.1-gcc-c++':
+      ensure  => latest,
+      require => [ Yumrepo['devtools'], Package['yum-plugin-fastestmirror']]
+  }
+
 }
