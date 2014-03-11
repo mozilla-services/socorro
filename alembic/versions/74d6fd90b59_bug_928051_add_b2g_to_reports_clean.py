21bcd4e4b3e70fa4076afec703b767b280e5048e
Robert Helmer <rhelmer@rhelmer.org>
2014-03-11 10:27:33 -0700

    bug 928051 - new keys on JSON

--- alembic/versions/74d6fd90b59_bug_928051_add_b2g_to_reports_clean.py
+++ alembic/versions/74d6fd90b59_bug_928051_add_b2g_to_reports_clean.py
@@ -35,7 +35,33 @@ def upgrade():
             WHERE product_name = 'B2G'
             AND release_channel = 'Release'
         )
-        SELECT update_product_versions()
+    """)
+
+    op.execute("""
+        TRUNCATE update_channel_map
+    """)
+
+    op.execute("""
+        INSERT INTO update_channel_map
+        (update_channel, productid, version_field, rewrite)
+        VALUES
+        ('nightly',
+         '{3c2e2abc-06d4-11e1-ac3b-374f68613e61}',
+         'B2G_OS_Version',
+         '{"Android_Manufacturer": "ZTE",
+           "Android_Model": "roamer2",
+           "Android_Version": "15(REL)",
+           "B2G_OS_Version": "1.0.1.0-prerelease",
+           "BuildID":
+                ["20130621133927", "20130621152332",
+                 "20130531232151", "20130617105829",
+                 "20130724040538"],
+            "ProductName": "B2G",
+            "ReleaseChannel": "nightly",
+            "Version": "18.0",
+            "rewrite_update_channel_to": "release-zte",
+            "rewrite_build_type_to": "release"}'
+        )
     """)
     op.execute(""" COMMIT """)
 
