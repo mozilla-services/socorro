5b5824a3e230935627b5dbdf4ff3fdd1f9cc893a
Ted Mielczarek <ted@mielczarek.org>
2014-06-25 15:07:32 -0400

    bug 1030276 - Display JSON processor output in Raw Dump tab

--- webapp-django/crashstats/crashstats/views.py
+++ webapp-django/crashstats/crashstats/views.py
@@ -1066,6 +1066,17 @@ def report_index(request, crash_id, default_context=None):
         else:
             raise
 
+    if 'json_dump' in context['report']:
+        json_dump = dict(context['report']['json_dump'])
+        if 'sensitive' in json_dump:
+            #TODO: could check permissions here and allow this
+            del json_dump['sensitive']
+        context['raw_processor_output'] = json.dumps(json_dump,
+                                                     sort_keys=True,
+                                                     indent=4, separators=(',', ': '))
+    else:
+        context['raw_processor_output'] = context['report']['dump']
+
     context['bug_product_map'] = settings.BUG_PRODUCT_MAP
 
     process_type = 'unknown'
