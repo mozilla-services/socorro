e97f785dcedd364eda3f6854e45338fecf712011
Trevor Bramwell <trevor@bramwell.net>
2014-08-15 11:55:59 -0700

    Pull out integration tests, and deps to own script

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,16 +31,7 @@ source scripts/bootstrap.sh
 # run unit tests
 source scripts/test.sh
 
-# pull pre-built, known version of breakpad
-wget --quiet 'https://ci.mozilla.org/job/breakpad/lastSuccessfulBuild/artifact/breakpad.tar.gz'
-tar -zxf breakpad.tar.gz
-mv breakpad stackwalk
-make stackwalker
-
-# run socorro integration test
-echo "Running integration test..."
-./scripts/rabbitmq-integration-test.sh --destroy
-./scripts/elasticsearch-integration-test.sh
+source scripts/integration-test.sh
 
 # package socorro.tar.gz for distribution
 mkdir builds/
