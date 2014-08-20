a41a6f7e98e4fe1664ae4fe30886471a556a390d
Robert Helmer <rhelmer@rhelmer.org>
2014-08-20 10:30:52 -0700

    dry

--- alembic/versions/52dbc7357409_bug_1056025_and_bug_1037855_rename_.py
+++ alembic/versions/52dbc7357409_bug_1056025_and_bug_1037855_rename_.py
@@ -19,66 +19,15 @@ from sqlalchemy import types
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql import table, column
 
+from socorro.cron.jobs.fetch_adi_from_hive import _RAW_ADI_QUERY
+
 def upgrade():
     op.execute("""TRUNCATE raw_adi""")
     now = datetime.datetime.utcnow()
     for backfill_date in [
         (now - datetime.timedelta(days=days)).strftime("%Y-%m-%d")
             for days in range(1,3)]:
-                op.execute("""
-                    INSERT INTO raw_adi (
-                        adi_count,
-                        date,
-                        product_name,
-                        product_os_platform,
-                        product_os_version,
-                        product_version,
-                        build,
-                        product_guid,
-                        update_channel
-                    )
-                    SELECT
-                        sum(count),
-                        report_date,
-                        CASE WHEN (raw_adi_logs.product_name = 'Fennec'
-                            AND product_guid = '{aa3c5121-dab2-40e2-81ca-7ea25febc110}')
-                        THEN 'FennecAndroid'
-                        WHEN (raw_adi_logs.product_name = 'Webapp Runtime')
-                        THEN 'WebappRuntime'
-                        ELSE raw_adi_logs.product_name
-                        END,
-                        product_os_platform,
-                        product_os_version,
-                        product_version,
-                        build,
-                        CASE WHEN (product_guid = 'webapprt@mozilla.org')
-                        THEN '{webapprt@mozilla.org}'
-                        ELSE product_guid
-                        END,
-                        CASE WHEN (build_channel ILIKE 'release%%')
-                        THEN 'release'
-                        ELSE build_channel
-                        END
-                    FROM raw_adi_logs
-                        -- FILTER with product_productid_map
-                        JOIN product_productid_map ON productid =
-                            CASE WHEN (product_guid = 'webapprt@mozilla.org')
-                            THEN '{webapprt@mozilla.org}'
-                            ELSE product_guid
-                            END
-                    WHERE
-                        report_date=%s
-                    GROUP BY
-                        report_date,
-                        raw_adi_logs.product_name,
-                        product_os_platform,
-                        product_os_version,
-                        product_version,
-                        build,
-                        product_guid,
-                        build_channel
-
-                """ % backfill_date)
+                op.execute(_RAW_ADI_QUERY % ("'" + backfill_date + "'"))
 
 def downgrade():
     op.execute("""TRUNCATE raw_adi""")
