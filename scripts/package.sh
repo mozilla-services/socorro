7a9a0894fff94d18a46c5f091028b34f9ae3eb8f
Trevor Bramwell <trevor@bramwell.net>
2014-08-26 09:57:31 -0700

    Replace tarball creation with simple fpm call

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,27 @@
 #! /bin/bash -e
 
-if [ -n $BUILD_NUMBER ]
-then
-  echo "$BUILD_NUMBER" > builds/socorro/JENKINS_BUILD_NUMBER
+##
+# Args
+#  1: Version of socorro to release
+#  2: Type of package to build
+##
+VERSION=${1:-$(git describe --tags | cut -d'-' -f1)}
+TYPE=${2:-"deb"}
+
+if [ ! -d 'builds/socorro' ]; then
+    echo "Socorro has not been built (build/socorro does not exist)."
+    echo "  Please run './scripts/build.sh' before continuing."
+    exit 1
 fi
-tar -C builds --mode 755 --exclude-vcs --owner 0 --group 0 -zcf socorro.tar.gz socorro/
+cd builds
+
+echo "> Building Socorro $VERSION ..."
+fpm -s dir -t $TYPE \
+    -v $VERSION \
+    -n "socorro" \
+    -m "<socorro-dev@mozilla.com>"
+    --license "MPL" \
+    --url "https://wiki.mozilla.org/Socorro"
+    socorro
+
+echo "> Build Complete."
