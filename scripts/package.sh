bd56431259b6b195ff829525a23c46ed119993fe
Robert Helmer <rhelmer@rhelmer.org>
2014-09-23 17:10:39 -0700

    fix bug 1065069 - reland RPM prelink fix for "Digest mismatch"

--- scripts/package.sh
+++ scripts/package.sh
@@ -33,6 +33,7 @@ if [ "$BUILD_TYPE" != "tar" ]; then
         --after-install scripts/package/after-install.sh \
         --before-remove scripts/package/before-remove.sh \
         --after-remove scripts/package/after-remove.sh \
+        --rpm-verifyscript scripts/package/prelink.sh \
         --config-files /etc/socorro \
         --exclude *.pyc \
         --exclude *.swp \
