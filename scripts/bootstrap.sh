6011bf0afd2bf875b381de4dc76975da04a5c565
Peter Bengtsson <mail@peterbe.com>
2014-12-05 18:11:44 -0800

    fixes bug 1108178 - upgrade to peep 2.0

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,7 +14,7 @@ fi
 source "$VIRTUAL_ENV/bin/activate"
 
 # install dev + prod dependencies
-${VIRTUAL_ENV}/bin/pip install tools/peep-1.2.tar.gz
+${VIRTUAL_ENV}/bin/pip install tools/peep-2.0.tar.gz
 ${VIRTUAL_ENV}/bin/peep install --download-cache=./pip-cache -r requirements.txt
 
 if [ "`uname -sm`" == "Linux x86_64" ]; then
