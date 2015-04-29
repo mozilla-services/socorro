7ec9d4068116ad5ad5fa911940322eee6e144f7c
Adrian Gaudebert <adrian@gaudebert.fr>
2015-04-29 15:03:25 +0200

    Bug 1000160 - Added migration for update_reports_clean.

---
+++ alembic/versions/56f5cdf9bcdb_bug_1000160_use_version_string_instead_.py
@@ -0,0 +1,26 @@
+"""bug 1000160 - Use version_string instead of release_version in update_reports_clean.
+
+Revision ID: 56f5cdf9bcdb
+Revises: 3f03539b66de
+Create Date: 2015-04-29 15:01:50.954659
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '56f5cdf9bcdb'
+down_revision = '3f03539b66de'
+
+from alembic import op
+from socorro.lib import citexttype, jsontype, buildtype
+from socorro.lib.migrations import fix_permissions, load_stored_proc
+
+import sqlalchemy as sa
+from sqlalchemy import types
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql import table, column
+
+def upgrade():
+    load_stored_proc(op, ['001_update_reports_clean.sql'])
+
+def downgrade():
+    load_stored_proc(op, ['001_update_reports_clean.sql'])
