67c2538fa7e9b81c3e082d5fe48670b76a4d346d
Robert Helmer <rhelmer@rhelmer.org>
2015-05-08 22:41:11 -0700

    bug 1162085, persist subdir correctly, and create now too

--- scripts/package/rpm-after-install.sh
+++ scripts/package/rpm-after-install.sh
@@ -4,6 +4,12 @@
 chown socorro /var/log/socorro
 chown socorro /var/lock/socorro
 
+# ensure uwsgi dir exists
+mkdir -p /run/uwsgi/socorro
+chown root:nginx /run/uwsgi
+chown socorro:nginx /run/uwsgi/socorro
+chmod 0755 /run/uwsgi /run/uwsgi/socorro
+
 # FIXME processor will silently fail if this does not exist :/
 mkdir /home/socorro/temp
 chown socorro /home/socorro/temp
