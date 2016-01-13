ffd6cf4eaed70fe772f6f760db7f7a313d91b458
Peter Bengtsson <mail@peterbe.com>
2016-01-13 13:40:30 -0500

    fixes bug 1238129 - Retire WebappRuntime and WebappRuntimeMobile products from Socorro

---
+++ alembic/versions/2990eb43b269_update_product_versions.py
@@ -0,0 +1,22 @@
+"""update product versions
+
+Revision ID: 2990eb43b269
+Revises: 3860644579f4
+Create Date: 2015-12-14 09:55:33.639675
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2990eb43b269'
+down_revision = '3860644579f4'
+
+from alembic import op
+from socorro.lib.migrations import load_stored_proc
+
+
+def upgrade():
+    load_stored_proc(op, ['update_product_versions.sql'])
+
+
+def downgrade():
+    load_stored_proc(op, ['update_product_versions.sql'])
