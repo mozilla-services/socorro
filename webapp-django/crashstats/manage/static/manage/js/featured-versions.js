f26e61cb8da2563e125485a8e8507a1fa602d3c7
Peter Bengtsson <mail@peterbe.com>
2016-03-16 08:00:11 -0400

    bug 1257149 - Remove 'Update features versions' button on non-prod

--- webapp-django/crashstats/manage/static/manage/js/featured-versions.js
+++ webapp-django/crashstats/manage/static/manage/js/featured-versions.js
@@ -22,37 +22,6 @@
             return submitForm($(this));
         });
 
-        if (document.location.host !== 'crash-stats.mozilla.com') {
-            // indicate what prod has for its featured versions
-            $.getJSON('https://crash-stats.mozilla.com/api/CurrentVersions/')
-            .done(function(products) {
-                $('.featured-on-prod').show();
-                var featured = {};
-                $.each(products, function(i, product) {
-                    if (product.featured) {
-                        if (!featured[product.product]) {
-                            featured[product.product] = [];
-                        }
-                        featured[product.product].push(product.version);
-                    }
-                });
-
-                $('.featured-on-prod button').on('click', function() {
-                    $('input[type="checkbox"]:checked').attr('checked', false);
-                    $('tbody tr').each(function() {
-                        var product = $('td', this).eq(0).text();
-                        var version = $('td', this).eq(1).text();
-                        if (featured[product].indexOf(version) > -1) {
-                            $('input[type="checkbox"]', this).attr('checked', true);
-                        }
-                    });
-                    $('.featured-on-prod p:hidden').show(600);
-                });
-            })
-            .fail(function() {
-                console.error.apply(console, arguments);
-            });
-        }
     });
 
 }($, document));
