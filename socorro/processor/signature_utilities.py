e3ece925c5a72d7d4bd805ebdd1b3b9d1828b6c2
Robert Kaiser <kairo@kairo.at>
2016-03-17 17:14:17 +0100

    Fixes bug 1257572 - Add js::AutoEnterOOMUnsafeRegion::crash to our markers for OOM signatures

--- socorro/processor/signature_utilities.py
+++ socorro/processor/signature_utilities.py
@@ -853,7 +853,8 @@ class OOMSignature(Rule):
     signature_fragments = (
         'NS_ABORT_OOM',
         'mozalloc_handle_oom',
-        'CrashAtUnhandlableOOM'
+        'CrashAtUnhandlableOOM',
+        'AutoEnterOOMUnsafeRegion'
     )
 
     #--------------------------------------------------------------------------
