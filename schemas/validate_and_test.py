f79e63ea3f1c4cf0654f4086ba5434f6d6417c9c
Peter Bengtsson <peterbe@mozilla.com>
2016-07-27 14:07:51 -0400

    validate_and_test relativity (#3401)
    * validate_and_test relativity
    
    * make it actually work
    
    r=adngdb

--- schemas/validate_and_test.py
+++ schemas/validate_and_test.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python
+
 import json
+import os
 
 import jsonschema
 import requests
 
 
 API_BASE = 'https://crash-stats.mozilla.com/api/{}/'
+HERE = os.path.dirname(__file__)
 
 
 def main():
-    schema = json.load(open('processed_crash.json'))
+    schema = json.load(open(os.path.join(HERE, 'processed_crash.json')))
     jsonschema.Draft4Validator.check_schema(schema)
     print('Processed Crash schema is valid')
 
@@ -26,7 +30,7 @@ def main():
     for hit in search['hits']:
         r = requests.get(
             API_BASE.format('ProcessedCrash'),
-            params={'uuid': hit['uuid']}
+            params={'crash_id': hit['uuid']}
         )
         print(r.url)
         crash = r.json()
