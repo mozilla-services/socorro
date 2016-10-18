0f6f9ea847ae2839ebb91ad943941f9f31fb4a24
Adrian Gaudebert <adrian@gaudebert.fr>
2016-10-18 17:22:57 +1300

    Fixes bug 1304108 - Removed unneeded JS file for the profile page.
    That file was conflicting with our timeutils.js file, causing an error in the parsing of crash reports dates.

--- webapp-django/crashstats/profile/static/profile/js/profile.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.crashes_list time').each(function (i) {
-        var elt = $(this);
-        var datetime = moment(elt.attr('datetime')).utc().format('MMMM Do YYYY, hh:mm:ss');
-        elt.attr('title', datetime);
-        elt.text(moment(elt.text()).fromNow());
-    });
-});
