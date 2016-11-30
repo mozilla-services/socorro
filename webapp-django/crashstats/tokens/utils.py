4051df17d5e64b390555d002b3966c4cf8e1528d
Peter Bengtsson <mail@peterbe.com>
2016-11-30 14:26:33 -0500

    make it a frozenset

--- webapp-django/crashstats/tokens/utils.py
+++ webapp-django/crashstats/tokens/utils.py
@@ -4,4 +4,7 @@ from raven.processors import SanitizePasswordsProcessor
 class RavenSanitizeAuthTokenProcessor(SanitizePasswordsProcessor):
     """Extend the standard SanitizePasswordsProcessor with one more key
     which is 'Auth-Token' which is what we use in our API."""
-    FIELDS = tuple(SanitizePasswordsProcessor.FIELDS) + ('auth-token',)
+    # Make it a frozenset because that's what the base class has.
+    FIELDS = frozenset(
+        tuple(SanitizePasswordsProcessor.FIELDS) + ('auth-token',)
+    )
