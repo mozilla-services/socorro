b4075c67746c08693132e059538b095ab3c4c1af
Eugene Zolenko <ezolenko@smarttech.com>
2016-12-06 11:03:01 -0700

    - unblocking support for AWS IAM roles instead of hardcoding access keys.

--- webapp-django/crashstats/settings/base.py
+++ webapp-django/crashstats/settings/base.py
@@ -577,8 +577,8 @@ ANALYZE_MODEL_FETCHES = config('ANALYZE_MODEL_FETCHES', False, cast=bool)
 
 
 # Credentials for being able to make an S3 connection
-AWS_ACCESS_KEY = config('AWS_ACCESS_KEY', '')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', '')
+AWS_ACCESS_KEY = config('AWS_ACCESS_KEY', None)
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', None)
 
 # Information for uploading symbols to S3
 SYMBOLS_BUCKET_DEFAULT_NAME = config('SYMBOLS_BUCKET_DEFAULT_NAME', '')
