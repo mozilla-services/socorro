f0a7f283f7380078e143ac7da124be8177eb793c
Peter Bengtsson <peterbe@mozilla.com>
2016-12-09 13:00:00 -1000

    fixes bug 1322615 - Don't look for status messages in JSON endpoints (#3623)
    * fixes bug 1322615 - Don't look for status messages in JSON endpoints
    
    * refactor simpler

--- webapp-django/crashstats/status/context_processors.py
+++ webapp-django/crashstats/status/context_processors.py
@@ -2,19 +2,8 @@ from crashstats.status.models import StatusMessage
 
 
 def status_message(request):
-    statuses = StatusMessage.objects.filter(enabled=True)
-
-    if not statuses.exists():
-        return {}
-
-    messages = []
-    for status in statuses.order_by('-created_at'):
-        messages.append({
-            'text': status.message,
-            'severity': status.severity,
-            'date': status.created_at,
-        })
-
     return {
-        'status_messages': messages,
+        'status_messages': (
+            StatusMessage.objects.filter(enabled=True).order_by('-created_at')
+        ),
     }
