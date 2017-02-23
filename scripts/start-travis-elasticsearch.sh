dd836e3bfaafa6def361094f5fbbcb500e30dfca
Adrian Gaudebert <adrian@gaudebert.fr>
2017-02-23 17:12:20 +0100

    Trying to use new ES version in travis.

--- scripts/start-travis-elasticsearch.sh
+++ scripts/start-travis-elasticsearch.sh
@@ -4,12 +4,12 @@ set -ex
 
 # Why download it and not use the elasticsearch service?
 # Two reasons
-#  - predictable version (1.4.x) is what we use in production
+#  - predictable version (5.1.x) is what we use in production
 #  - running it as a service would require sudo which we don't want to use
-wget --no-check-certificate 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.4.5.tar.gz'
-tar zxf elasticsearch-1.4.5.tar.gz
+wget --no-check-certificate 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-5.1.1.tar.gz'
+tar zxf elasticsearch-5.1.1.tar.gz
 echo "script.disable_dynamic: false" > elasticsearch.yml
 echo "index.number_of_shards: 1" >> elasticsearch.yml
 echo "index.number_of_replicas: 0" >> elasticsearch.yml
 # the -d flag starts it as a daemon in the background
-./elasticsearch-1.4.5/bin/elasticsearch -d --config=elasticsearch.yml
+./elasticsearch-5.1.1/bin/elasticsearch -d --config=elasticsearch.yml
