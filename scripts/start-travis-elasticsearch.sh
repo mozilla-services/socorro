7874a193713bf7515a8ea79c97f6fdef65680628
Adrian Gaudebert <adrian@gaudebert.fr>
2017-02-27 16:14:13 +0100

    Using instructions from Peter.

--- scripts/start-travis-elasticsearch.sh
+++ scripts/start-travis-elasticsearch.sh
@@ -6,12 +6,7 @@ set -ex
 # Two reasons
 #  - predictable version (5.1.x) is what we use in production
 #  - running it as a service would require sudo which we don't want to use
-wget --no-check-certificate 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.tar.gz'
-tar zxf elasticsearch-5.1.1.tar.gz
+wget --no-check-certificate 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.deb'
+sudo dpkg -i --force-confnew elasticsearch-5.1.1.deb
 
-# Increase the limit of mmap counts in virtual memory. See:
-# https://www.elastic.co/guide/en/elasticsearch/reference/5.1/vm-max-map-count.html
-sudo sysctl -w vm.max_map_count=262144
-
-# the -d flag starts it as a daemon in the background
-./elasticsearch-5.1.1/bin/elasticsearch -d
+sudo service elasticsearch start
