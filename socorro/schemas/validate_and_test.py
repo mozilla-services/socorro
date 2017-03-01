84f524d989b89f1bead85fc88dfffea8d973c76d
Peter Bengtsson <mail@peterbe.com>
2017-03-01 16:08:44 -0500

    fixes bug 1343623 - validate_and_test.py should test more realistically

--- socorro/schemas/validate_and_test.py
+++ socorro/schemas/validate_and_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import json
 import os
@@ -6,15 +7,37 @@ import os
 import jsonschema
 import requests
 
+from socorro.external.boto.crashstorage import TelemetryBotoS3CrashStorage
 
 API_BASE = 'https://crash-stats.mozilla.com/api/{}/'
 HERE = os.path.dirname(__file__)
 
 
+class MockedTelemetryBotoS3CrashStorage(TelemetryBotoS3CrashStorage):
+
+    def __init__(self):
+        # Deliberately not doing anything fancy with config. So no super call.
+        # Prep the self._all_fields so it's available when
+        # self.save_raw_and_processed() is called.
+        r = requests.get(
+            API_BASE.format('SuperSearchFields'),
+        )
+        print(r.url)
+        self._all_fields = r.json()
+
+    def _get_all_fields(self):
+        return self._all_fields
+
+    def save_processed(self, crash):
+        self.combined = crash
+
+
 def main():
-    schema = json.load(open(os.path.join(HERE, 'crash_report.json')))
+    file_path = os.path.join(HERE, 'crash_report.json')
+    with open(file_path) as f:
+        schema = json.load(f)
     jsonschema.Draft4Validator.check_schema(schema)
-    print('Processed Crash schema is valid')
+    print('{} is a valid JSON schema'.format(file_path))
 
     print('Fetching data...')
     r = requests.get(
@@ -27,15 +50,29 @@ def main():
     )
     search = r.json()
 
+    processor = MockedTelemetryBotoS3CrashStorage()
+
     print('Testing {} random recent crash reports'.format(len(search['hits'])))
     for hit in search['hits']:
+        r = requests.get(
+            API_BASE.format('RawCrash'),
+            params={'crash_id': hit['uuid']}
+        )
+        print(r.url)
+        raw_crash = r.json()
         r = requests.get(
             API_BASE.format('ProcessedCrash'),
             params={'crash_id': hit['uuid']}
         )
         print(r.url)
-        crash = r.json()
-        jsonschema.validate(crash, schema)
+        processed_crash = r.json()
+        processor.save_raw_and_processed(
+            raw_crash,
+            (),  # dumps
+            processed_crash,
+            hit['uuid'],
+        )
+        jsonschema.validate(processor.combined, schema)
 
     print('Done testing, all crash reports passed.')
 
