bd003510ce5ded46813127fd86f0781b9e5cdb4c
Peter Bengtsson <peterbe@mozilla.com>
2017-03-07 12:57:49 -0500

    fixes bug 1344862 - remember preferred panes on Summary tab (#3690)
    * fixes bug 1344862 - remember preferred panes on Summary tab
    
    * nit addressed

--- webapp-django/crashstats/signature/static/signature/js/signature_tab_summary.js
+++ webapp-django/crashstats/signature/static/signature/js/signature_tab_summary.js
@@ -22,9 +22,25 @@ SignatureReport.SummaryTab.prototype = SignatureReport.inherit(SignatureReport.T
 SignatureReport.SummaryTab.prototype.onAjaxSuccess = function (contentElement, data) {
     SignatureReport.Tab.prototype.onAjaxSuccess.apply(this, arguments);
 
+    var localStorageKey = 'pref-opened-panels';
+    var prefOpenedPanels = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+    prefOpenedPanels.forEach(function(id, i) {
+        var parent = $('#' + id + '.panel');
+        $('.content', parent).toggle();
+        $('.options', parent).toggleClass('hide');
+    });
+
     $(contentElement).on('click', '.panel header', function (e) {
         e.preventDefault();
-        $('.content', $(this).parent()).toggle();
+        var parent = $(this).parent();
+        var panelId = parent.attr('id');
+        if ($('.content:visible', parent).length) {
+            prefOpenedPanels.splice(prefOpenedPanels.indexOf(panelId), 1);
+        } else {
+            prefOpenedPanels.push(panelId);
+        }
+        localStorage.setItem(localStorageKey, JSON.stringify(prefOpenedPanels));
+        $('.content', parent).toggle();
         $('.options', this).toggleClass('hide');
     });
 };
