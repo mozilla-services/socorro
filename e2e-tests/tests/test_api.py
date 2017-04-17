dc257feb07ce5e66d4730b37dba789fddf12a9dd
Peter Bengtsson <mail@peterbe.com>
2017-04-17 09:57:10 -0400

    nits fixed

--- e2e-tests/tests/test_api.py
+++ e2e-tests/tests/test_api.py
@@ -8,9 +8,9 @@ import requests
 import pytest
 
 
-# Because there are so few crashes for these product, instead of
+# Because there are so few crashes for these products, instead of
 # looking for crashes from the *active* versions, we'll for any
-# any recent crashes for these.
+# version of any recent crashes for these.
 ANY_VERSION_PRODUCTS = ('SeaMonkey',)
 
 
@@ -32,7 +32,7 @@ class TestAPI:
         # for a single crash for that product
         product_to_uuid = {}
         for product, versions in product_versions.items():
-            if product == ANY_VERSION_PRODUCTS:
+            if product in ANY_VERSION_PRODUCTS:
                 # Effectively, don't filter by any particular version
                 versions = None
             response = requests.get(base_url + '/api/SuperSearch/', {
