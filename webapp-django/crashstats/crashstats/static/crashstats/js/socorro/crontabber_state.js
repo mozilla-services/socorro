e950f32510f940ae6a203ab90df4e4c57d2f5839
Peter Bengtsson <peterbe@mozilla.com>
2017-04-24 09:54:09 -0400

    fixes bug 1357833 - Reflect jobs that don't run in /crontabber-state (#3735)
    * fixes bug 1357833 - Reflect jobs that don't run in /crontabber-state
    
    * more nit cleanups

--- webapp-django/crashstats/crashstats/static/crashstats/js/socorro/crontabber_state.js
+++ webapp-django/crashstats/crashstats/static/crashstats/js/socorro/crontabber_state.js
@@ -1,20 +1,26 @@
-var margin = {top: 1, right: 20, bottom: 6, left: 10},
-    width = d3.select('#crontabber-chart').property('scrollWidth')
-            - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+/*global moment, d3, _ */
+var margin = {top: 1, right: 20, bottom: 6, left: 10};
+var width = d3.select('#crontabber-chart').property('scrollWidth') - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+// To be obsolete it means that your next_run was more than
+// 24 hours ago.
+var longAgo = new Date(new Date() - 24 * 3600 * 1000);
 
-var format = d3.format(",.0f"),
+var format = d3.format(',.0f'),
     color = function(node) {
         if (node.error_count > 0 || node.errors > 0) {
-            return "#E41A1C"; // red
+            return '#E41A1C'; // red
         }
         if (node.skips > 0) {
-            return "#fafa78"; // yellow
+            return '#fafa78'; // yellow
         }
         if (node.ongoing) {
-            return "#9f3774"; // pinkish
+            return '#9f3774'; // pinkish
         }
-        return "#4DAF4A"; // green
+        if (node._obsolete) {
+            return '#cfcfcf'; // light grey
+        }
+        return '#4DAF4A'; // green
     },
     title = function(d) {
         var name = d.name,
@@ -25,15 +31,18 @@ var format = d3.format(",.0f"),
             name = d.source.name;
         }
         if(errors > 0) {
-            return name + " has failed " + format(errors) + " times";
+            return name + ' has failed ' + format(errors) + ' times';
         }
         if (skips > 0) {
-            return name + " has been skipped " + format(skips) + " times.";
+            return name + ' has been skipped ' + format(skips) + ' times.';
         }
         if (d.ongoing) {
-            return name + " is ongoing for " + moment(d.ongoing).fromNow(true);
+            return name + ' is ongoing for ' + moment(d.ongoing).fromNow(true);
         }
-        return name + " is working normally.";
+        if (d._obsolete) {
+            return name + ' is obsolete. Last run ' + moment(d.last_run).fromNow(false);
+        }
+        return name + ' is working normally.';
     };
 
 var svg = d3.select('#crontabber-chart').append('svg')
@@ -51,15 +60,15 @@ var sankey = d3.sankey()
 var path = sankey.link();
 
 function escapeHtml(unsafe) {
-   return unsafe
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;");
+    return unsafe
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
 }
 
-d3.json("/api/CrontabberState/", function(data) {
+d3.json('/api/CrontabberState/', function(data) {
 
     /**
      * Reshape the data
@@ -67,13 +76,15 @@ d3.json("/api/CrontabberState/", function(data) {
      *   nodes: [ { name: 'something' }, { name: 'otherthing' } ]
      *   links: [ { source: 0, target: 1, value: 9001 } ]
      */
-    var nodes = _.map(data.state, function(v, k, l) {
+    var nodes = _.map(data.state, function(v, k) {
         v.name = k;
+        // prefix with a _ because it's not native to the source data
+        v._obsolete = new Date(v.next_run) < longAgo;
         return v;
     });
 
     // cache array positions
-    _.each(nodes, function(e, i, l) {
+    _.each(nodes, function(e, i) {
         e.pos = i;
     });
 
@@ -87,7 +98,7 @@ d3.json("/api/CrontabberState/", function(data) {
                 return data.state[name];
             }),
             skips = _.map(parents, count_skips),
-            errors = _.pluck(parents, "error_count");
+            errors = _.pluck(parents, 'error_count');
         skips = _.max([_.max(errors) + _.max(skips), 0]);
         node.skips = skips;
         return skips;
@@ -95,7 +106,7 @@ d3.json("/api/CrontabberState/", function(data) {
 
     // reverse linked lists
     var links = [];
-    _.each(nodes, function(element, index, list) {
+    _.each(nodes, function(element, index) {
         _.each(element.depends_on, function (d) {
             var dep = data.state[d];
             links.push({
@@ -103,7 +114,7 @@ d3.json("/api/CrontabberState/", function(data) {
                 target: index,
                 value: 1,
                 errors: dep.error_count,
-                skips: dep.skips
+                skips: dep.skips,
             });
         });
     });
@@ -113,52 +124,57 @@ d3.json("/api/CrontabberState/", function(data) {
         .links(links)
         .layout(32);
 
-    var link = svg.append("g").selectAll(".link")
+    var link = svg.append('g').selectAll('.link')
         .data(links)
-      .enter().append("path")
-        .attr("class", "link")
-        .attr("d", path)
-        .style("stroke-width", function(d) {
+      .enter().append('path')
+        .attr('class', 'link')
+        .attr('d', path)
+        .style('stroke-width', function(d) {
             return Math.max(1, d.dy);
         })
-        .style("stroke", function(d) {
+        .style('stroke', function(d) {
             return d.color = color(d);
         })
         .sort(function(a, b) { return b.dy - a.dy; });
 
-    link.append("title")
+    link.append('title')
         .text(title);
 
-    var node = svg.append('g').selectAll(".node")
+    var node = svg.append('g').selectAll('.node')
         .data(nodes)
-      .enter().append('g')
+        .enter()
+        .append('g')
         .attr('class', 'node')
         .attr('transform', function(d) {
-            return "translate(" + d.x + "," + d.y + ")";
+            return 'translate(' + d.x + ',' + d.y + ')';
         })
       .call(d3.behavior.drag()
         .origin(function(d) { return d; })
         .on('dragstart', function() {
             this.parentNode.appendChild(this);
         })
-        .on("drag", dragmove));
+        .on('drag', dragmove));
 
-    node.append("rect")
-        .attr("height", function(d) { return d.dy; })
-        .attr("width", sankey.nodeWidth())
-        .style("fill", function(d) {
+    node.append('rect')
+        .attr('height', function(d) { return d.dy; })
+        .attr('width', sankey.nodeWidth())
+        .style('fill', function(d) {
             return d.color = color(d);
         })
-        .style("stroke", function(d) {
+        .style('opacity', function(d) {
+            return d.opacity = d._obsolete && 0.3 || 1.0;
+        })
+        .style('stroke', function(d) {
             return d3.rgb(d.color).darker(2);
         })
-      .append("title")
-        .text(title);
+      .append('title')
+        .text(title)
+        .style(color);
 
     node.append('text')
         .attr('x', -6)
         .attr('y', function(d) { return d.dy / 2; })
-        .attr('dy', ".35em")
+        .attr('dy', '.35em')
         .attr('text-anchor', 'end')
         .attr('transform', null)
         .text(function(d) { return d.name; })
@@ -169,10 +185,10 @@ d3.json("/api/CrontabberState/", function(data) {
     function dragmove(d) {
         d.y = Math.max(0, Math.min(height - d.dy, d3.event.y));
         d3.select(this).attr(
-            "transform",
-            "translate(" + d.x + "," + d.y + ")");
+            'transform',
+            'translate(' + d.x + ',' + d.y + ')');
         sankey.relayout();
-        link.attr("d", path);
+        link.attr('d', path);
     }
 
     // misconfigured, no jobs
@@ -189,23 +205,26 @@ d3.json("/api/CrontabberState/", function(data) {
             'error_count',
             'next_run',
             'last_success',
-            'depends_on'
+            'depends_on',
         ];
 
     table.classed('data-table tablesorter', true);
 
-    thead.append("tr").selectAll("th")
+    thead.append('tr').selectAll('th')
         .data(tableFields)
-      .enter().append("th")
+      .enter().append('th')
         .text(function capitalize(s) {
             return s[0].toUpperCase() + s.slice(1).replace('_', ' ');
         })
-        .classed("header", true);
+        .classed('header', true);
 
-    var tr = tbody.selectAll("tr")
+    tbody.selectAll('tr')
         .data(nodes)
-      .enter().append("tr")
-        .selectAll("td")
+        .enter().append('tr')
+        .filter(function(node) {
+            return !node._obsolete;
+        })
+        .selectAll('td')
         .data(function(d) {
             // get only the tableFields
             var scrubbed = _.map(tableFields, function(field) {
@@ -213,10 +232,10 @@ d3.json("/api/CrontabberState/", function(data) {
             });
             return scrubbed;
         })
-      .enter().append("td")
+        .enter().append('td')
         .text(function(d, i) {
-            field = tableFields[i];
-            isTime = (field === "last_success" || field === "next_run");
+            var field = tableFields[i];
+            var isTime = (field === 'last_success' || field === 'next_run');
             if (isTime) {
                 if (d) {
                     return moment(d).fromNow();
@@ -224,8 +243,8 @@ d3.json("/api/CrontabberState/", function(data) {
                     return '';
                 }
             }
-            if (typeof(d) === "object") {
-                joined = _.reduce(d, function(m, i) {
+            if (typeof(d) === 'object') {
+                var joined = _.reduce(d, function(m, i) {
                     return m + i + ', ';
                 }, '');
                 return joined.substring(0, joined.length - 2);
@@ -245,29 +264,29 @@ d3.json("/api/CrontabberState/", function(data) {
 
     table.classed('data-table tablesorter', true);
 
-    thead.append("tr").selectAll("th")
+    thead.append('tr').selectAll('th')
         .data(tableFields)
-      .enter()
-        .append("th")
+        .enter()
+        .append('th')
 
         .text(function capitalize(s) {
             if (s === 'ongoing') {
-                return "Ongoing for";
+                return 'Ongoing for';
             }
             return s[0].toUpperCase() + s.slice(1).replace('_', ' ');
         })
-        .classed("header", true);
+        .classed('header', true);
 
-    tr = tbody.selectAll("tr")
+    tbody.selectAll('tr')
         .data(nodes)
-      .enter().append("tr")
+        .enter().append('tr')
         .filter(function(node) {
             if (node.ongoing) {
                 anyOngoing = true;
             }
             return node.ongoing;
         })
-        .selectAll("td")
+        .selectAll('td')
         .data(function(d) {
             // get only the tableFields
             var scrubbed = _.map(tableFields, function(field) {
@@ -275,9 +294,9 @@ d3.json("/api/CrontabberState/", function(data) {
             });
             return scrubbed;
         })
-      .enter().append("td")
+        .enter().append('td')
         .text(function(d, i) {
-            field = tableFields[i];
+            var field = tableFields[i];
             if (field === 'ongoing') {
                 return moment(d).fromNow(true);
             }
@@ -285,8 +304,60 @@ d3.json("/api/CrontabberState/", function(data) {
         });
 
     if (anyOngoing) {
-        // if there are any ongoing rows, only they display the ongoing panel
+        // if there are any ongoing rows, only they display the obsolete panel
         $('div.ongoing').show();
+    }
+
+    // now do only the obsolete jobs
+    table = d3.select('.obsolete .body').append('table');
+    thead = table.append('thead');
+    tbody = table.append('tbody');
+    tableFields = [
+        'name',
+        'last_run',
+    ];
+    var anyObsolete = false;
+
+    table.classed('data-table tablesorter', true);
+    thead.append('tr').selectAll('th')
+        .data(tableFields)
+        .enter()
+        .append('th')
+        .text(function capitalize(s) {
+            return s[0].toUpperCase() + s.slice(1).replace('_', ' ');
+        })
+        .classed('header', true);
+
+    tbody.selectAll('tr')
+        .data(nodes)
+        .enter().append('tr')
+        .filter(function(node) {
+            if (node._obsolete) {
+                anyObsolete = true;
+                return true;
+            }
+            return false;
+        })
+        .selectAll('td')
+        .data(function(d) {
+            // get only the tableFields
+            var scrubbed = _.map(tableFields, function(field) {
+                return d[field];
+            });
+            return scrubbed;
+        })
+        .enter().append('td')
+        .text(function(d, i) {
+            var field = tableFields[i];
+            if (field === 'last_run') {
+                return moment(d).fromNow(false);
+            }
+            return d;
+        });
+
+    if (anyObsolete) {
+        // if there are any ongoing rows, only they display the ongoing panel
+        $('div.obsolete').show();
     }
 
     table = d3.select('#failing').append('table');
