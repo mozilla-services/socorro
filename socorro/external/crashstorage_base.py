480a326e7bfacd17be96baf4f18ad8e59b127bca
Adrian Gaudebert <adrian@gaudebert.fr>
2017-05-16 12:14:20 +0200

    Better variable usage.

--- socorro/external/crashstorage_base.py
+++ socorro/external/crashstorage_base.py
@@ -596,26 +596,26 @@ class PolyCrashStorage(CrashStorageBase):
             try:
                 actual_store = getattr(a_store, 'wrapped_object', a_store)
 
-                if (
-                    hasattr(actual_store, 'is_mutator') and
-                    actual_store.is_mutator()
-                ):
+                if hasattr(actual_store, 'is_mutator') and actual_store.is_mutator():
                     # We do this because `a_store.save_raw_and_processed`
                     # expects the processed crash to be a DotDict but
                     # you can't deepcopy those, so we deepcopy the
                     # pure dict version and then dress it back up as a
                     # DotDict.
-                    processed_crash = SocorroDotDict(
+                    my_processed_crash = SocorroDotDict(
                         copy.deepcopy(processed_crash_as_dict)
                     )
-                    raw_crash = SocorroDotDict(
+                    my_raw_crash = SocorroDotDict(
                         copy.deepcopy(raw_crash_as_dict)
                     )
+                else:
+                    my_processed_crash = processed_crash
+                    my_raw_crash = raw_crash
 
                 a_store.save_raw_and_processed(
-                    raw_crash,
+                    my_raw_crash,
                     dump,
-                    processed_crash,
+                    my_processed_crash,
                     crash_id
                 )
             except Exception:
