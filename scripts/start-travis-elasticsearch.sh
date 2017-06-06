c60baa11b975aa70c41578e07dd6061bade9219b
Adrian Gaudebert <adrian@gaudebert.fr>
2017-06-06 17:05:37 +0200

    Updated travis and circle to run with latest Elasticsearch.

--- scripts/start-travis-elasticsearch.sh
+++ scripts/start-travis-elasticsearch.sh
@@ -4,9 +4,9 @@ set -ex
 
 # Why download it and not use the elasticsearch service?
 # Two reasons
-#  - predictable version (5.1.x) is what we use in production
+#  - predictable version (5.3.x) is what we use in production
 #  - running it as a service would require sudo which we don't want to use
-wget --no-check-certificate 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.deb'
-sudo dpkg -i --force-confnew elasticsearch-5.1.1.deb
+wget --no-check-certificate 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.3.deb'
+sudo dpkg -i --force-confnew elasticsearch-5.3.3.deb
 
 sudo service elasticsearch start
