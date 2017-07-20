c061cade9fe99989631a11f9e764d3d1510b5dae
Peter Bengtsson <peterbe@mozilla.com>
2017-07-20 10:30:25 -0400

    bug 1375426 - rewrite graphics report with SuperSearch (#3831)
    * bug 1375426 - rewrite graphics report with SuperSearch
    
    * oops
    
    * review comments
    
    * nits addressed

--- webapp-django/crashstats/crashstats/models.py
+++ webapp-django/crashstats/crashstats/models.py
@@ -21,7 +21,6 @@ import socorro.external.postgresql.platforms
 import socorro.external.postgresql.bugs
 import socorro.external.postgresql.crashes
 import socorro.external.postgresql.products
-import socorro.external.postgresql.graphics_report
 import socorro.external.postgresql.graphics_devices
 import socorro.external.postgresql.crontabber_state
 import socorro.external.postgresql.adi
@@ -1138,28 +1137,6 @@ class ProductBuildTypes(SocorroMiddleware):
 
     API_WHITELIST = (
         'hits',
-    )
-
-
-class GraphicsReport(SocorroMiddleware):
-    """The legacy solution to supply the CSV reports that the Mozilla
-    Graphics Team needs."""
-
-    # This endpoint is protected in a django view with permission
-    # requirements. That means we don't have to worry about it being
-    # overly requested by rogue clients.
-    # Also, the response payload is usually very very large meaning
-    # it will cause strain having to store it in the cacheing server
-    # when it does get re-used much by repeated queries.
-    cache_seconds = 0
-
-    implementation = (
-        socorro.external.postgresql.graphics_report.GraphicsReport
-    )
-
-    required_params = (
-        'product',
-        ('date', datetime.date),
     )
 
 
