e89451f22a40fd115d0c80531d34d854e37c7d2c
Peter Bengtsson <mail@peterbe.com>
2017-07-24 15:02:42 -0400

    picking first differently

--- webapp-django/crashstats/base/tests/test_finders.py
+++ webapp-django/crashstats/base/tests/test_finders.py
@@ -14,15 +14,15 @@ class LeftoverPipelineFinder(DjangoTestCase):
 
     def test_missing_css_source_file(self):
         busted_pipeline = copy.deepcopy(settings.PIPELINE)
-        # Doesn't matter which key we chose to bust
-        for key in busted_pipeline['STYLESHEETS']:
-            filenames = busted_pipeline['STYLESHEETS'][key]['source_filenames']
-            # add a junk one
-            filenames += ('neverheardof.css',)
-            busted_pipeline['STYLESHEETS'][key]['source_filenames'] = (
-                filenames
-            )
-            break
+        # Doesn't matter which key we chose to bust, so let's just
+        # pick the first one.
+        key = busted_pipeline['STYLESHEETS'].keys()[0]
+        filenames = busted_pipeline['STYLESHEETS'][key]['source_filenames']
+        # add a junk one
+        filenames += ('neverheardof.css',)
+        busted_pipeline['STYLESHEETS'][key]['source_filenames'] = (
+            filenames
+        )
         with self.settings(PIPELINE=busted_pipeline):
             assert_raises(
                 ImproperlyConfigured,
