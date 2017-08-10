a03b4e584f0428d6da79ab8eaa05f0aed0d347a1
Peter Bengtsson <mail@peterbe.com>
2017-08-10 11:04:17 -0400

    simplified memoize function

--- socorro/cron/jobs/ftpscraper.py
+++ socorro/cron/jobs/ftpscraper.py
@@ -5,7 +5,6 @@ import os
 import json
 import urlparse
 import fnmatch
-from functools import partial
 
 import mock
 import lxml.html
@@ -26,43 +25,15 @@ from socorro.app.socorro_app import App, main
 from socorro.lib.datetimeutil import string_to_datetime
 
 
-class memoize(object):
-    """cache the return value of a method
+def memoize_download(fun):
+    cache = {}
 
-    This class is meant to be used as a decorator of methods. The return value
-    from a given method invocation will be cached on the instance whose method
-    was invoked. All arguments passed to a method decorated with memoize must
-    be hashable.
+    def inner(self, url):
+        if url not in cache:
+            cache[url] = fun(self, url)
+        return cache[url]
 
-    If a memoized method is invoked directly on its class the result will not
-    be cached. Instead the method will be invoked like a static method:
-    class Obj(object):
-        @memoize
-        def add_to(self, arg):
-            return self + arg
-    Obj.add_to(1) # not enough arguments
-    Obj.add_to(1, 2) # returns 3, result is not cached
-    """
-    def __init__(self, func):
-        self.func = func
-
-    def __get__(self, obj, objtype=None):
-        if obj is None:
-            return self.func
-        return partial(self, obj)
-
-    def __call__(self, *args, **kw):
-        obj = args[0]
-        try:
-            cache = obj.__cache
-        except AttributeError:
-            cache = obj.__cache = {}
-        key = (self.func, args[1:], frozenset(kw.items()))
-        if key in cache:
-            res = cache[key]
-        else:
-            res = cache[key] = self.func(*args, **kw)
-        return res
+    return inner
 
 
 class ScrapersMixin(object):
@@ -310,7 +281,7 @@ class FTPScraperCronApp(BaseCronApp, ScrapersMixin):
             HTTPAdapter(max_retries=self.config.retries)
         )
 
-    @memoize
+    @memoize_download
     def download(self, url):
         response = self.session.get(
             url,
