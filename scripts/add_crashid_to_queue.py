b0a74b78ae8743d434daee2705a6bbb62b6870e3
Will Kahn-Greene <willkg@mozilla.com>
2017-08-14 15:52:33 -0400

    bug 1388449 - add add_crashid_to_queue and scaffolding
    * adds a is_crash_id_valid lib function for verifying crash ids
    * adds a scripts/add_crashid_to_queue.py script which is a stub that executes
      functionality in socorro/scripts/add_crashid_to_queue.py
    * adds unit tests for new module

---
+++ scripts/add_crashid_to_queue.py
@@ -0,0 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# See socorro/scripts/add_crashid_to_queue.py
+
+import sys
+
+from socorro.scripts import add_crashid_to_queue
+
+
+if __name__ == '__main__':
+    sys.exit(add_crashid_to_queue.main(sys.argv[1:]))
