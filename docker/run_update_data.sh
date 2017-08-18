ac94ab3701140519bb2c389912459570869dc00c
Will Kahn-Greene <willkg@mozilla.com>
2017-08-18 16:14:01 -0400

    fixes bug 1391771 - fixes processor in local dev environment
    * fix configuration so processor uses S3 container
    * add two more things to run_update_data.sh that maintain the appropriate
      weekly tables
    * update localstack to 0.7.4

--- docker/run_update_data.sh
+++ docker/run_update_data.sh
@@ -8,14 +8,22 @@ set -eo pipefail
 
 # Fetch and update release information for these products (comma-delimited)
 PRODUCTS="firefox"
+CRONTABBER="docker-compose run crontabber python socorro/cron/crontabber_app.py"
+
 
 # Fetch release data
-docker-compose run crontabber python socorro/cron/crontabber_app.py --job=ftpscraper \
+${CRONTABBER} --job=ftpscraper \
        --crontabber.class-FTPScraperCronApp.products=${PRODUCTS}
 
 # Update featured versions data based on release data
-docker-compose run crontabber python socorro/cron/crontabber_app.py --job=featured-versions-automatic \
+${CRONTABBER} --job=featured-versions-automatic \
        --crontabber.class-FeaturedVersionsAutomaticCronApp.products=${PRODUCTS}
 
 # Fetch normalization data for versions we know about
 docker-compose run processor python docker/fetch_normalization_data.py
+
+# Truncate tables
+${CRONTABBER} --job=truncate-partitions
+
+# Create new weekly tables
+${CRONTABBER} --job=weekly-reports-partitions
