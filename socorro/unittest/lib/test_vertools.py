33dab04dddf8a9244c2e24aba370b3962e802b02
Will Kahn-Greene <willkg@mozilla.com>
2017-08-30 07:56:07 -0400

    Adjust normalize test removing ver2 which isn't used

--- socorro/unittest/lib/test_vertools.py
+++ socorro/unittest/lib/test_vertools.py
@@ -11,15 +11,39 @@ phs = ver_tools._padding_high_string
 pl = ver_tools._padding_list
 
 
-@pytest.mark.parametrize('ver, expected, ver2', [
-    ('3', [3, phs, 0, phs] + pl * 3, '3'),
-    ('3.', [3, phs, 0, phs] + pl * 3, '3'),
-    ('3.0', [3, phs, 0, phs] + pl * 3, '3'),
-    ('3.0.0', [3, phs, 0, phs] + pl * 3, '3'),
-    ('3.5', [3, phs, 0, phs, 5, phs, 0, phs] + pl * 2, '3.5'),
-    ('3.5pre', [3, phs, 0, phs, 5, 'pre', 0, phs] + pl * 2, '3.5pre'),
-    ('3.5b3', [3, phs, 0, phs, 5, 'b', 3, phs] + pl * 2, '3.5b3'),
-    ('3.6.4plugin3', [3, phs, 0, phs, 6, phs, 0, phs, 4, 'plugin', 3, phs] + pl, '3.6.4plugin3'),
+@pytest.mark.parametrize('ver, expected', [
+    (
+        '3',
+        [3, phs, 0, phs] + pl * 3
+    ),
+    (
+        '3.',
+        [3, phs, 0, phs] + pl * 3
+    ),
+    (
+        '3.0',
+        [3, phs, 0, phs] + pl * 3
+    ),
+    (
+        '3.0.0',
+        [3, phs, 0, phs] + pl * 3
+    ),
+    (
+        '3.5',
+        [3, phs, 0, phs, 5, phs, 0, phs] + pl * 2
+    ),
+    (
+        '3.5pre',
+        [3, phs, 0, phs, 5, 'pre', 0, phs] + pl * 2
+    ),
+    (
+        '3.5b3',
+        [3, phs, 0, phs, 5, 'b', 3, phs] + pl * 2
+    ),
+    (
+        '3.6.4plugin3',
+        [3, phs, 0, phs, 6, phs, 0, phs, 4, 'plugin', 3, phs] + pl
+    ),
 ])
-def test_normalize(ver, expected, ver2):
+def test_normalize(ver, expected):
     assert ver_tools.normalize(ver) == expected
