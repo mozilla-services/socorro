9118e5a4508db4b44edae1765a7ef2c544287c1f
Will Kahn-Greene <willkg@mozilla.com>
2017-10-09 13:39:02 -0400

    Run flake8 on socorro/unittest/external/ files

--- socorro/unittest/external/statsd/test_stastsd_rule_benchmark.py
+++ socorro/unittest/external/statsd/test_stastsd_rule_benchmark.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from datetime import datetime
-
 from configman.dotdict import DotDict
 from mock import patch, call, Mock
 import pytest
