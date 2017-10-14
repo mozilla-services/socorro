937f64913b635e057a0c4bc2c5ba4dcbc3cb5e45
Will Kahn-Greene <willkg@mozilla.com>
2017-10-14 08:14:44 -0400

    bug 1405675 - update crashstats/symbols/tests/* to pytest

--- webapp-django/crashstats/symbols/tests/test_utils.py
+++ webapp-django/crashstats/symbols/tests/test_utils.py
@@ -1,5 +1,3 @@
-from nose.tools import ok_
-
 from crashstats.symbols import utils
 from crashstats.base.tests.testbase import TestCase
 
@@ -17,30 +15,30 @@ class TestUtils(TestCase):
         with open(ZIP_FILE) as f:
             result = utils.preview_archive_content(f, 'foo.zip')
             # the sample.zip file contains...
-            ok_('south-africa-flag.jpeg' in result)
+            assert 'south-africa-flag.jpeg' in result
             # and it's 69183 bytes
-            ok_('69183' in result)
+            assert '69183' in result
 
     def test_preview_tar(self):
         with open(TAR_FILE) as f:
             result = utils.preview_archive_content(f, 'foo.tar')
             # the sample.tar file contains...
-            ok_('south-africa-flag.jpeg' in result)
+            assert 'south-africa-flag.jpeg' in result
             # and it's 69183 bytes
-            ok_('69183' in result)
+            assert '69183' in result
 
     def test_preview_tgz(self):
         with open(TGZ_FILE) as f:
             result = utils.preview_archive_content(f, 'foo.tgz')
             # the sample.tgz file contains...
-            ok_('south-africa-flag.jpeg' in result)
+            assert 'south-africa-flag.jpeg' in result
             # and it's 69183 bytes
-            ok_('69183' in result)
+            assert '69183' in result
 
     def test_preview_targz(self):
         with open(TARGZ_FILE) as f:
             result = utils.preview_archive_content(f, 'foo.tar.gz')
             # the sample.tar.gz file contains...
-            ok_('south-africa-flag.jpeg' in result)
+            assert 'south-africa-flag.jpeg' in result
             # and it's 69183 bytes
-            ok_('69183' in result)
+            assert '69183' in result
