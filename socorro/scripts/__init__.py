7633f253049a1b464ae251b2587ea9ee9e8b05cb
Will Kahn-Greene <willkg@mozilla.com>
2017-10-16 10:36:14 -0400

    fixes bug 1409038 - Add --dumps/--no-dumps flag to scripts/fetch_crash_data.py

--- socorro/scripts/__init__.py
+++ socorro/scripts/__init__.py
@@ -24,3 +24,40 @@ class WrappedTextHelpFormatter(argparse.HelpFormatter):
                 parts[i] = super(WrappedTextHelpFormatter, self)._fill_text(part, width, indent)
 
         return '\n\n'.join(parts)
+
+
+class FlagAction(argparse.Action):
+    """Facilitates --flag, --no-flag arguments
+
+    Usage::
+
+        parser.add_argument(
+            '--flag', '--no-flag', action=FlagAction, default=True,
+            help='whether to enable flag'
+        )
+
+
+    Which allows you to do::
+
+        $ command
+        $ command --flag
+        $ command --no-flag
+
+
+    And the help works nicely, too::
+
+        $ command --help
+        ...
+        optional arguments:
+           --flag, --no-flag  whether to enable flag
+
+    """
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super(FlagAction, self).__init__(option_strings, dest, nargs=0, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if option_string.startswith('--no'):
+            value = False
+        else:
+            value = True
+        setattr(namespace, self.dest, value)
