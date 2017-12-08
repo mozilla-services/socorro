2221e8265c0fc3c51c96f16ce4c1558f2e152ff1
Will Kahn-Greene <willkg@mozilla.com>
2017-12-07 22:11:42 -0500

    First pass at scripts cleanup
    This removes:
    
    * scripts related to building .deb packages--we don't do that anymore
    * scripts for managing the database--we don't use these anymore
    * Makefile rules used by Vagrant that we don't use anymore
    * scripts/lint.sh--we use flake8 now

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Jenkins build script for running tests and packaging build
+# Travis CI build script for running tests
 
 echo "this is build.sh"
 
--- scripts/package/deb-before-install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -e
-
-# Create a new socorro user
-set +e
-grep socorro /etc/passwd 2>&1 > /dev/null
-if [ $? -ne 0 ]; then
-    useradd -m socorro 2> /dev/null
-fi
-set -e
