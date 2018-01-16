28f9c9d7501cd956bc6f014fad7f2d34cbfb26be
Calixte Denizet <calixte.denizet@gmail.com>
2018-01-16 16:45:28 +0100

    Bug 1430181: Add an API endpoint (CrashStopData) to Socorro.
    The purpose of this endpoint is to have in one shot all the data needed by the webextension CrashStop which displays crash data in Bugzilla.

--- webapp-django/crashstats/tools/forms.py
+++ webapp-django/crashstats/tools/forms.py
@@ -16,3 +16,32 @@ class NewSignaturesForm(BaseForm):
         """Remove all empty values from the list of products. """
         value = self.cleaned_data['product']
         return filter(bool, value)
+
+
+class CrashStopDataForm(BaseForm):
+
+    buildid = MultipleValueField(required=True)
+    signature = MultipleValueField(required=True)
+    product = MultipleValueField(required=True)
+    channel = MultipleValueField(required=True)
+
+    def clean(self, s):
+        """Remove all empty values from the list """
+        value = self.cleaned_data[s]
+        return filter(bool, value)
+
+    def clean_product(self):
+        """Remove all empty values from the list of products. """
+        return self.clean('product')
+
+    def clean_channel(self):
+        """Remove all empty values from the list of channels. """
+        return self.clean('channel')
+
+    def clean_buildid(self):
+        """Remove all empty values from the list of buildids. """
+        return self.clean('buildid')
+
+    def clean_signature(self):
+        """Remove all empty values from the list of signatures. """
+        return self.clean('signature')
