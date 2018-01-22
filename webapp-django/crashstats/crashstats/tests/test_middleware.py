33342491e38039c6311ee9a7c4b51a8c88699bc6
Michael Kelly <mkelly@mozilla.com>
2018-01-22 12:19:21 -0800

    Fix bug 1321870: Use last IP instead of first from X-Forwarded-For.

--- webapp-django/crashstats/crashstats/tests/test_middleware.py
+++ webapp-django/crashstats/crashstats/tests/test_middleware.py
@@ -7,15 +7,31 @@ from crashstats.crashstats.middleware import SetRemoteAddrFromForwardedFor
 class TestSetRemoteAddrFromForwardedFor(DjangoTestCase):
 
     def test_no_headers(self):
-        """should not break if there is no HTTP_X_FORWARDED_FOR"""
+        """Should not break if there is no HTTP_X_FORWARDED_FOR"""
         middleware = SetRemoteAddrFromForwardedFor()
         request = RequestFactory().get('/')
         response = middleware.process_request(request)
         assert response is None
 
-    def test_happy_path(self):
-        """the first (comma separated) IP in HTTP_X_FORWARDED_FOR should
-        update request.META['REMOTE_ADDR']"""
+    def test_single_ip(self):
+        """Ihe IP in HTTP_X_FORWARDED_FOR should update
+        request.META['REMOTE_ADDR'].
+
+        """
+        middleware = SetRemoteAddrFromForwardedFor()
+        request = RequestFactory(**{
+            'HTTP_X_FORWARDED_FOR': '100.100.100.100',
+            'REMOTE_ADDR': '123.123.123.123',
+        }).get('/')
+        response = middleware.process_request(request)
+        assert response is None
+        assert request.META['REMOTE_ADDR'] == '100.100.100.100'
+
+    def test_ip_list(self):
+        """The last (comma separated) IP in HTTP_X_FORWARDED_FOR should
+        update request.META['REMOTE_ADDR'].
+
+        """
         middleware = SetRemoteAddrFromForwardedFor()
         request = RequestFactory(**{
             'HTTP_X_FORWARDED_FOR': '245.245.245.245 , 100.100.100.100',
@@ -23,4 +39,4 @@ class TestSetRemoteAddrFromForwardedFor(DjangoTestCase):
         }).get('/')
         response = middleware.process_request(request)
         assert response is None
-        assert request.META['REMOTE_ADDR'] == '245.245.245.245'
+        assert request.META['REMOTE_ADDR'] == '100.100.100.100'
