39f815a6bb59f5153c45d969223834ba3ecc5e92
Will Kahn-Greene <willkg@mozilla.com>
2018-03-12 12:18:56 -0400

    fix bug 1442022 - nix new relic
    We don't use new relic much anymore, so this nixes it.

--- webapp-django/wsgi/socorro-crashstats.py
+++ webapp-django/wsgi/socorro-crashstats.py
@@ -1,29 +1,8 @@
 from __future__ import print_function
 
 import os
-import sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crashstats.settings')
 
 from django.core.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
-
-
-try:
-    import newrelic.agent
-except ImportError:
-    newrelic = False
-
-
-if newrelic:
-    newrelic_ini = os.getenv('NEWRELIC_PYTHON_INI_FILE', None)
-    if newrelic_ini:
-        if os.path.isfile(newrelic_ini):
-            newrelic.agent.initialize(newrelic_ini)
-            application = newrelic.agent.wsgi_application()(application)
-        else:
-            print(
-                "NEWRELIC_PYTHON_INI_FILE set but file does not exist. "
-                "Skipping to initialize newrelic agent.",
-                file=sys.stderr
-            )
