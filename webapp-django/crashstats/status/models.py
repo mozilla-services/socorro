1c2cfb376abf31e45f20af55329bf0d720c00e63
Will Kahn-Greene <willkg@mozilla.com>
2018-05-22 11:45:57 -0400

    Add StatusMessage to Django admin

--- webapp-django/crashstats/status/models.py
+++ webapp-django/crashstats/status/models.py
@@ -4,6 +4,13 @@ from django.utils import timezone
 
 class StatusMessage(models.Model):
     message = models.TextField()
-    severity = models.CharField(max_length=20)
+    severity = models.CharField(
+        max_length=20,
+        choices=(
+            ('info', 'Info'),
+            ('warning', 'Warning'),
+            ('critical', 'Critical'),
+        )
+    )
     enabled = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=timezone.now)
