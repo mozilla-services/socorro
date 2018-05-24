c04df654f46c040cd6faedf6cafc63a604aec23c
Will Kahn-Greene <willkg@mozilla.com>
2018-05-24 12:34:09 -0400

    fix bug 1464107: add script to run migrations
    This codifies all the migration stuff in a single script which we'll run
    as a post-deploy script.
    
    This adds curl and gawk to the Ubuntu setup. We need curl for installing
    sentry-cli and we need gawk because it's the version of awk that sentry-cli
    uses.
    
    This also adds the sentry-cli to the crontabber container. This enables
    us to send errors in bash scripts to Sentry.

---
+++ scripts/run_migrations.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This script runs migrations for Socorro. Run this in a crontabber docker
+# container.
+
+# If RAVEN_DSN is defined, then define SENTRY_DSN and evoke the sentry-cli bash
+# hook which will send errors to sentry.
+if [ -n "${RAVEN_DSN}" ]; then
+    echo "RAVEN_DSN defined--enabling sentry."
+    export SENTRY_DSN=${RAVEN_DSN}
+    eval "$(sentry-cli bash-hook)"
+else
+    echo "RAVEN_DSN not defined--not enabling sentry."
+fi
+
+# Get a datestamp
+date
+
+# Run Django migrations
+python webapp-django/manage.py migrate --no-input
+
+# Run Alembic migrations
+alembic -c docker/config/alembic.ini upgrade head
