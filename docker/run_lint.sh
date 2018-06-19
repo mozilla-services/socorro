c8c543393dd1bcf8894b2b905879788141de3c03
Will Kahn-Greene <willkg@mozilla.com>
2018-06-19 14:04:10 -0400

    fix bug 1468763: fix lint and test rules
    This fixes linting and testing so there's a single make rule for linting and
    one for testing. This fixes Circle configuration so it runs both.

---
+++ docker/run_lint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Runs linting.
+
+# This should be called from inside a container.
+
+set -e
+
+# Run flake8 to lint Python tests
+echo ">>> flake8 (python)"
+cd /app
+flake8
+
+echo ">>> eslint (js)"
+cd /app/webapp-django
+/webapp-frontend-deps/node_modules/.bin/eslint .
