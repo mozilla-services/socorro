b4e93635c49f4c505f7175eba1632aaedbb4bb9c
Will Kahn-Greene <willkg@mozilla.com>
2018-06-26 14:27:35 -0400

    fix bug 1471299: remove orphaned code the submitter used
    We rewrote the stage submitter as an AWS Lambda job and no longer have
    the stage submitter code in the socorro repo. This removes some code
    that the submitter used that we no longer use and has no tests.

--- socorro/external/fs/filesystem.py
+++ socorro/external/fs/filesystem.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from past.builtins import cmp
 import errno
 import os
 from os.path import curdir
@@ -99,65 +98,3 @@ def makedirs(name, mode=777, osModule=os):
             # it might be an existing non-directory
             if not osModule.path.isdir(name):
                 raise OSError(errno.ENOTDIR, "Not a directory: '%s'" % name)
-
-
-# Copyright 2005 by Centaur Software Engineering, Inc.
-#
-#    This file is part of The CSE Python Library.
-#
-#    The CSE Python Library is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU General Public License as published
-#    by the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    The CSE Python Library is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with The CSE Python Library; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-def defaultAcceptanceFunction(dummy):
-    return True
-
-
-# FIXME(willkg): This is only used in the submtter. We should delete it or rewrite with os.walk.
-def findFileGenerator(
-    rootDirectory,
-    acceptanceFunction=defaultAcceptanceFunction,
-    directoryAcceptanceFunction=defaultAcceptanceFunction,
-    directorySortFunction=cmp,
-    maxDepth=100000,
-    **kwargs
-):
-    """This function returns a generator that walks a filesystem tree.  It
-    applies a user supplied function to each file that it encounters.  It only
-    returns files for which the user supplied function returns true. It returns
-    a tuple consiting of three items: the root path of the file, the name of
-    the object, and the complete pathname of the object.  The user supplied
-    function uses the same tuple as its input.
-    """
-    if 0 >= maxDepth:
-        return
-    keep_dir = directoryAcceptanceFunction
-    for aCurrentDirectoryItem in [
-            (rootDirectory, x, os.path.join(rootDirectory, x))
-            for x in sorted(os.listdir(rootDirectory), directorySortFunction)
-    ]:
-        if (os.path.isdir(aCurrentDirectoryItem[2]) and keep_dir(aCurrentDirectoryItem)):
-            for aSubdirectoryItem in findFileGenerator(
-                aCurrentDirectoryItem[2],
-                acceptanceFunction,
-                directoryAcceptanceFunction,
-                directorySortFunction,
-                maxDepth=maxDepth - 1
-            ):
-                yield aSubdirectoryItem
-        if acceptanceFunction(aCurrentDirectoryItem):
-            yield aCurrentDirectoryItem
-
-
-if __name__ == "__main__":
-    for path, name, pathname in findFileGenerator("."):
-        print(pathname)
