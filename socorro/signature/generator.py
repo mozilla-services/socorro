813b6d7d45507978de39a26ba24323008260606e
Will Kahn-Greene <willkg@mozilla.com>
2018-08-01 10:41:26 -0400

    fix bug 1463756: Remove unused logger

--- socorro/signature/generator.py
+++ socorro/signature/generator.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import logging
 import sys
 
 from socorro.signature.rules import (
@@ -37,9 +36,6 @@ DEFAULT_PIPELINE = [
     SigFixWhitespace(),
     SigTruncate(),
 ]
-
-
-logger = logging.getLogger(__name__)
 
 
 class SignatureGenerator:
