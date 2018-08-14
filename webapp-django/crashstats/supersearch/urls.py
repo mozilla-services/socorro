2140eb5574178676124057a1e9e107607404d9d7
Will Kahn-Greene <willkg@mozilla.com>
2018-08-14 12:56:57 -0400

    Fix lint issue

--- webapp-django/crashstats/supersearch/urls.py
+++ webapp-django/crashstats/supersearch/urls.py
@@ -1,21 +1,11 @@
 from django.conf.urls import url
 from . import views
 
-app_name='supersearch'
+app_name = 'supersearch'
 urlpatterns = [
-    url(r'^search/$',
-        views.search,
-        name='search'),
-    url(r'^search/custom/$',
-        views.search_custom,
-        name='search_custom'),
-    url(r'^search/results/$',
-        views.search_results,
-        name='search_results'),
-    url(r'^search/query/$',
-        views.search_query,
-        name='search_query'),
-    url(r'^search/fields/$',
-        views.search_fields,
-        name='search_fields'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^search/custom/$', views.search_custom, name='search_custom'),
+    url(r'^search/results/$', views.search_results, name='search_results'),
+    url(r'^search/query/$', views.search_query, name='search_query'),
+    url(r'^search/fields/$', views.search_fields, name='search_fields'),
 ]
