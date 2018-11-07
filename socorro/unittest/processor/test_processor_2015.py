d0ae0f347797b908584e7c6830d188dbeb795eff
Will Kahn-Greene <willkg@mozilla.com>
2018-11-07 11:43:09 -0500

    fix bug 1505233: push exception handling to process_crash
    We pulled all the exception handling out of Rule. This puts it in
    process_crash. It'll surface errors in sentry and also add a processor
    note so we can reprocess crashes.

--- socorro/unittest/processor/test_processor_2015.py
+++ socorro/unittest/processor/test_processor_2015.py
@@ -2,14 +2,20 @@ import ujson
 
 from configman import ConfigurationManager
 from configman.dotdict import DotDict
-from mock import Mock, patch
+from mock import MagicMock, Mock, patch
 
 from socorro.processor.processor_2015 import Processor2015
 from socorro.processor.general_transform_rules import (
     CPUInfoRule,
     OSInfoRule
 )
+from socorro.processor.rules.base import Rule
 from socorro.unittest.testbase import TestCase
+
+
+class BadRule(Rule):
+    def action(self, *args, **kwargs):
+        raise KeyError('pii')
 
 
 class TestProcessor2015(TestCase):
@@ -22,19 +28,61 @@ class TestProcessor2015(TestCase):
         config.logger = Mock()
         config.database_class = Mock()
         config.transaction_executor_class = Mock()
+        config.sentry = Mock()
+        config.processor_name = 'dwight'
         return config
 
-    def test_Processor2015_init(self):
-        Processor2015(self.get_config())
+    @patch('socorro.lib.raven_client.raven')
+    def test_rule_error(self, mock_raven):
+        captured_exceptions = []  # a global
+
+        def mock_capture_exception(exc_info):
+            captured_exceptions.append(exc_info[1])
+            return 'someidentifier'
+
+        client = MagicMock()
+
+        def mock_Client(**config):
+            client.config = config
+            client.captureException.side_effect = mock_capture_exception
+            return client
+
+        mock_raven.Client.side_effect = mock_Client
+
+        config = self.get_config()
+        config.sentry = DotDict()
+        config.sentry.dsn = ''
+
+        # Test with no dsn set
+        raw_crash = {
+            'uuid': '1'
+        }
+        processed_crash = DotDict()
+
+        processor = Processor2015(config, rules=[BadRule])
+        processor.process_crash(raw_crash, {}, processed_crash)
+
+        # Notes were added
+        assert (
+            processed_crash.processor_notes ==
+            'dwight; Processor2015; rule BadRule failed: KeyError'
+        )
+
+        # Test with dsn set
+        config.sentry.dsn = 'https://user:pwd@sentry.example.com/01/'
+        processed_crash = DotDict()
+
+        processor = Processor2015(config, rules=[BadRule])
+        processor.process_crash(raw_crash, {}, processed_crash)
+
+        # Notes were added again
+        assert (
+            processed_crash.processor_notes ==
+            'dwight; Processor2015; rule BadRule failed: KeyError'
+        )
+        assert isinstance(captured_exceptions[0], KeyError)
 
     def test_process_crash_existing_processed_crash(self):
-        config = self.get_config()
-        config.processor_name = 'dwight'
-
-        p = Processor2015(config, rules=[
-            CPUInfoRule,
-            OSInfoRule,
-        ])
         raw_crash = DotDict({
             'uuid': '1'
         })
@@ -44,13 +92,13 @@ class TestProcessor2015(TestCase):
             'started_datetime': '2014-01-01T00:00:00'
         })
 
+        p = Processor2015(self.get_config(), rules=[
+            CPUInfoRule,
+            OSInfoRule,
+        ])
         with patch('socorro.processor.processor_2015.utc_now') as faked_utcnow:
             faked_utcnow.return_value = '2015-01-01T00:00:00'
-            processed_crash = p.process_crash(
-                raw_crash,
-                raw_dumps,
-                processed_crash
-            )
+            processed_crash = p.process_crash(raw_crash, raw_dumps, processed_crash)
 
         assert processed_crash.success
         assert processed_crash.started_datetime == '2015-01-01T00:00:00'
@@ -62,88 +110,3 @@ class TestProcessor2015(TestCase):
             " we've been here before; yep"
         )
         assert processed_crash.processor_notes == expected
-
-    @patch('socorro.processor.breakpad_transform_rules.subprocess')
-    def test_process_over_255_chars(self, mocked_subprocess_module):
-        thread_over_255_chars = {
-            "frames": [
-                {
-                    "file": "nsTerminator.cpp:604367e1fa5e",
-                    "frame": 0,
-                    "function": ("mozilla::`anonymous namespace::"
-                                 "RunWatchdog(void *)" + "a" * 255),
-                    "function_offset": "0x0",
-                    "line": 151,
-                    "module": "xul.dll",
-                    "module_offset": "0x783f2b",
-                    "offset": "0x67903f2b",
-                    "registers": {
-                        "eax": "0x0000003f",
-                        "ebp": "0x163ff96c",
-                        "ebx": "0x0cf44450",
-                        "ecx": "0x691e3698",
-                        "edi": "0x76d3f551",
-                        "edx": "0x01dc1010",
-                        "efl": "0x00000246",
-                        "eip": "0x67903f2b",
-                        "esi": "0x0000003f",
-                        "esp": "0x163ff968"
-                    },
-                    "trust": "context"
-                }
-            ]
-        }
-        stackwalker_output_str = ujson.dumps({
-            "crash_info": {
-                "address": "0x0",
-                "crashing_thread": 0,
-                "type": "EXC_BAD_ACCESS / KERN_INVALID_ADDRESS"
-            },
-            "status": "OK",
-            "crashing_thread": thread_over_255_chars,
-            "threads": [thread_over_255_chars],
-            "system_info": {
-                "os": "Windows NT",
-                "cpu_arch": "x86"
-            },
-            "sensitive": {
-                "exploitability": "high"
-            },
-        })
-
-        config = self.get_config()
-        config.processor_name = 'dwight'
-
-        mocked_subprocess_handle = (
-            mocked_subprocess_module.Popen.return_value
-        )
-        mocked_subprocess_handle.wait.return_value = 0
-        mocked_subprocess_handle.stdout.read.return_value = (
-            stackwalker_output_str
-        )
-        p = Processor2015(config)
-
-        raw_crash = DotDict({
-            "uuid": "00000000-0000-0000-0000-000002140504",
-            "CrashTime": "1336519554",
-            "SecondsSinceLastCrash": "86985",
-            "PluginHang": "1",
-            "ProductName": "Firefox",
-            "Version": "19",
-            "BuildID": "20121031"
-        })
-        raw_dumps = {"upload_file_minidump": "a_fake_dump.dump"}
-
-        processed_crash = p.process_crash(
-            raw_crash,
-            raw_dumps,
-            DotDict()
-        )
-
-        assert processed_crash.success
-        expected = (
-            'dwight; Processor2015; SigTrunc: signature truncated due to length'
-        )
-        assert processed_crash.processor_notes == expected
-        assert processed_crash.signature.startswith('shutdownhang')
-        assert len(processed_crash.signature) == 255
