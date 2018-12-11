eec0c4ed742e3e9802d46abb49eb9171f9460ed4
Will Kahn-Greene <willkg@mozilla.com>
2018-12-11 14:02:34 -0500

    fix bug 1406703: switch to python:3.6.7-slim-jessie

--- docker/run_tests_in_docker.sh
+++ docker/run_tests_in_docker.sh
@@ -22,7 +22,7 @@ SOCORRO_GID=${SOCORRO_GID:-"10001"}
 
 # Use the same image we use for building docker images because it's cached.
 # Otherwise this doesn't make any difference.
-BASEIMAGENAME="python:3.6.5-slim"
+BASEIMAGENAME="python:3.6.7-slim-jessie"
 TESTIMAGE="local/socorro_app"
 
 # Start services in background (this is idempotent)
