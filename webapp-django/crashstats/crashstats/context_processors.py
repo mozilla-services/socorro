e601e715d4b3717fdd7745aba2f062633936a8d9
Will Kahn-Greene <willkg@mozilla.com>
2019-02-02 10:15:45 -0500

    Switch the context variable to "settings"

--- webapp-django/crashstats/crashstats/context_processors.py
+++ webapp-django/crashstats/crashstats/context_processors.py
@@ -3,7 +3,5 @@ from django.conf import settings as django_settings
 
 def settings(request):
     return {
-        'SETTINGS': django_settings,
-        'DEBUG': django_settings.DEBUG,
-        'GOOGLE_ANALYTICS_ID': django_settings.GOOGLE_ANALYTICS_ID,
+        'settings': django_settings,
     }
