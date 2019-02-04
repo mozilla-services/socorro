d5a799ff555a962c31913796880bb1b607e7fbe4
Will Kahn-Greene <willkg@mozilla.com>
2019-02-04 18:10:59 -0500

    fix bug 1525098: add licence header to all python files

--- webapp-django/crashstats/sources/views.py
+++ webapp-django/crashstats/sources/views.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import get_lexer_for_filename, CppLexer
