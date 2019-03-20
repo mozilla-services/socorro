efb1f4f0530c2b735c2488a6cb3d2c52827578a9
Will Kahn-Greene <willkg@mozilla.com>
2019-03-20 11:10:42 -0400

    Remove dead code from crontabber and fix --list-jobs
    A while back, I nixed most of the depends_on code, but forgot to remove
    the code that used. This fixes that.

--- socorro/cron/base.py
+++ socorro/cron/base.py
@@ -2,9 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import collections
 import datetime
-import functools
 import logging
 import re
 
@@ -15,116 +13,6 @@ from socorro.lib.datetimeutil import utc_now
 
 class FrequencyDefinitionError(Exception):
     pass
-
-
-class CircularDAGError(Exception):
-    pass
-
-
-# The following two functions have been plucked from
-# https://bitbucket.org/ericvsmith/toposort but modified so it works
-# in Python 2.6.
-
-def toposort(data):
-    """Dependencies are expressed as a dictionary whose keys are items
-    and whose values are a set of dependent items. Output is a list of
-    sets in topological order. The first set consists of items with no
-    dependences, each subsequent set consists of items that depend upon
-    items in the preceeding sets.
-    """
-
-    # Special case empty input.
-    if len(data) == 0:
-        return
-
-    # Copy the input so as to leave it unmodified.
-    data = data.copy()
-
-    # Ignore self dependencies.
-    for k, v in data.items():
-        v.discard(k)
-    # Find all items that don't depend on anything.
-    extra_items_in_deps = functools.reduce(
-        set.union, data.values()
-    ) - set(data.keys())
-
-    # Add empty dependences where needed.
-    data.update(dict((item, set()) for item in extra_items_in_deps))
-    while True:
-        ordered = set(item for item, dep in data.items() if len(dep) == 0)
-        if not ordered:
-            break
-        yield ordered
-        data = dict(
-            (item, (dep - ordered))
-            for item, dep in data.items()
-            if item not in ordered
-        )
-    if len(data) != 0:
-        raise ValueError(
-            'Cyclic dependencies exist among these items: {}'
-            .format(', '.join(repr(x) for x in data.items()))
-        )
-
-
-def toposort_flatten(data, sort=True):
-    """Returns a single list of dependencies. For any set returned by
-    toposort(), those items are sorted and appended to the result (just to
-    make the results deterministic)."""
-
-    result = []
-    for d in toposort(data):
-        result.extend((sorted if sort else list)(d))
-    return result
-
-
-def reorder_dag(sequence,
-                depends_getter=lambda x: x.depends_on,
-                name_getter=lambda x: x.app_name,
-                impatience_max=100):
-    """
-    DAG = Directed Acyclic Graph
-    If we have something like:
-        C depends on B
-        B depends on A
-        A doesn't depend on any
-
-    Given the order of [C, B, A] expect it to return [A, B, C]
-
-    :arg sequence: some sort of iterable list
-    :arg depends_getter: a callable that extracts the depends on sub-list
-    :arg name_getter: a callable that extracts the name
-    :arg impatience_max: a max count that is reached before we end up in
-                         an infinite loop.
-    """
-
-    jobs = collections.defaultdict(list)
-    map_ = {}
-    _count_roots = 0
-    for each in sequence:
-        name = name_getter(each)
-        depends_on = depends_getter(each)
-        if depends_on is None:
-            depends_on = []
-        elif isinstance(depends_on, tuple):
-            depends_on = list(depends_on)
-        elif not isinstance(depends_on, list):
-            depends_on = [depends_on]
-        if not depends_on:
-            _count_roots += 1
-        jobs[name] += depends_on
-        map_[name] = each
-
-    if not _count_roots:
-        raise CircularDAGError("No job is at the root")
-
-    try:
-        jobs = dict(zip(jobs.keys(), map(set, jobs.values())))
-        ordered_jobs = list(toposort_flatten(jobs))
-    except ValueError as e:
-        raise CircularDAGError(e)
-
-    return [map_[x] for x in ordered_jobs if x in map_]
 
 
 def convert_frequency(frequency):
