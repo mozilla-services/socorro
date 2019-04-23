8c08f90759e84ead9ba5dadb47aaa59c5521c348
Will Kahn-Greene <willkg@mozilla.com>
2019-04-22 21:58:31 -0400

    bug 1546254: add django cmd support to socorro-cmd

--- webapp-django/manage.py
+++ webapp-django/manage.py
@@ -8,11 +8,15 @@ import os
 import sys
 
 
-if __name__ == '__main__':
+def main(argv):
     # Note! If you for some reason change that change the wsgi
     # starting-point script too.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crashstats.settings')
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(argv)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
