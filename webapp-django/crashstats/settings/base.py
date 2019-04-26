e6e988ab81801df64cda63e04dbbe87238785460
John Whitlock <jwhitlock@mozilla.com>
2019-04-26 15:24:11 -0500

    bug 1543097: Convert webapp to sentry_sdk 0.7.14
    Use the standard Django integration of sentry_sdk to replace raven.
    We're using the send_default_pii=False (the default), to explicitly
    not collect user data like email addresses, and connection data like IP
    addresses and cookies.

--- webapp-django/crashstats/settings/base.py
+++ webapp-django/crashstats/settings/base.py
@@ -11,7 +11,9 @@ import os
 import socket
 
 from decouple import config, Csv
+from sentry_sdk.integrations.django import DjangoIntegration
 import dj_database_url
+import sentry_sdk
 
 from crashstats.settings.bundles import NPM_FILE_PATTERNS, PIPELINE_CSS, PIPELINE_JS  # noqa
 from socorro.lib.revision_data import get_version
@@ -98,7 +100,6 @@ INSTALLED_APPS = (
     'crashstats.sources',
 
     'django.contrib.messages',
-    'raven.contrib.django.raven_compat',
     'waffle',
     'django_jinja',
 )
@@ -586,25 +587,16 @@ SOCORRO_REVISION = get_version()
 # Comma-separated list of urls that serve version information in JSON format
 OVERVIEW_VERSION_URLS = config('OVERVIEW_VERSION_URLS', '')
 
-# Raven sends errors to Sentry.
-# The release is optional.
+# Sentry aggregates reports of uncaught errors and other events
 SENTRY_DSN = config('SENTRY_DSN', '')
+SENTRY_DEBUG = config('SENTRY_DEBUG', False)  # Be noisy at init and processing events
 if SENTRY_DSN:
-    RAVEN_CONFIG = {
-        'dsn': SENTRY_DSN,
-        'release': SOCORRO_REVISION,
-        # Defines keys to be sanitized by SanitizeKeysProcessor
-        'sanitize_keys': [
-            'sessionid',
-            'csrftoken',
-            'anoncsrf',
-            'sc',
-        ],
-        'processors': (
-            'raven.processors.SanitizeKeysProcessor',
-            'raven.processors.SanitizePasswordsProcessor',
-        )
-    }
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        release=SOCORRO_REVISION,
+        send_default_pii=False,
+        integrations=[DjangoIntegration()],
+        debug=SENTRY_DEBUG)
 
 GOOGLE_ANALYTICS_ID = config('GOOGLE_ANALYTICS_ID', None)
 
