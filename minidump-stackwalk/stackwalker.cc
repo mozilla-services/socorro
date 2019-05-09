03e0f495ccc1f7e5597ab8e95f15efaaafec7e0f
Will Kahn-Greene <willkg@mozilla.com>
2019-05-09 16:08:49 -0400

    bug 1549717: fix module assert error
    There are some crash reports where the module in the frame is empty. This was
    causing the assertion to fail. Instead of asserting it's not empyt, we can
    convert it to an "if" block and add the module to the frame information only if
    it's there like we do with some other things. This works with the rest of the
    processing pipeline and the webapp report view.

--- minidump-stackwalk/stackwalker.cc
+++ minidump-stackwalk/stackwalker.cc
@@ -567,8 +567,9 @@ bool ConvertStackToJSON(const ProcessState& process_state,
       if (ContainsModule(modules_with_corrupt_symbols, frame->module)) {
         frame_data["corrupt_symbols"] = true;
       }
-      assert(!frame->module->code_file().empty());
-      frame_data["module"] = PathnameStripper::File(frame->module->code_file());
+      if (!frame->module->code_file().empty()) {
+        frame_data["module"] = PathnameStripper::File(frame->module->code_file());
+      }
 
       if (!frame->function_name.empty()) {
         frame_data["function"] = frame->function_name;
