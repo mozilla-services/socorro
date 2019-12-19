ce5e60b2778f848decda08b140ec6e7ef6f64838
Will Kahn-Greene <willkg@mozilla.com>
2019-12-19 10:44:49 -0500

    Enhance sqs_cli to set up required SQS queues
    This adds create-all and delete-all to sqs_cli so it creates/deletes all
    the required SQS queues for the local dev environment.
    
    This also adds an SQS section to docker/run_setup.py so that `make
    setup` sets up SQS queues.

--- docker/run_setup.sh
+++ docker/run_setup.sh
@@ -4,10 +4,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-set -e
-
 # Run scripts for setting up data sources for the local development environment.
 # This assumes that it is running in the webapp docker container.
+
+set -e
 
 # Drop and re-create the breakpad database with tables, stored procedures,
 # types, indexes, and keys; also bulk-loads static data for some lookup tables
@@ -23,6 +23,10 @@ set -e
 # Delete and create Pub/Sub topics and subscriptions
 /app/socorro-cmd pubsub delete
 /app/socorro-cmd pubsub create
+
+# Delete and create SQS queues
+/app/socorro-cmd sqs delete-all
+/app/socorro-cmd sqs create-all
 
 # Initialize the cronrun bookkeeping for all configured jobs to success
 /app/webapp-django/manage.py cronmarksuccess all
