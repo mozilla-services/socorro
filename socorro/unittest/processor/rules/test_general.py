a733787f9c10199a5d676ff3e556c72f59b85b73
Will Kahn-Greene <willkg@mozilla.com>
2020-04-27 10:49:08 -0400

    bug 1383113: switch to getitem notation for general rules

--- socorro/unittest/processor/rules/test_general.py
+++ socorro/unittest/processor/rules/test_general.py
@@ -14,101 +14,100 @@ from socorro.processor.rules.general import (
     IdentifierRule,
     OSInfoRule,
 )
-from socorro.unittest.processor import get_basic_processor_meta
 
 
-canonical_standard_raw_crash = DotDict(
-    {
-        "uuid": "00000000-0000-0000-0000-000002140504",
-        "InstallTime": "1335439892",
-        "AdapterVendorID": "0x1002",
-        "TotalVirtualMemory": "4294836224",
-        "Comments": "why did my browser crash?  #fail",
-        "Theme": "classic/1.0",
-        "Version": "12.0",
-        "Email": "noreply@mozilla.com",
-        "Vendor": "Mozilla",
-        "EMCheckCompatibility": "true",
-        "Throttleable": "1",
-        "id": "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",
-        "buildid": "20120420145725",
-        "AvailablePageFile": "10641510400",
-        "version": "12.0",
-        "AdapterDeviceID": "0x7280",
-        "ReleaseChannel": "release",
-        "submitted_timestamp": "2012-05-08T23:26:33.454482+00:00",
-        "URL": "http://www.mozilla.com",
-        "timestamp": 1336519593.454627,
-        "Notes": (
-            "AdapterVendorID: 0x1002, AdapterDeviceID: 0x7280, "
-            "AdapterSubsysID: 01821043, "
-            "AdapterDriverVersion: 8.593.100.0\nD3D10 Layers? D3D10 "
-            "Layers- D3D9 Layers? D3D9 Layers- "
-        ),
-        "CrashTime": "1336519554",
-        "Winsock_LSP": (
-            "MSAFD Tcpip [TCP/IPv6] : 2 : 1 :  \n "
-            "MSAFD Tcpip [UDP/IPv6] : 2 : 2 : "
-            "%SystemRoot%\\system32\\mswsock.dll \n "
-            "MSAFD Tcpip [RAW/IPv6] : 2 : 3 :  \n "
-            "MSAFD Tcpip [TCP/IP] : 2 : 1 : "
-            "%SystemRoot%\\system32\\mswsock.dll \n "
-            "MSAFD Tcpip [UDP/IP] : 2 : 2 :  \n "
-            "MSAFD Tcpip [RAW/IP] : 2 : 3 : "
-            "%SystemRoot%\\system32\\mswsock.dll \n "
-            "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
-            "\u0443\u0441\u043b\u0443\u0433 RSVP TCPv6 : 2 : 1 :  \n "
-            "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
-            "\u0443\u0441\u043b\u0443\u0433 RSVP TCP : 2 : 1 : "
-            "%SystemRoot%\\system32\\mswsock.dll \n "
-            "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
-            "\u0443\u0441\u043b\u0443\u0433 RSVP UDPv6 : 2 : 2 :  \n "
-            "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
-            "\u0443\u0441\u043b\u0443\u0433 RSVP UDP : 2 : 2 : "
-            "%SystemRoot%\\system32\\mswsock.dll"
-        ),
-        "FramePoisonBase": "00000000f0de0000",
-        "AvailablePhysicalMemory": "2227773440",
-        "FramePoisonSize": "65536",
-        "StartupTime": "1336499438",
-        "Add-ons": (
-            "adblockpopups@jessehakanen.net:0.3,"
-            "dmpluginff%40westbyte.com:1%2C4.8,"
-            "firebug@software.joehewitt.com:1.9.1,"
-            "killjasmin@pierros14.com:2.4,"
-            "support@surfanonymous-free.com:1.0,"
-            "uploader@adblockfilters.mozdev.org:2.1,"
-            "{a0d7ccb3-214d-498b-b4aa-0e8fda9a7bf7}:20111107,"
-            "{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}:2.0.3,"
-            "anttoolbar@ant.com:2.4.6.4,"
-            "{972ce4c6-7e08-4474-a285-3208198ce6fd}:12.0,"
-            "elemhidehelper@adblockplus.org:1.2.1"
-        ),
-        "BuildID": "20120420145725",
-        "SecondsSinceLastCrash": "86985",
-        "ProductName": "Firefox",
-        "legacy_processing": 0,
-        "AvailableVirtualMemory": "3812708352",
-        "SystemMemoryUsePercentage": "48",
-        "ProductID": "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",
-        "Distributor": "Mozilla",
-        "Distributor_version": "12.0",
-    }
-)
+canonical_standard_raw_crash = {
+    "uuid": "00000000-0000-0000-0000-000002140504",
+    "InstallTime": "1335439892",
+    "AdapterVendorID": "0x1002",
+    "TotalVirtualMemory": "4294836224",
+    "Comments": "why did my browser crash?  #fail",
+    "Theme": "classic/1.0",
+    "Version": "12.0",
+    "Email": "noreply@mozilla.com",
+    "Vendor": "Mozilla",
+    "EMCheckCompatibility": "true",
+    "Throttleable": "1",
+    "id": "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",
+    "buildid": "20120420145725",
+    "AvailablePageFile": "10641510400",
+    "version": "12.0",
+    "AdapterDeviceID": "0x7280",
+    "ReleaseChannel": "release",
+    "submitted_timestamp": "2012-05-08T23:26:33.454482+00:00",
+    "URL": "http://www.mozilla.com",
+    "timestamp": 1336519593.454627,
+    "Notes": (
+        "AdapterVendorID: 0x1002, AdapterDeviceID: 0x7280, "
+        "AdapterSubsysID: 01821043, "
+        "AdapterDriverVersion: 8.593.100.0\nD3D10 Layers? D3D10 "
+        "Layers- D3D9 Layers? D3D9 Layers- "
+    ),
+    "CrashTime": "1336519554",
+    "Winsock_LSP": (
+        "MSAFD Tcpip [TCP/IPv6] : 2 : 1 :  \n "
+        "MSAFD Tcpip [UDP/IPv6] : 2 : 2 : "
+        "%SystemRoot%\\system32\\mswsock.dll \n "
+        "MSAFD Tcpip [RAW/IPv6] : 2 : 3 :  \n "
+        "MSAFD Tcpip [TCP/IP] : 2 : 1 : "
+        "%SystemRoot%\\system32\\mswsock.dll \n "
+        "MSAFD Tcpip [UDP/IP] : 2 : 2 :  \n "
+        "MSAFD Tcpip [RAW/IP] : 2 : 3 : "
+        "%SystemRoot%\\system32\\mswsock.dll \n "
+        "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
+        "\u0443\u0441\u043b\u0443\u0433 RSVP TCPv6 : 2 : 1 :  \n "
+        "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
+        "\u0443\u0441\u043b\u0443\u0433 RSVP TCP : 2 : 1 : "
+        "%SystemRoot%\\system32\\mswsock.dll \n "
+        "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
+        "\u0443\u0441\u043b\u0443\u0433 RSVP UDPv6 : 2 : 2 :  \n "
+        "\u041f\u043e\u0441\u0442\u0430\u0432\u0449\u0438\u043a "
+        "\u0443\u0441\u043b\u0443\u0433 RSVP UDP : 2 : 2 : "
+        "%SystemRoot%\\system32\\mswsock.dll"
+    ),
+    "FramePoisonBase": "00000000f0de0000",
+    "AvailablePhysicalMemory": "2227773440",
+    "FramePoisonSize": "65536",
+    "StartupTime": "1336499438",
+    "Add-ons": (
+        "adblockpopups@jessehakanen.net:0.3,"
+        "dmpluginff%40westbyte.com:1%2C4.8,"
+        "firebug@software.joehewitt.com:1.9.1,"
+        "killjasmin@pierros14.com:2.4,"
+        "support@surfanonymous-free.com:1.0,"
+        "uploader@adblockfilters.mozdev.org:2.1,"
+        "{a0d7ccb3-214d-498b-b4aa-0e8fda9a7bf7}:20111107,"
+        "{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}:2.0.3,"
+        "anttoolbar@ant.com:2.4.6.4,"
+        "{972ce4c6-7e08-4474-a285-3208198ce6fd}:12.0,"
+        "elemhidehelper@adblockplus.org:1.2.1"
+    ),
+    "BuildID": "20120420145725",
+    "SecondsSinceLastCrash": "86985",
+    "ProductName": "Firefox",
+    "legacy_processing": 0,
+    "AvailableVirtualMemory": "3812708352",
+    "SystemMemoryUsePercentage": "48",
+    "ProductID": "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",
+    "Distributor": "Mozilla",
+    "Distributor_version": "12.0",
+}
 
-canonical_processed_crash = DotDict(
-    {
-        "json_dump": {
-            "system_info": {
-                "os_ver": "6.1.7601 Service Pack 1 ",
-                "cpu_count": 4,
-                "cpu_info": "GenuineIntel family 6 model 42 stepping 7",
-                "cpu_arch": "x86",
-                "os": "Windows NT",
-            }
+canonical_processed_crash = {
+    "json_dump": {
+        "system_info": {
+            "os_ver": "6.1.7601 Service Pack 1 ",
+            "cpu_count": 4,
+            "cpu_info": "GenuineIntel family 6 model 42 stepping 7",
+            "cpu_arch": "x86",
+            "os": "Windows NT",
         }
     }
-)
+}
+
+
+def get_basic_processor_meta():
+    return {"processor_notes": []}
 
 
 class TestDeNoneRule:
@@ -127,6 +126,8 @@ class TestDeNoneRule:
         assert raw_crash == expected
 
     def test_denone_with_dotdict(self):
+        # We want to explicitly test with DotDict since it might have different deletion
+        # things
         raw_crash = DotDict({"foo": "bar", "baz": None})
         expected = DotDict({"foo": "bar"})
 
@@ -213,9 +214,9 @@ class TestCPUInfoRule(object):
         # the call to be tested
         rule.act(raw_crash, raw_dumps, processed_crash, processor_meta)
 
-        assert processed_crash.cpu_arch == "x86"
-        assert processed_crash.cpu_info == "GenuineIntel family 6 model 42 stepping 7"
-        assert processed_crash.cpu_count == 4
+        assert processed_crash["cpu_arch"] == "x86"
+        assert processed_crash["cpu_info"] == "GenuineIntel family 6 model 42 stepping 7"
+        assert processed_crash["cpu_count"] == 4
 
         # raw crash should be unchanged
         assert raw_crash == canonical_standard_raw_crash
@@ -225,8 +226,7 @@ class TestCPUInfoRule(object):
         raw_dumps = {}
         system_info = copy.copy(canonical_processed_crash["json_dump"]["system_info"])
         del system_info["cpu_count"]
-        processed_crash = DotDict()
-        processed_crash.json_dump = {"system_info": system_info}
+        processed_crash = {"json_dump": {"system_info": system_info}}
         processor_meta = get_basic_processor_meta()
 
         rule = CPUInfoRule()
@@ -234,8 +234,10 @@ class TestCPUInfoRule(object):
         # the call to be tested
         rule.act(raw_crash, raw_dumps, processed_crash, processor_meta)
 
-        assert processed_crash.cpu_info == "GenuineIntel family 6 model 42 stepping 7"
-        assert processed_crash.cpu_arch == "x86"
+        assert (
+            processed_crash["cpu_info"] == "GenuineIntel family 6 model 42 stepping 7"
+        )
+        assert processed_crash["cpu_arch"] == "x86"
 
         # raw crash should be unchanged
         assert raw_crash == canonical_standard_raw_crash
@@ -261,16 +263,14 @@ class TestCPUInfoRule(object):
 class TestOSInfoRule(object):
     def test_everything_we_hoped_for(self):
         raw_crash = {}
-        processed_crash = DotDict(
-            {
-                "json_dump": {
-                    "system_info": {
-                        "os": "Windows NT",
-                        "os_ver": "6.1.7601 Service Pack 1",
-                    }
+        processed_crash = {
+            "json_dump": {
+                "system_info": {
+                    "os": "Windows NT",
+                    "os_ver": "6.1.7601 Service Pack 1",
                 }
             }
-        )
+        }
         processor_meta = get_basic_processor_meta()
 
         rule = OSInfoRule()
@@ -286,7 +286,7 @@ class TestOSInfoRule(object):
 
     def test_stuff_missing(self):
         raw_crash = {}
-        processed_crash = DotDict()
+        processed_crash = {}
         processor_meta = get_basic_processor_meta()
 
         rule = OSInfoRule()
