e5560f039c4077cb5b7a18db95e0c2a658e001bf
Will Kahn-Greene <willkg@mozilla.com>
2021-07-13 12:27:31 -0400

    Update django-waffle, boto3, awscli
    * update django-waffle to 2.2.1 which fixes the Jinja2 deprecation
      warning
    * update boto3 from 1.17.44 to 1.17.110
    * update awscli from 1.19.44 to 1.19.110
    * update requests from 2.25.1 to 2.26.0
      * switches from chardet to charset-normalizer
      * updates from idna 2 to idna 3

--- webapp-django/crashstats/settings/base.py
+++ webapp-django/crashstats/settings/base.py
@@ -169,14 +169,7 @@ TEMPLATES = [
                 "django_jinja.builtins.extensions.StaticFilesExtension",
                 "django_jinja.builtins.extensions.DjangoFiltersExtension",
                 "pipeline.jinja2.PipelineExtension",
-                # NOTE(willkg): We haven't used waffle in a while and should remove it.
-                # waffle uses a deprecated function in Jinja2 which is raising errors in
-                # our tests, so I'm commenting this out for now.
-                #
-                # We can uncomment this when
-                # https://github.com/django-waffle/django-waffle/issues/402 is fixed or
-                # we remove waffle.
-                # "waffle.jinja.WaffleExtension",
+                "waffle.jinja.WaffleExtension",
             ],
             "globals": {},
         },
