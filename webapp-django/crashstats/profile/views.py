c9ebd3a06daac2dd0d6646adbe3caaee5ef12b13
Will Kahn-Greene <willkg@mozilla.com>
2021-07-16 12:44:22 -0400

    bug 1688907: remove crash report list in profile page

--- webapp-django/crashstats/profile/views.py
+++ webapp-django/crashstats/profile/views.py
@@ -2,13 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import datetime
-
 from django.shortcuts import render
 from django.contrib.auth.models import Permission
 
 from crashstats.crashstats.decorators import pass_default_context, login_required
-from crashstats.supersearch.models import SuperSearchUnredacted
 
 
 @pass_default_context
@@ -18,19 +15,5 @@ def profile(request, default_context=None):
     context["permissions"] = Permission.objects.filter(content_type__model="").order_by(
         "name"
     )
-
-    start_date = (datetime.datetime.utcnow() - datetime.timedelta(weeks=4)).isoformat()
-
-    api = SuperSearchUnredacted()
-    results = api.get(
-        email=request.user.email,
-        date=">%s" % start_date,
-        _columns=["date", "uuid"],
-        _sort="-date",
-    )
-
-    context["crashes_list"] = [
-        dict(zip(("crash_id", "date"), (x["uuid"], x["date"]))) for x in results["hits"]
-    ]
 
     return render(request, "profile/profile.html", context)
