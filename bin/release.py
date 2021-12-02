a3d6adc938d5aefdf3d7cc89f9e98377a50b7cf1
Will Kahn-Greene <willkg@mozilla.com>
2021-12-02 09:36:36 -0500

    Update release script
    This picks up a nicety where it'll print out what I should copy and
    paste into the bug.

--- bin/release.py
+++ bin/release.py
@@ -13,7 +13,7 @@ both.
 This requires Python 3 to run.
 
 repo: https://github.com/willkg/socorro-release/
-sha: 926e826140d55515360205b28b4dd524997893a6
+sha: 522573cab99d7d7de2106853f3283d30bf88aaa9
 
 """
 
@@ -151,6 +151,20 @@ def make_tag(bug_number, remote_name, tag_name, commits_since_tag):
     input(f">>> Ready to push to remote {remote_name}? Ctrl-c to cancel")
     print(">>> Pushing...")
     subprocess.check_call(["git", "push", "--tags", remote_name, tag_name])
+
+    if bug_number:
+        # Show tag for adding to bug comment
+        print(f">>> Show tag... Copy and paste this into bug #{bug_number}.")
+        print(">>> %<-----------------------------------------------")
+        output = check_output(f"git show {tag_name}")
+        # Truncate the output at "diff --git"
+        output = output[: output.find("diff --git")].strip()
+        print(f"Tagged {tag_name}:")
+        print("")
+        print("```")
+        print(output)
+        print("```")
+        print(">>> %<-----------------------------------------------")
 
 
 def make_bug(
