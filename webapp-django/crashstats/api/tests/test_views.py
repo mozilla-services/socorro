5042b96c10e5b309f5af7702d0efe7e8fdc5e337
Will Kahn-Greene <willkg@mozilla.com>
2022-01-26 13:45:32 -0500

    Fix importing from collections
    Importing things from collections is deprecated in Python 3.3 and
    removed in Python 3.10.

--- webapp-django/crashstats/api/tests/test_views.py
+++ webapp-django/crashstats/api/tests/test_views.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from collections import Iterable
+from collections.abc import Iterable
 import contextlib
 import json
 from unittest import mock
