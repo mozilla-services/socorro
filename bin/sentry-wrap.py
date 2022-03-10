8ea993bbe32ddbdbc6bbaf44ee94e4973ae004f4
Will Kahn-Greene <willkg@mozilla.com>
2022-03-10 11:06:47 -0500

    bug 1758701: switch sentry-wrap.py to click
    This switches sentry-wrap.py to use click instead of argparse. We've got
    click in the requirements and it's easier to use for subcommands, so we
    should switch.
    
    This also improves the error handling situations so it's clearer what
    happened when something happened.
    
    This also adds timings to the success/fail status output.

--- bin/sentry-wrap.py
+++ bin/sentry-wrap.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -5,47 +7,49 @@
 # Wraps a command such that if it fails, an error report is sent to the Sentry service
 # specified by SENTRY_DSN in the environment.
 #
-# Usage: python bin/sentry-wrap.py -- [CMD]
+# Usage: python bin/sentry-wrap.py wrap -- [CMD]
+#    Wraps a process in error-reporting Sentry goodness.
+#
+# Usage: python bin/sentry-wrap.py test
+#    Tests Sentry configuration and connection.
 
 
-import argparse
 import os
 import shlex
 import subprocess
 import sys
+import time
+import traceback
 
+import click
 import sentry_sdk
 from sentry_sdk import capture_exception, capture_message
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Python-based cli for sentry error reporting."
-    )
-    parser.add_argument(
-        "--timeout",
-        type=int,
-        default=300,
-        help="Timeout to wait for process before giving up.",
-    )
-    parser.add_argument("cmd", nargs="+", help="The process to run.")
-
-    args = parser.parse_args()
-
+@click.command()
+@click.option("--timeout", default=300, help="Timeout in seconds to wait for process before giving up.")
+@click.argument("cmd", nargs=-1)
+@click.pass_context
+def wrap_process(ctx, timeout, cmd):
     sentry_dsn = os.environ.get("SENTRY_DSN")
 
     if not sentry_dsn:
         print("SENTRY_DSN is not defined. Exiting.")
         sys.exit(1)
 
+    if not cmd:
+        raise click.UsageError("CMD required")
+
+    start_time = time.time()
+
     sentry_sdk.init(sentry_dsn)
 
-    cmd = " ".join(args.cmd)
+    cmd = " ".join(cmd)
     cmd_args = shlex.split(cmd)
     print(f"Running: {cmd_args}")
 
     try:
-        ret = subprocess.run(cmd_args, capture_output=True, timeout=args.timeout)
+        ret = subprocess.run(cmd_args, capture_output=True, timeout=timeout)
         if ret.returncode != 0:
             sentry_sdk.set_context(
                 "status",
@@ -55,16 +59,28 @@ def main():
                     "stderr": ret.stderr.decode("utf-8"),
                 },
             )
-            capture_message(f"Command {args.cmd} failed.")
+            capture_message(f"Command {cmd!r} failed.")
+            print(ret.stdout.decode("utf-8"))
+            print(ret.stderr.decode("utf-8"))
+            time_delta = (time.time() - start_time) / 1000
+            print(f"Fail. {time_delta:.2f}s")
+            ctx.exit(1)
 
         else:
             print(ret.stdout.decode("utf-8"))
-            print("Success!")
+            time_delta = (time.time() - start_time) / 1000
+            print(f"Success! {time_delta:.2f}s")
+
+    except click.exceptions.Exit:
+        raise
 
     except Exception as exc:
         capture_exception(exc)
-        sys.exit(1)
+        traceback.print_exc()
+        time_delta = (time.time() - start_time) / 1000
+        print(f"Fail. {time_delta:.2f}s")
+        ctx.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    wrap_process()
