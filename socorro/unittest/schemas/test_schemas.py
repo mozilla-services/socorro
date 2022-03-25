fc4ca3742c32f6cf16f6b0ae60e5c1a69b8d2569
Will Kahn-Greene <willkg@mozilla.com>
2022-03-25 10:39:21 -0400

    bug 1754035: add test to validate schemas are valid schemas
    We had a validate_java_exception.py script which validated that one
    file, but it wasn't executed in CI and it only validated one of the two
    schemas we have.
    
    This removes that script and adds a test that validates all the schemas
    in that directory.
    
    This also improves the schema documentation.

---
+++ socorro/unittest/schemas/test_schemas.py
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+
+import jsonschema
+
+
+def test_validate_schemas(reporoot):
+    """Validate the schemas are themselves valid jsonschema"""
+    path = reporoot / "socorro" / "schemas"
+
+    for fn in path.glob("*.json"):
+        print(fn)
+        with open(fn) as fp:
+            schema = json.load(fp)
+
+        jsonschema.Draft4Validator.check_schema(schema)
