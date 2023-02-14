c5d71c7ca9dbf610d6ce02bb0ca9b6e70c3ff9e2
Will Kahn-Greene <willkg@mozilla.com>
2023-02-14 16:04:47 -0500

    bug 1529342: update webapp/crashstats/signature/ tests
    This also removes most of the mocking, fixes the test data, and makes
    the tests correct.

--- webapp/conftest.py
+++ webapp/conftest.py
@@ -2,5 +2,34 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import pytest
+
+from django.contrib.auth.models import Group
+
+
 # Load the socorro/tests/conftest.py file so webapp tests can use those pytest fixtures
 pytest_plugins = ["socorro.tests.conftest"]
+
+
+class UserHelper:
+    def __init__(self, django_user_model):
+        self._django_user_model = django_user_model
+
+    def create_user(self, username="example", password="pwd"):
+        user = self._django_user_model.objects.create_user(
+            username=username, password=password
+        )
+        return user
+
+    def create_protected_user(self, username="example", password="pwd"):
+        user = self.create_user(username=username, password=password)
+        group = Group.objects.get(name="Hackers")
+        user.groups.add(group)
+        assert user.has_perm("crashstats.view_pii")
+        assert user.has_perm("crashstats.view_rawdump")
+        return user
+
+
+@pytest.fixture
+def user_helper(django_user_model):
+    return UserHelper(django_user_model)
