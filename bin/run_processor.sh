67f58571e7f6bc201288c78941cc379e73db78ee
Will Kahn-Greene <willkg@mozilla.com>
2023-02-16 12:57:17 -0500

    bug 1698682: run processor with honcho

--- bin/run_processor.sh
+++ bin/run_processor.sh
@@ -4,18 +4,19 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Usage: bin/run_processor.sh
+# Usage: bin/run_processor_honcho.sh
 #
-# Runs the processor.
+# Runs processor process manager.
 #
 # Note: This should be called from inside a container.
 
 set -euo pipefail
 
-CMDPREFIX="${CMDPREFIX:-}"
+PROCESSOR_WORKERS=${PROCESSOR_WORKERS:-"1"}
 
-# Add /stackwalk to the path
-PATH=/stackwalk:${PATH:-}
-
-# Run the processor
-${CMDPREFIX} /app/socorro-cmd processor
+# Run honcho with PROCESSOR_WORKERS number of processor worker processes
+honcho \
+    --procfile /app/processor/Procfile \
+    --app-root /app \
+    start \
+    --concurrency "processor=${PROCESSOR_WORKERS}"
