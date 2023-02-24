89050d480678e201cafff0ed84f2d74c3215595f
Will Kahn-Greene <willkg@mozilla.com>
2023-02-24 10:41:10 -0500

    bug 1529342: add run_postdeploy.sh script
    We have several things we need to run post-deploy. This wraps them into
    a single script so it's easier to add more and make sure they're wrapped
    in Sentry handling.

--- bin/run_postdeploy.sh
+++ bin/run_postdeploy.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Usage: bin/run_migrations.sh
+# Usage: bin/run_postdeploy.sh
 #
 # This script runs migrations for Socorro. Run this in a crontabber docker
 # container.
@@ -24,10 +24,8 @@ else
     echo "SENTRY_DSN not defined--not enabling sentry."
 fi
 
-echo "starting run_migrations.sh: $(date)"
+echo "$(date): run migrations"
+${PRECMD} /app/bin/run_migrations.sh
 
-# Run Django migrations
-${PRECMD} python webapp/manage.py migrate --no-input
-
-# Insert missing versions
-${PRECMD} python bin/insert_missing_versions.py
+echo "$(date): upload telemetry schema"
+${PRECMD} python /app/bin/upload_telemetry_schema.py
