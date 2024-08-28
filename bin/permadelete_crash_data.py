d2161b5adea574e239cb69eaf0612e84374dcd86
Will Kahn-Greene <willkg@mozilla.com>
2024-08-28 16:50:34 -0400

    Adjust permadelete to do before/after CrashVerify requests
    This helps us understand the state of things before and after the code
    deletes the crash report.
    
    This also fixes exit codes when hitting errors.

--- bin/permadelete_crash_data.py
+++ bin/permadelete_crash_data.py
@@ -38,11 +38,20 @@ import click
 
 from socorro import settings
 from socorro.libclass import build_instance_from_settings
+from socorro.lib.librequests import session_with_retries
 
 
 logging.basicConfig()
 
 logger = logging.getLogger(__name__)
+
+
+HOSTS = {
+    # NOTE(willkg): this script runs in a docker container in the local dev environment
+    "local": "http://webapp:8000",
+    "stage": "https://crash-stats.allizom.org",
+    "prod": "https://crash-stats.mozilla.org",
+}
 
 
 def crashid_generator(fn):
@@ -56,20 +65,31 @@ def crashid_generator(fn):
 
 
 @click.command()
+@click.argument("env", nargs=1)
 @click.argument("crashidsfile", nargs=1)
 @click.pass_context
-def cmd_permadelete(ctx, crashidsfile):
+def cmd_permadelete(ctx, env, crashidsfile):
     """
     Permanently deletes crash report data from crash storage.
+
+    The env should be either "local", "stage", or "prod".
 
     The crashidsfile should be a complete path to the file with
     crashids in it--one per line. This will skip lines prefixed
     with a # treating them like comments.
 
     """
+    if env not in ["local", "stage", "prod"]:
+        click.echo('env should be either "local", "stage", or "prod".', err=True)
+        ctx.exit(1)
+
     if not os.path.exists(crashidsfile):
-        click.echo(f"File {crashidsfile!r} does not exist.")
-        return 1
+        click.echo(f"File {crashidsfile!r} does not exist.", err=True)
+        ctx.exit(1)
+
+    crashverify_url = f"{HOSTS[env]}/api/CrashVerify/"
+    session = session_with_retries()
+    headers = {"User-Agent": "socorro-permadelete/1.0"}
 
     crashstorage = build_instance_from_settings(settings.STORAGE)
     es_crashstorage = build_instance_from_settings(settings.ES_STORAGE)
@@ -79,6 +99,10 @@ def cmd_permadelete(ctx, crashidsfile):
     for crashid in crashids:
         click.echo(f"Working on {crashid!r}")
         start_time = datetime.datetime.now()
+        resp = session.get(
+            crashverify_url, headers=headers, params={"crash_id": crashid}
+        )
+        click.echo(f">>> before: {resp.json()}")
         # delete from storage
         click.echo(">>> Deleting from storage ...")
         crashstorage.delete_crash(crashid)
@@ -86,7 +110,12 @@ def cmd_permadelete(ctx, crashidsfile):
         click.echo(">>> Deleting from Elasticsearch ...")
         es_crashstorage.delete_crash(crashid)
         end_time = datetime.datetime.now()
+        resp = session.get(
+            crashverify_url, headers=headers, params={"crash_id": crashid}
+        )
+        click.echo(f">>> after: {resp.json()}")
         click.echo(f">>> Time: {end_time - start_time}")
+        click.echo("")
 
     click.echo("Done!")
 
