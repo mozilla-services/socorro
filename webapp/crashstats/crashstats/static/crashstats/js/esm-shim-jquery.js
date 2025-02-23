38437299962ccdf467285c90183251c16e903e86
Amri Toufali <amri@handsomemedia.com>
2025-02-23 13:23:12 -0800

    OBS-394: jquery ESM shim

---
+++ webapp/crashstats/crashstats/static/crashstats/js/esm-shim-jquery.js
@@ -0,0 +1,10 @@
+// # This Source Code Form is subject to the terms of the Mozilla Public
+// # License, v. 2.0. If a copy of the MPL was not distributed with this
+// # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// This jQuery shim is necessary as a separate file because imports are hoisted/evaluated before we can assign the global var.
+// By encapsulating this here, this entire shim will process before subsequent imports.
+
+import $ from 'jquery'
+window.jQuery = window.$ = $
+export { $ }
