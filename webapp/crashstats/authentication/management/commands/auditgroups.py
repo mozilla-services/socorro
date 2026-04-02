1c563bf4d2b75c3dec9e2aa0d415015d53bfeff9
Haseeb Afzal <hafzal@mozilla.com>
2026-04-02 12:35:38 -0400

    added auth0 checks

--- webapp/crashstats/authentication/management/commands/auditgroups.py
+++ webapp/crashstats/authentication/management/commands/auditgroups.py
@@ -13,9 +13,11 @@ from django.contrib.admin.models import LogEntry, CHANGE
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.conf import settings
 
 from crashstats.authentication.models import PolicyException
-
+from socorro.lib.librequests import session_with_retries
+from urllib.parse import urlparse
 
 VALID_EMAIL_DOMAINS = ("mozilla.com", "mozilla.org")
 
@@ -37,6 +39,47 @@ def delta_days(since_datetime):
     """Return the delta in days between now and since_datetime"""
     return (timezone.now() - since_datetime).days
 
+def get_access_token(client_id, client_secret, domain, session):
+    url = f"https://{domain}/oauth/token"
+    audience = f"https://{domain}/api/v2/"
+    payload = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials",
+        "audience": audience,
+    }
+    response = session.post(url, json=payload)
+    if response.status_code != 200:
+        raise RuntimeError(response.status_code)
+    return response.json()["access_token"]
+
+
+def find_users(client_id, client_secret, domain, email, session):
+    access_token = get_access_token(client_id, client_secret, domain, session)
+
+    url = f"https://{domain}/api/v2/users"
+    query = {"q": f'email:"{email}"'}
+    response = session.get(
+        url, params=query, headers={"Authorization": f"Bearer {access_token}"}
+    )
+    if response.status_code != 200:
+        raise RuntimeError(response.status_code)
+
+    return response.json()
+
+def is_blocked_in_auth0(email):
+    session = session_with_retries(total_retries=5)
+    users = find_users(
+        settings.OIDC_RP_CLIENT_ID,
+        settings.OIDC_RP_CLIENT_SECRET,
+        urlparse(settings.OIDC_OP_USER_ENDPOINT).netloc,
+        email,
+        session,
+    )
+    for user in users:
+        if user.get("blocked"):
+            return True
+    return False
 
 class Command(BaseCommand):
     help = "Audits Django groups and removes inactive users."
@@ -72,7 +115,16 @@ class Command(BaseCommand):
         # Go through the users and mark the ones for removal
         users_to_remove = []
         for user in hackers_group.user_set.all():
-            if not user.is_active:
+            is_blocked = False
+            try:
+                if self.is_employee_or_exception(user):
+                    is_blocked = is_blocked_in_auth0(user.email)
+            except RuntimeError as e:
+                self.stdout.write(f"Auth0 failed for: {user.email}: {e}")
+
+            if is_blocked:
+                users_to_remove.append((user, "user has lost employment"))
+            elif not user.is_active:
                 users_to_remove.append((user, "!is_active"))
 
             elif not self.is_employee_or_exception(user):
@@ -100,7 +152,13 @@ class Command(BaseCommand):
             self.stdout.write("Removing: %s (%s)" % (user.email, reason))
             if dryrun is False:
                 hackers_group.user_set.remove(user)
-
+                change_message = f"Removed {user.email} from hackers--{reason}."
+                
+                if reason == "user has lost employment":
+                    user.token_set.all().delete()
+                    self.stdout.write("Removing %s's tokens: (%s)" % (user.email, reason))
+                    change_message = f"Removed {user.email} from hackers and revoked API tokens--{reason}."
+                
                 # Toss a LogEntry in so we can keep track of when people get
                 # de-granted and what did it
                 LogEntry.objects.log_action(
@@ -109,8 +167,7 @@ class Command(BaseCommand):
                     object_id=user.pk,
                     object_repr=user.email,
                     action_flag=CHANGE,
-                    change_message="Removed %s from hackers--%s."
-                    % (user.email, reason),
+                    change_message=change_message,
                 )
 
         self.stdout.write("Total removed: %s" % len(users_to_remove))
