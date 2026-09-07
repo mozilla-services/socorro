86648f86df8581869dad89f43fcd0a630549c293
Sven Marnach <sven@mozilla.com>
2026-09-07 18:03:04 +0200

    bug-2068008: Fix control flow bug in the auditgroups cron job.

--- webapp/crashstats/authentication/management/commands/auditgroups.py
+++ webapp/crashstats/authentication/management/commands/auditgroups.py
@@ -69,6 +69,8 @@ def find_users(client_id, client_secret, domain, email, session):
 
 
 def is_blocked_in_auth0(email):
+    if settings.LOCAL_DEV_ENV:
+        return False
     session = session_with_retries(total_retries=5)
     users = find_users(
         settings.OIDC_RP_CLIENT_ID,
@@ -77,10 +79,7 @@ def is_blocked_in_auth0(email):
         email,
         session,
     )
-    for user in users:
-        if user.get("blocked"):
-            return True
-    return False
+    return any(user.get("blocked") for user in users)
 
 
 class Command(BaseCommand):
@@ -117,17 +116,17 @@ class Command(BaseCommand):
         # Go through the users and mark the ones for removal
         users_to_remove = []
         for user in hackers_group.user_set.all():
-            if not settings.LOCAL_DEV_ENV:
-                try:
-                    # User may be blocked as a security mitigation. Eg: too many login attempts
-                    if is_blocked_in_auth0(user.email):
-                        users_to_remove.append(
-                            (user, "user has most likely lost employment")
-                        )
-                except RuntimeError as e:
-                    self.stdout.write(f"Auth0 failed for: {user.email}: {e}")
+            try:
+                # User may be blocked as a security mitigation. Eg: too many login attempts
+                if is_blocked_in_auth0(user.email):
+                    users_to_remove.append(
+                        (user, "user has most likely lost employment")
+                    )
+                    continue
+            except RuntimeError as e:
+                self.stdout.write(f"Auth0 failed for: {user.email}: {e}")
 
-            elif not user.is_active:
+            if not user.is_active:
                 users_to_remove.append((user, "!is_active"))
 
             elif not self.is_employee_or_exception(user):
